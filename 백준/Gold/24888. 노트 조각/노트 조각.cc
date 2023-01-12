b7410f8b3ed2225a9f242066a7c288dac2b079b6
kss418 <108602943+kss418@users.noreply.github.com>
2023-01-12 17:22:42 +0900

    [Gold I] Title: 노트 조각, Time: 196 ms, Memory: 28016 KB -BaekjoonHub

--- 백준/Gold/24888. 노트 조각/노트 조각.cc
+++ 백준/Gold/24888. 노트 조각/노트 조각.cc
@@ -1,93 +1,107 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <cstdio>
 #include <iostream>
+#include <stdlib.h>
 #include <algorithm>
 #include <vector>
+#include <string>
+#include <string.h>
+#include <stack>
+#include <queue>
+#include <utility>
+#include <cmath>
+#include <map>
+#include <unordered_map>
 #include <set>
 #include <deque>
-#include <queue>
-#include <cstring>
+
+#define INF 1e9
+#define endl '\n'
+#define ll long long
 using namespace std;
-typedef long long ll;
-ll n, m, k, t; string s;
 
-const int MAX = 201010;
-vector <pair<int, int>> arr[MAX];
-ll a[MAX], d[MAX], dp[MAX], track[MAX];
-priority_queue <pair<ll, int>, vector<pair<ll, int>>, greater<pair<ll, int>>> pq;
-
-
-int main() {
-	cin.tie(0);
-	cout.tie(0);
-	ios::sync_with_stdio(false);
-
-	cin >> n >> m;
-
-	fill(d, d + n + 1,1e16);
-	memset(dp, -1, sizeof(dp));
-
-	while (m--) {
-		int st, en, cost;
-		cin >> st >> en >> cost;
-		arr[st].push_back({ en,cost });
-		arr[en].push_back({ st,cost });
-	}
-
-	int cnt = 0;
-	for (int i = 1; i <= n; i++) {
-		cin >> a[i];
-		cnt += a[i];
-	}
-	
-	dp[1] = a[1];
-	d[1] = 0;
-	pq.push({ d[1], 1 });
-	while (!pq.empty()) {
-		pair <ll, int> cur = pq.top();
-		pq.pop();
-		if (d[cur.second] != cur.first) {
-			continue;
-		}
-
-		for (auto nxt : arr[cur.second]) {
-			if (d[nxt.first] > d[cur.second] + nxt.second) {
-				d[nxt.first] = d[cur.second] + nxt.second;
-				track[nxt.first] = cur.second;
-				dp[nxt.first] = dp[cur.second] + a[nxt.first];
-
-				pq.push({ d[nxt.first],nxt.first });
-			}
-			else if (d[nxt.first] == d[cur.second] + nxt.second) {
-				if (dp[nxt.first] < dp[cur.second] + a[nxt.first]) {
-					track[nxt.first] = cur.second;
-					dp[nxt.first] = dp[cur.second] + a[nxt.first];
-					pq.push({ d[nxt.first],nxt.first });
-				}
-			}
-		}
-	}
-
-	vector <int> result;
-	if (dp[n] == cnt) {
-		int cur = n;
-		while (1) {
-			result.push_back(cur);
-			if (cur == 1) {
-				break;
-			}
-			cur = track[cur];
-		}
-
-		reverse(result.begin(), result.end());
-		cout << result.size() << "\n";
-		for (auto i : result) {
-			cout << i << " ";
-		}
-	}
-	else {
-		cout << -1;
-	}
+vector<pair<ll, ll>> arr[201010];
+ll cost[201010];
+ll dis[201010];
+ll dp[201010];
+ll seq[201010];
+vector<ll> path;
 
 
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
 
-	return 0;
+    ll n, m; cin >> n >> m;
+    for (int i = 0; i < m; i++) {
+        int u, v, w; cin >> u >> v >> w;
+        arr[u].push_back({ v,w });
+        arr[v].push_back({ u,w });
+    }
+
+    ll note = 0;
+    for (ll i = 1; i <= n; i++) {
+        cin >> cost[i];
+        note += cost[i];
+    }
+
+    memset(dis, 0x3f, sizeof(dis));
+    priority_queue<pair<ll, ll>, vector<pair<ll, ll>>, greater<pair<ll, ll>>> que;
+    dis[1] = 0;
+    que.push({ 0,1 });
+
+    dp[1] = cost[1];
+    ll curD; ll cur;
+    while (!que.empty())
+    {
+        tie(curD, cur) = que.top();
+        que.pop();
+        if (dis[cur] != curD) {
+            continue;
+        }
+
+        for (auto i : arr[cur]) {
+            ll next = i.first;
+            ll nextD = i.second;
+            if (nextD + curD < dis[next]) {
+                dis[next] = nextD + curD;
+                dp[next] = dp[cur] + cost[next];
+                que.push({ nextD + curD,next });
+                   seq[next] = cur;
+            }
+            else if (nextD + curD == dis[next]) {
+                if (dp[next] < dp[cur] + cost[next]) {
+                    dp[next] = max(dp[next], dp[cur] + cost[next]);
+                    que.push({ nextD + curD,next });
+                    seq[next] = cur;
+                }
+            }
+        }
+    }
+
+    if (dp[n] != note) {
+        cout << -1;
+        return 0;
+    }
+
+    ll x = n;
+    while (1) {
+        path.push_back(x);
+        if (x == 1) {
+            break;
+        }
+        x = seq[x];
+    }
+    reverse(path.begin(),path.end());
+
+
+    cout << path.size() << endl;
+    for (auto i : path) {
+        cout << i << " ";
+    }
+
+
+    return 0;
 }
