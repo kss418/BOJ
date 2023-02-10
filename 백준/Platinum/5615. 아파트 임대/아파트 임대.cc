f0d6a235f26c5c42734aae3228ad0b7d6eea7e9d
kss418 <108602943+kss418@users.noreply.github.com>
2023-02-10 15:42:14 +0900

    [Platinum I] Title: 아파트 임대, Time: 292 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Platinum/5615. 아파트 임대/아파트 임대.cc
+++ 백준/Platinum/5615. 아파트 임대/아파트 임대.cc
@@ -1,13 +1,5 @@
-#include <iostream>
-#include <algorithm>
-#include <vector>
-#include <cstring>
-#include <set>
-#include <queue>
-#include <deque>
-#include <cmath>
-#include <map>
-#include <stack>
+#include <bits/stdc++.h>
+#define fastio 	cin.tie(0), cout.tie(0),ios::sync_with_stdio(0);
 using namespace std; typedef long long ll;
 typedef pair<int, int> pi; typedef pair<ll, ll> pll;
 typedef unsigned long long ull;
@@ -19,51 +11,53 @@ const int MAX = 11;
 vector <ull> l_list = { 2, 325, 9375, 28178, 450775, 9780504, 1795265022 };
 vector <ull> i_list = { 2,7,61 };
 
+tuple <ll, ll, ll> gcd(ll a, ll b) {
+	if (!b) return { a,1,0 };
+	ll g, x, y;
+	tie(g, x, y) = gcd(b, a % b);
+	return{ g, y, y - a / b * x };
+}
 
+ull mul_mod(ll a, ll b, ll c) {
+	ll ret = a * b - c * ull(1.l / c * a * b);
+	return ret + c * (ret < 0) - c * (ret >= (ll)c);
+}
 
 ull pow_mod(ull a, ull b, ull c) {
-	if (!b) return 1;
-
-	if (b % 2) {
-		b--;
-		ull ret = pow_mod(a, b / 2, c) % c;
-		return (ret % c * ret % c) * a % c;
+	ull ret = 1; a %= c;
+	for (; b; b >>= 1) {
+		if (b & 1) ret = mul_mod(ret, a, c);
+		a = mul_mod(a, a, c);
 	}
-	else {
-		ull ret = pow_mod(a, b / 2, c) % c;
-		return (ret % c * ret % c) % c;
-	}
+	return ret;
 }
 
 bool mr(ull a, ull b) {
-	ull cur = a - 1;
-	cur /= 2;
-	while (cur % 2 == 0) {
-		if (pow_mod(b, cur, a) == a - 1) {
-			return 1;
-		}
-		cur /= 2;
+	if (a % b == 0) return 0;
+	for (ll d = a - 1; ; d >>= 1) {
+		ll t = pow_mod(b, d, a);
+		if (d & 1) return t != 1 && t != a - 1;
+		if (t == a - 1) return 0;
 	}
-	ull last = pow_mod(b, cur, a);
-	if (last == a - 1 || last == 1) return 1;
-	return 0;
 }
 
 bool is_prime(ull n) {
-	if (n <= 1)
-		return 0;
-	if (n == 2 || n == 7 || n == 61) return 1;
-	for (ull i : i_list)
-		if (!mr(n, i))
-			return 0;
+	if (n == 2 || n == 3 || n == 5 || n == 7) return 1;
+	if (n % 2 == 0 || n % 3 == 0 || n % 5 == 0 || n % 7 == 0) return 0;
+	if (n < 121) return n > 1;
+	if (n < 1ull << 32) for (ull i : { 2, 7, 61 }) {
+		if (n == i) return 1;
+		if (n > i && mr(n, i)) return 0;
+	}
+	else for (ull i : { 2, 325, 9375, 28178, 450775, 9780504, 1795265022 }) {
+		if (n == i) return 1;
+		if (n > i && mr(n, i)) return 0;
+	}
 	return 1;
 }
 
 int main() {
-	cin.tie(0);
-	cout.tie(0);
-	ios::sync_with_stdio(false);
-
+	fastio;
 
 	cin >> t;
 	ll result = 0;
@@ -72,8 +66,8 @@ int main() {
 		n = 2 * n + 1;
 		result += is_prime(n);
 	}
-	cout << result;
 
+	cout << result;
 
 	return 0;
 }
