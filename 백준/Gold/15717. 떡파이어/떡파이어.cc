777b89b7d3d5fe0caa6f1571c9f47e97e5038421
kss418 <108602943+kss418@users.noreply.github.com>
2023-02-10 21:48:15 +0900

    [Gold V] Title: 떡파이어, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/15717. 떡파이어/떡파이어.cc
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+#define fastio 	cin.tie(0), cout.tie(0),ios::sync_with_stdio(0);
+using namespace std; typedef long long ll;
+typedef pair<int, int> pi; typedef pair<ll, ll> pll;
+typedef unsigned long long ull;
+ll n, m, k, t; string s;
+
+
+const ll INF = 0x3f3f3f3f3f3f3f3f;
+const int MAX = 1110;
+const ll MOD = 1e9 + 7;
+
+ll mul_mod(ll a, ll b, ll c) {
+	ll ret = a * b - c * ll(1.l / c * a * b);
+	return ret + c * (ret < 0) - c * (ret >= (ll)c);
+}
+
+ll pow_mod(ll a, ll b, ll c) {
+	ll ret = 1; a %= c;
+	for (; b; b >>= 1) {
+		if (b & 1) ret = mul_mod(ret, a, c);
+		a = mul_mod(a, a, c);
+	}
+	return ret;
+}
+
+
+int main() {
+	fastio;
+
+	cin >> n;
+	if (!n) {
+		cout << 1;
+		return 0;
+	}
+
+	cout << pow_mod(2, n - 1, MOD);
+
+
+	return 0;
+}
