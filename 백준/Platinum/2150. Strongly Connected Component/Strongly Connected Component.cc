7725a3e2078ae8c489db901b00a4efed846f65bf
kss418 <108602943+kss418@users.noreply.github.com>
2024-08-30 16:32:15 +0900

    [Platinum V] Title: Strongly Connected Component, Time: 20 ms, Memory: 4436 KB -BaekjoonHub

--- 백준/Platinum/2150. Strongly Connected Component/Strongly Connected Component.cc
+++ 백준/Platinum/2150. Strongly Connected Component/Strongly Connected Component.cc
@@ -1,82 +1,93 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-#include <stack>
-using namespace std;
-typedef long long ll;
-int n, m;
-vector <int> arr[10001];
-int ord[10001];
-int finish[10001];
-int seq = 1;
-stack <int> list;
-vector <vector <int>> result;
-vector <int> cur;
+#include <bits/stdc++.h>
+#define fastio cin.tie(0), cout.tie(0), ios::sync_with_stdio(0);
+using namespace std; typedef long long ll;
+using pll = pair<ll, ll>; using tll = tuple<ll, ll, ll>;
+ll n, m, k, t; string s;
 
-int dfs(int node) {
-	ord[node] = seq++;
-	list.push(node);
-	int ret = ord[node];
-	for (auto i : arr[node]) {
-		if (ord[i] == 0) {
-			ret = min(ret, dfs(i));
-		}
-		else if (finish[i] == 0) {
-			ret = min(ret, ord[i]);
-		}
-	}
+constexpr ll INF = 0x3f3f3f3f3f3f3f3f;
+constexpr ll MAX = 1010101;
+constexpr ll MOD = 1e9 + 7;
 
-	if (ret == ord[node]) {
-		while (1) {
-			int top = list.top();
-			list.pop();
-			cur.push_back(top);
-			finish[top] = 1;
-			if (top == node) {
-				sort(cur.begin(), cur.end());
-				result.push_back(cur);
-				cur.clear();
-				break;
-			}
-		}
-	}
+class _scc { // 1-based index
+public:
+    ll n, dcnt = 0, scnt = 0;
+    vector <ll> d, sn;
+    vector<vector<ll>> adj, scc;
+    vector <bool> fin;
+    stack <ll> st;
 
-	return ret;
-}
+    _scc(ll n) {
+        this->n = n;
+        d.resize(n + 1); fin.resize(n + 1);
+        adj.resize(n + 1); sn.resize(n + 1);
+    }
 
+    void add(ll st, ll en) {
+        adj[st].push_back(en);
+    }
+
+
+    ll dfs(ll cur) {
+        d[cur] = ++dcnt;
+        st.push(cur);
+
+        ll mn = d[cur];
+        for (auto& nxt : adj[cur]) {
+            if (!d[nxt]) mn = min(mn, dfs(nxt));
+            else if (!fin[nxt]) mn = min(mn, d[nxt]);
+        }
+
+        if (mn == d[cur]) {
+            vector <ll> curscc;
+            while (1) {
+                ll t = st.top(); st.pop();
+                curscc.push_back(t);
+                fin[t] = 1; sn[t] = scnt;
+                if (t == cur) break;
+            }
+
+            sort(curscc.begin(), curscc.end());
+            scc.push_back(curscc); scnt++;
+        }
+
+        return mn;
+    }
+
+    void init() {
+        for (int i = 1; i <= n; i++) {
+            if (d[i]) continue;
+            dfs(i);
+        }
+    }
+
+    vector <vector <ll>> ret() { // scc 반환
+        sort(scc.begin(), scc.end());
+        return scc;
+    }
+
+    ll num(ll a) { // scc 번호 반환
+        return sn[a];
+    }
+};
 
 int main() {
-	cin.tie(0);
-	cout.tie(0);
-	ios_base::sync_with_stdio(false);
+    fastio;
+    cin >> n >> m;
+    _scc scc(n);
+    while (m--) {
+        ll s, e;
+        cin >> s >> e;
+        scc.add(s, e);
+    }
+    
+    scc.init();
+    vector <vector<ll>> ret = scc.ret();
 
-	cin >> n >> m;
-	int st, en;
-	for (int i = 0; i < m; i++) {
-		cin >> st >> en;
-		arr[st].push_back(en);
-	}
+    cout << ret.size() << "\n";
+    for (auto& i : ret) {
+        for (auto& j : i) cout << j << " ";
+        cout << -1 << "\n";
+    }
 
-	for (int i = 1; i <= n; i++) {
-		if (ord[i] == 0) {
-			dfs(i);
-		}
-	}
-
-	sort(result.begin(), result.end());
-	cout << result.size() << "\n";
-	for (auto i : result) {
-		for (auto j : i) {
-			cout << j << " ";
-		}
-		cout << -1 << "\n";
-	}
-	
-
-
-
-
-	return 0;
+    return 0;
 }
-
-
