caa36e5235df1c9b89961aa1cd44719131d643bf
kss418 <108602943+kss418@users.noreply.github.com>
2024-09-02 15:12:48 +0900

    [Platinum III] Title: 검색 엔진, Time: 0 ms, Memory: 2540 KB -BaekjoonHub

---
+++ 백준/Platinum/1108. 검색 엔진/검색 엔진.cc
@@ -0,0 +1,143 @@
+#include <bits/stdc++.h>
+#define fastio cin.tie(0), cout.tie(0), ios::sync_with_stdio(0);
+using namespace std; typedef long long ll;
+using pll = pair<ll, ll>; using tll = tuple<ll, ll, ll>;
+ll n, m, k, t; string s;
+
+constexpr ll INF = 0x3f3f3f3f3f3f3f3f;
+constexpr ll MAX = 2501;
+constexpr ll MOD = 1e9 + 7;
+vector <vector <ll>> revadj;
+
+class _scc { // 1-based index
+public:
+    ll n, dcnt = 0, scnt = 0;
+    vector <ll> d, sn, ind; // dfsn, scc번호, ind
+    vector<vector<ll>> adj, scc; // 간선, scc
+    vector <bool> fin;
+    stack <ll> st;
+    vector <set <ll>> sccset; // 위상정렬 간선
+
+    _scc(ll n) {
+        this->n = n;
+        d.resize(n + 1); fin.resize(n + 1);
+        adj.resize(n + 1); sn.resize(n + 1);
+    }
+
+    void add(ll st, ll en) {
+        adj[st].push_back(en);
+    }
+
+    ll dfs(ll cur) {
+        d[cur] = ++dcnt;
+        st.push(cur);
+
+        ll mn = d[cur];
+        for (auto& nxt : adj[cur]) {
+            if (!d[nxt]) mn = min(mn, dfs(nxt));
+            else if (!fin[nxt]) mn = min(mn, d[nxt]);
+        }
+
+        if (mn == d[cur]) {
+            vector <ll> curscc;
+            while (1) {
+                ll t = st.top(); st.pop();
+                curscc.push_back(t);
+                fin[t] = 1; sn[t] = scnt;
+                if (t == cur) break;
+            }
+
+            sort(curscc.begin(), curscc.end());
+            scc.push_back(curscc); scnt++;
+        }
+
+        return mn;
+    }
+
+    void init() {
+        for (int i = 1; i <= n; i++) {
+            if (d[i]) continue;
+            dfs(i);
+        }
+    }
+
+    vector <vector <ll>> ret() { // scc 반환
+        return scc;
+    }
+
+    ll num(ll a) { // scc 번호 반환
+        return sn[a];
+    }
+
+    vector <ll> top() { //위상정렬
+        deque <ll> q; vector <ll> ts;
+        sccset.resize(n + 1); ind.resize(n + 1);
+
+        for (auto& i : scc) {
+            for (auto& cur : i) {
+                for (auto nxt : adj[cur]) {
+                    if (num(cur) == num(nxt)) continue;
+                    if (sccset[num(cur)].count(num(nxt))) continue;
+                    sccset[num(cur)].insert(num(nxt));
+                    ind[num(nxt)]++;
+                }
+            }
+        }
+
+        for (int i = 0; i < scnt; i++) {
+            if (ind[i]) continue;
+            q.push_back(i);
+        }
+
+        while (!q.empty()) {
+            ll cur = q.front(); q.pop_front();
+            ts.push_back(cur);
+
+            for (auto& nxt : sccset[cur]) {
+
+                if (!--ind[nxt]) q.push_back(nxt);
+            }
+        }
+
+        return ts;
+    }
+};
+ll cnt = 0;
+map<string, ll> num;
+vector <ll> track, dp;
+
+int main() {
+    fastio;
+
+    cin >> n;
+    _scc scc(MAX);
+    while (n--) {
+        string cur, nxt;
+        cin >> cur; 
+        if (!num.count(cur)) num[cur] = ++cnt;
+        cin >> m;
+        while (m--) {
+            cin >> nxt;
+            if (!num.count(nxt)) num[nxt] = ++cnt;
+
+            scc.add(num[nxt], num[cur]);
+        }
+    }
+    scc.init();
+
+    dp.resize(MAX, 1);
+    for (int i = scc.scnt - 1; i >= 0; i--) {
+        for (auto& cur : scc.scc[i]) {
+            for (auto& nxt : scc.adj[cur]) {
+                if (scc.num(cur) == scc.num(nxt)) continue;
+                dp[nxt] += dp[cur];
+            }
+        }
+    }
+    
+    cin >> s;
+    cout << dp[num[s]];
+
+
+    return 0;
+}
