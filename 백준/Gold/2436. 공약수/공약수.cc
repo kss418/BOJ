227241e1d40296646ebbbde1f87b176d8c8a09ca
kss418 <108602943+kss418@users.noreply.github.com>
2024-09-03 03:05:54 +0900

    [Gold V] Title: 공약수, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

---
+++ 백준/Gold/2436. 공약수/공약수.cc
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+#define fastio cin.tie(0), cout.tie(0), ios::sync_with_stdio(0);
+using namespace std; typedef long long ll;
+using pll = pair<ll, ll>; using tll = tuple<ll, ll, ll>;
+ll n, m, k, t; string s;
+
+constexpr ll INF = 0x3f3f3f3f3f3f3f3f;
+constexpr ll MAX = 1010101;
+constexpr ll MOD = 1e9 + 7;
+
+class _gcd {
+public:
+    _gcd() {}
+
+    tuple <ll, ll, ll> init(ll a, ll b) {
+        if (!b) return { a, 1, 0 };
+        auto [g, x, y] = init(b, a % b);
+        return { g, y, x - a / b * y };
+    }
+
+    ll ret(ll a, ll b) {
+        return get<0>(init(a, b));
+    }
+
+    ll inv(ll a, ll mod) {
+        auto [g, x, y] = init(a, mod);
+        if (!a || g != 1) return -1;
+        return (x < 0 ? x + mod : x);
+    }
+};
+
+
+int main() {
+    fastio;
+   
+    _gcd gcd;
+    cin >> n >> m;
+    ll div = m / n;
+    for (int i = sqrt(div); i >= 1; i--) {
+        if (div % i) continue;
+        if (gcd.ret(i, div / i) != 1) continue;
+        cout << n * i << " " << n * div / i;
+        break;
+    }
+
+    return 0;
+}
