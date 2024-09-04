5d4f45fa4b8b51b414b09fb1cfcd6558a16008a8
kss418 <108602943+kss418@users.noreply.github.com>
2024-09-04 14:52:19 +0900

    [Bronze I] Title: 소인수분해, Time: 276 ms, Memory: 93352 KB -BaekjoonHub

---
+++ 백준/Bronze/11653. 소인수분해/소인수분해.cc
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+#define fastio cin.tie(0), cout.tie(0), ios::sync_with_stdio(0);
+using namespace std; typedef long long ll;
+using ull = unsigned long long;
+using pll = pair<ll, ll>; using tll = tuple<ll, ll, ll>;
+ll n, m, k, t; string s;
+
+constexpr ll INF = 0x3f3f3f3f3f3f3f3f;
+constexpr ll MAX = 10101010;
+constexpr ll MOD = 1e9 + 7;
+
+class _es {
+public:
+    ll n; vector <ll> p, pn;
+    _es(ll n) {
+        this->n = n;
+        p.resize(n + 1, -1);
+    }
+
+    void init() {
+        p[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            if (p[i] != -1) continue; p[i] = i;
+            pn.push_back(i);
+            for (int j = 2 * i; j <= n; j += i) {
+                if (p[j] == -1) p[j] = i;
+            }
+        }
+    }
+
+    ll mp(ll n) { // 최소 소인수 반환
+        return p[n];
+    }
+
+    vector<ll> ret() {
+        return pn;
+    }
+
+    ll is_p(ll n) {
+        if (n == 1) return 0;
+        return p[n] == n;
+    }
+};
+
+int main() {
+    fastio;
+    
+    _es es(MAX);
+    es.init();
+
+    cin >> n;
+    while (n != 1) {
+        cout << es.mp(n) << "\n";
+        n /= es.mp(n);
+    }
+
+
+    return 0;
+}
