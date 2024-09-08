aabb9b68452e05e107815b96c290edb58769025a
kss418 <108602943+kss418@users.noreply.github.com>
2024-09-08 10:56:01 +0900

    [Bronze IV] Title: FA, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/14935. FA/FA.cc
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#define fastio cin.tie(0), cout.tie(0), ios::sync_with_stdio(0);
+using namespace std; typedef long long ll;
+using ull = unsigned long long;
+using pll = pair<ll, ll>; using tll = tuple<ll, ll, ll>;
+ll n, m, k, t; string s;
+
+constexpr ll INF = 0x3f3f3f3f3f3f3f3f;
+constexpr ll MAX = 201010;
+constexpr ll MOD = 1e9 + 7;
+
+
+int main() {
+    fastio;
+
+    cin >> n;
+    cout << "FA";
+
+    return 0;
+}
