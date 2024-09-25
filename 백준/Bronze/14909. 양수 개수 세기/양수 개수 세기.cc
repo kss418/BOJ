edc10b9757c9b2a4578b20f151d2864fcb5bcc28
kss418 <108602943+kss418@users.noreply.github.com>
2024-09-25 20:30:32 +0900

    [Bronze III] Title: 양수 개수 세기, Time: 92 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/14909. 양수 개수 세기/양수 개수 세기.cc
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+#define fastio cin.tie(0), cout.tie(0), ios::sync_with_stdio(0);
+#define all(x) (x).begin(), (x).end()
+#define x first 
+#define y second
+using namespace std; typedef long long ll;
+using ull = unsigned long long;
+using pll = pair<ll, ll>; using tll = tuple<ll, ll, ll>;
+ll n, m, k, t; string s;
+
+constexpr ll INF = 0x3f3f3f3f3f3f3f3f;
+constexpr ll MAX = 201010;
+constexpr ll MOD = 998244353;
+
+int main() {
+    fastio;
+
+    ll result = 0;
+    while(cin >> n){
+        if(n >= 1) result++;
+    }
+    
+    cout << result;
+
+
+    return 0;
+}
