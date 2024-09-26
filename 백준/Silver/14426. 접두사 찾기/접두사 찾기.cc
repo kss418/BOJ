e7198f0d26bd1eea0ea34d944bda8e158a23ede8
kss418 <108602943+kss418@users.noreply.github.com>
2024-09-26 20:07:32 +0900

    [Silver I] Title: 접두사 찾기, Time: 184 ms, Memory: 162148 KB -BaekjoonHub

--- 백준/Silver/14426. 접두사 찾기/접두사 찾기.cc
+++ 백준/Silver/14426. 접두사 찾기/접두사 찾기.cc
@@ -1,66 +1,75 @@
-#include <iostream>
-#include <algorithm>
-#include <vector>
-#include <cstring>
-#include <set>
-#include <queue>
-#include <deque>
-#include <cmath>
-#include <map>
-#include <stack>
+#include <bits/stdc++.h>
+#define fastio cin.tie(0), cout.tie(0), ios::sync_with_stdio(0);
+#define all(x) (x).begin(), (x).end()
+#define x first 
+#define y second
 using namespace std; typedef long long ll;
-typedef pair<int, int> pi; typedef pair<ll, ll> pll;
+using ull = unsigned long long;
+using pll = pair<ll, ll>; using tll = tuple<ll, ll, ll>;
 ll n, m, k, t; string s;
 
+constexpr ll INF = 0x3f3f3f3f3f3f3f3f;
+constexpr ll MAX = 201010;
+constexpr ll MOD = 998244353;
 
-const ll INF = 0x3f3f3f3f3f3f3f3f;
-const int MAX = 501 * 10001;
-int seq = 2, arr[MAX][26];
+class _trie { // 0-based index
+public:
+    ll n, m, seq = 2;
+    vector <vector<ll>> adj;
+    vector <ll> chk;
 
+    _trie(ll k, ll n, ll m = 26) { // 문자열 개수, 문자열 길이, 문자 개수
+       this->m = m; this->n = n; 
+       chk.resize(n * k + 1); adj.resize(n * k + 1);
+    }
 
-void insert(string a) {
-	int cur = 1;
-	for (auto st : a) {
-		if (arr[cur][st - 'a'] == -1) {
-			arr[cur][st - 'a'] = seq++;
-		}
-		cur = arr[cur][st - 'a'];
-	}
-}
+    void insert(string& s){
+        ll cur = 1;
+        for(auto &st : s){
+            if(adj[cur].empty()) adj[cur].resize(m + 1);
+            if(!adj[cur][st - 'a']) adj[cur][st - 'a'] = seq++;
+            cur = adj[cur][st - 'a'];
+        }
+        chk[cur] = 1;
+    }
 
+    void erase(string& s){
+        ll cur = 1;
+        for(auto &st : s){
+            if(!adj[cur][st - 'a']) return;
+            cur = adj[cur][st - 'a'];
+        }
+        chk[cur] = 0;
+    }
 
-bool find(string a) {
-	int cur = 1;
-	for (auto st : a) {
-		if (arr[cur][st - 'a'] == -1) {
-			return 0;
-		}
-		cur = arr[cur][st - 'a'];
-	}
-
-	return 1;
-}
-
+    bool find(string& s){
+        ll cur = 1;
+        for(auto &st : s){
+            if(!adj[cur][st - 'a']) return 0;
+            cur = adj[cur][st - 'a'];
+        }
+        return 1;
+    }
+};
 
 int main() {
-	cin.tie(0);
-	cout.tie(0);
-	ios::sync_with_stdio(false);
+    fastio;
 
-	cin >> n >> m;
-	memset(arr, -1, sizeof(arr));
-	while (n--) {
-		cin >> s;
-		insert(s);
-	}
+    cin >> n >> m;
+    _trie trie(10101, 501);
+    while(n--){
+        cin >> s;
+        trie.insert(s);
+    }
 
-	ll result = 0;
-	while (m--) {
-		cin >> s;
-		result += find(s);
-	}
-	
-	cout << result;
+    ll result = 0;
+    while(m--){
+        cin >> s;
+        result += trie.find(s);
+    }
 
-	return 0;
+    cout << result;
+
+
+    return 0;
 }
