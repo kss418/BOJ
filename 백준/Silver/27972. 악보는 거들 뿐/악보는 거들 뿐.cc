7e5e7a16baf3c6778ecedaed2bbee5eb2195b2a9
kss418 <108602943+kss418@users.noreply.github.com>
2024-10-10 14:02:46 +0900

    [Silver I] Title: 악보는 거들 뿐, Time: 60 ms, Memory: 5936 KB -BaekjoonHub

---
+++ 백준/Silver/27972. 악보는 거들 뿐/악보는 거들 뿐.cc
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+#define ordered_set tree<int, null_type, less<int>, rb_tree_tag,tree_order_statistics_node_update>
+#define fastio cin.tie(0), cout.tie(0), ios::sync_with_stdio(0);
+#define all(x) (x).begin(), (x).end()
+#define x first 
+#define y second
+using namespace std; typedef long long ll;
+using ull = unsigned long long;
+using pll = pair<ll, ll>; using tll = tuple<ll, ll, ll>;
+ll n, m, k, t; string s;
+
+constexpr ll INF = 0x3f3f3f3f3f3f3f3f;
+constexpr ll MAX = 501010;
+constexpr ll MOD = 998244353;
+ll a[MAX];
+
+
+int main() {
+    fastio;
+
+    cin >> n;
+    for(int i = 1;i <= n;i++) cin >> a[i];
+
+    ll cnt = 0, result = 0;
+    for(int i = 1;i <= n;i++){
+        if(a[i] > a[i - 1]) cnt++;
+        else if(a[i] < a[i - 1]) cnt = 1;
+
+        result = max(cnt, result);
+    }
+
+    cnt = 0;
+    for(int i = 1;i <= n;i++){
+        if(a[i] < a[i - 1]) cnt++;
+        else if(a[i] > a[i - 1]) cnt = 1;
+
+        result = max(cnt, result);
+    }
+
+    cout << result;
+
+
+    return 0;
+}
