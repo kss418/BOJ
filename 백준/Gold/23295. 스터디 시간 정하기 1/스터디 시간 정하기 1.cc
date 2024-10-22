3b23fb32b769196d8c42026eb72dbf5cc3fb715b
kss418 <108602943+kss418@users.noreply.github.com>
2024-10-22 23:58:58 +0900

    [Gold III] Title: 스터디 시간 정하기 1, Time: 52 ms, Memory: 2808 KB -BaekjoonHub

---
+++ 백준/Gold/23295. 스터디 시간 정하기 1/스터디 시간 정하기 1.cc
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+#define ordered_set tree<int, null_type, less<int>, rb_tree_tag,tree_order_statistics_node_update>
+#define fastio cin.tie(0), cout.tie(0), ios::sync_with_stdio(0);
+#define all(x) (x).begin(), (x).end()
+#define x first 
+#define y second
+using namespace std; typedef long long ll;
+using ld = long double;
+using ull = unsigned long long;
+using pll = pair<ll, ll>; using tll = tuple<ll, ll, ll>;
+ll n, m, k, t; string s;
+
+constexpr ll INF = 0x3f3f3f3f3f3f3f3f;
+constexpr ll MAX = 101010;
+constexpr ll MOD = 998244353;
+ll cnt[MAX];
+
+int main() {
+    fastio;
+
+    cin >> n >> m; 
+    for(int i = 1;i <= n;i++){
+        cin >> k;
+        for(int i = 1;i <= k;i++){
+            ll s, e; cin >> s >> e;
+            cnt[s]++; cnt[e]--;
+        }
+    }
+    for(int i = 1;i <= 100001;i++) cnt[i] += cnt[i - 1];
+
+    ll mx = 0, result = 0, sum = 0;
+    for(int i = 0;i < m;i++) sum += cnt[i];
+    for(int i = m;i <= 100001;i++){
+        if(sum > mx){
+            mx = sum; result = i;
+        }
+        sum += cnt[i]; sum -= cnt[i - m];
+    }
+
+    cout << result - m << " " << result;
+
+    return 0;
+}
