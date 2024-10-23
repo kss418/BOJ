7d183e091d96f63940a3c6a875440e1388d45f24
kss418 <108602943+kss418@users.noreply.github.com>
2024-10-23 18:44:32 +0900

    [Gold V] Title: 가짜소수, Time: 0 ms, Memory: 3592 KB -BaekjoonHub

---
+++ 백준/Gold/4233. 가짜소수/가짜소수.cc
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+#define ordered_set tree<int, null_type, less<int>, rb_tree_tag,tree_order_statistics_node_update>
+#define fastio cin.tie(0), cout.tie(0), ios::sync_with_stdio(0);
+#define all(x) (x).begin(), (x).end()
+#define x first 
+#define y second
+using namespace std; typedef long long ll;
+using ld = long double;
+using ull = unsigned long long;
+using pll = pair<ll, ll>; using tll = tuple<ll, ll, ll>;
+ll n, m, k, t; string s;
+
+constexpr ll INF = 0x3f3f3f3f3f3f3f3f;
+constexpr ll MAX = 201010;
+constexpr ll MOD = 998244353;
+ll a[MAX];
+
+class _pow {
+public:
+	_pow() {}
+
+	ll ret(ll a, ll p) {
+		if (!p) return 1;
+		if (p == 1) return a;
+
+		ll cur = ret(a, p / 2);
+		cur = cur * cur;
+		if (p % 2 == 0) return cur;
+		return cur * a;
+	}
+
+	ll ret(ll a, ll p, ll mod) {
+		if (!p) return 1;
+		if (p == 1) return a % mod;
+
+		ll cur = ret(a, p / 2, mod);
+		cur = cur * cur % mod;
+		if (p % 2 == 0) return cur % mod;
+		return (cur * a) % mod;
+	}
+};
+
+
+int main() {
+    fastio;
+
+    _pow pow; 
+    while(1){
+        ll p, a; cin >> p >> a;
+        if(!a && !p) return 0;
+		bool prime = 1;
+		for(ll i = 2;i * i <= p;i++){
+			if(p % i) continue;
+			prime = 0; break;
+		}
+
+        ll num = pow.ret(a, p, p);
+        if(num % p == a % p && !prime) cout << "yes\n";
+        else cout << "no\n";
+    }
+
+    return 0;
+}
