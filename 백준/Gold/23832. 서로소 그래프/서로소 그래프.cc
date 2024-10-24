500eeac4520211c993ced03c4717d14d9e71eabd
kss418 <108602943+kss418@users.noreply.github.com>
2024-10-24 14:43:30 +0900

    [Gold I] Title: 서로소 그래프, Time: 0 ms, Memory: 3988 KB -BaekjoonHub

--- 백준/Gold/23832. 서로소 그래프/서로소 그래프.cc
+++ 백준/Gold/23832. 서로소 그래프/서로소 그래프.cc
@@ -1,38 +1,85 @@
 #include <bits/stdc++.h>
-#define fastio 	cin.tie(0), cout.tie(0),ios::sync_with_stdio(0);
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+#define ordered_set tree<int, null_type, less<int>, rb_tree_tag,tree_order_statistics_node_update>
+#define fastio cin.tie(0), cout.tie(0), ios::sync_with_stdio(0);
+#define all(x) (x).begin(), (x).end()
+#define x first 
+#define y second
 using namespace std; typedef long long ll;
-typedef pair<int, int> pi; typedef pair<ll, ll> pll;
-typedef unsigned long long ull;
+using ld = long double;
+using ull = unsigned long long;
+using pll = pair<ll, ll>; using tll = tuple<ll, ll, ll>;
 ll n, m, k, t; string s;
 
+constexpr ll INF = 0x3f3f3f3f3f3f3f3f;
+constexpr ll MAX = 201010;
+constexpr ll MOD = 998244353;
+ll a[MAX];
 
-const ll INF = 0x3f3f3f3f3f3f3f3f;
-const int MAX = 1110;
+class _es { // flag (1 == prime / 2 == phi / 4 == mobius)
+public:
+    ll n; vector <ll> pv, pn, mov, phiv;
+    vector <bool> v;
+    _es(ll n, ll flag = 1) {
+        this->n = n;
 
-ll op(ll a) {
-	ll ret = a;
-	for (ll i = 2; i * i <= a; i++) {
-		if (a % i == 0) {
-			ret *= (1 - 1 / (double)i);
-		}
-		while (a % i == 0)	a /= i;
-		if (a == 1) break;
-	}
+        if(flag & 1){
+            pv.resize(n + 1, -1); pv[1] = 1;
+            for (ll i = 2;i <= n;i++) {
+                if(pv[i] != -1) continue; pv[i] = i;
+                pn.push_back(i);
+                for (int j = 2 * i; j <= n; j += i) {
+                    if(pv[j] == -1) pv[j] = i;
+                }
+            }
+        }
 
-	if (a > 1) ret *= (1 - 1 / (double)a);
-	return ret;
-}
+        if(flag & 2){
+            phiv.resize(n + 1); iota(all(phiv), 0);
+            v.resize(n + 1);
+            for(ll i = 2;i <= n;i++){
+                if(v[i]) continue;
+                for(ll j = i;j <= n;j += i){
+                    phiv[j] /= i; phiv[j] *= (i - 1); v[j] = 1;
+                }
+            }
+        }
+
+        if(flag & 4){
+            mov.resize(n + 1, -2);
+            mov[0] = 0; mov[1] = 1;
+            for(ll i = 2;i <= n;i++){
+                if(mov[i] != -2) continue; mov[i] = -1;
+                for(ll j = 2 * i;j <= n;j += i) {
+                    if(mov[j] == -2) mov[j] = 1;
+                    mov[j] *= -1;
+                }
+                for(ll j = i * i;j <= n; j += i * i) mov[j] = 0;
+            }
+        }
+    }
+
+    ll mp(ll n) { return pv[n]; } // 최소 소인수 반환
+    vector<ll> ret() { return pn; } // 소수 벡터 반환
+    bool prime(ll n) { // 소수 판별
+        if (n == 1) return 0;
+        return pv[n] == n;
+    }
+    ll mobius(ll n){ return mov[n]; }
+    ll phi(ll n){ return phiv[n]; }
+};
 
 int main() {
-	fastio;
+    fastio;
 
-	cin >> n;
-	ll result = 0;
-	for (int i = 2; i <= n; i++) {
-		result += op(i);
-	}
-	cout << result;
+    cin >> n; _es es(n, 2);
+    ll result = 0;
+    for(int i = 2;i <= n;i++) result += es.phi(i);
 
-
-	return 0;
+    cout << result;
+    
+    
+    return 0;
 }
