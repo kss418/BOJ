a0cb903cebe5faec1d84cdd9c29ff0f3a48dbf1b
kss418 <108602943+kss418@users.noreply.github.com>
2024-10-29 15:03:56 +0900

    [Gold IV] Title: 테트리스 게임, Time: 12 ms, Memory: 2100 KB -BaekjoonHub

---
+++ 백준/Gold/4920. 테트리스 게임/테트리스 게임.cc
@@ -0,0 +1,93 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+#define ordered_set tree<int, null_type, less<int>, rb_tree_tag,tree_order_statistics_node_update>
+#define fastio cin.tie(0), cout.tie(0), ios::sync_with_stdio(0);
+#define all(x) (x).begin(), (x).end()
+#define x first 
+#define y second
+using namespace std; typedef long long ll;
+using ld = long double;
+using ull = unsigned long long;
+using pll = pair<ll, ll>; using tll = tuple<ll, ll, ll>;
+ll n, m, k, t; string s;
+
+constexpr ll INF = 0x3f3f3f3f3f3f3f3f;
+constexpr ll MAX = 101;
+constexpr ll MOD = 998244353;
+ll a[MAX][MAX], result;
+bool match[4][4][13];
+
+void init(){
+    for(int i = 0;i < 4;i++) match[0][i][0] = match[i][0][1] = 1;
+    for(int i = 0;i < 2;i++) match[i][1][2] = 1;
+    match[0][0][2] = match[1][2][2] = 1;
+    
+    for(int i = 0;i < 2;i++) match[1][i][3] = 1;
+    match[0][1][3] = match[2][0][3] = 1;
+
+    for(int j = 0;j < 3;j++) match[0][j][4] = match[1][j][5] = 1;
+    match[1][2][4] = match[0][0][5] = 1;
+
+    for(int j = 0;j < 3;j++) match[j][0][6] = match[j][1][7] = 1;
+    match[0][1][6] = match[2][0][7] = 1;
+
+    for(int i = 0;i < 3;i++){
+        match[i][0][8] = match[i][1][9] = 1;
+    }
+    match[1][1][8] = match[1][0][9] = 1;
+
+    for(int i = 0;i < 3;i++){
+        match[0][i][10] = match[1][i][11] = 1;
+    }
+    match[1][1][10] = match[0][1][11] = 1;
+    
+    for(int i = 0;i < 2;i++){
+        for(int j = 0;j < 2;j++) match[i][j][12] = 1;
+    }
+}
+
+ll cnt(ll cy, ll cx, ll num){
+    ll ret = 0;
+    for(int i = 0;i < 4;i++){
+        for(int j = 0;j < 4;j++){
+            ll ny = cy + i, nx = cx + j;
+            if(ny <= 0 || nx <= 0 || ny > n || nx > n){
+                if(match[i][j][num]) return -INF;
+                continue;
+            }
+
+            ret += a[ny][nx] * match[i][j][num];
+        }
+    }
+
+    return ret;
+}
+
+int main() {
+    fastio;
+
+    init();
+
+    while(1){
+        cin >> n; if(!n) break; t++;
+        for(int i = 1;i <= n;i++){
+            for(int j = 1;j <= n;j++) cin >> a[i][j];
+        }
+
+        result = -INF;
+        for(int i = 1;i <= n;i++){
+            for(int j = 1;j <= n;j++){
+                for(int k = 0;k < 13;k++){
+                    result = max(result, cnt(i, j, k));
+                }
+            }
+        }
+        
+        cout << t << ". " << result << "\n";
+    }
+
+    return 0;
+}
+
