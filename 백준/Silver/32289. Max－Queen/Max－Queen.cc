4616c01ab2a88f0bd66ce17197c8a8ceb8094bc7
kss418 <108602943+kss418@users.noreply.github.com>
2024-10-29 21:06:57 +0900

    [Silver V] Title: Max-Queen, Time: 0 ms, Memory: 3592 KB -BaekjoonHub

---
+++ 백준/Silver/32289. Max－Queen/Max－Queen.cc
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+#define ordered_set tree<int, null_type, less<int>, rb_tree_tag,tree_order_statistics_node_update>
+#define fastio cin.tie(0), cout.tie(0), ios::sync_with_stdio(0);
+#define all(x) (x).begin(), (x).end()
+#define x first 
+#define y second
+using namespace std; typedef long long ll;
+using ld = long double;
+using ull = unsigned long long;
+using pll = pair<ll, ll>; using tll = tuple<ll, ll, ll>;
+ll n, m, k, t; string s;
+
+constexpr ll INF = 0x3f3f3f3f3f3f3f3f;
+constexpr ll MAX = 201010;
+constexpr ll MOD = 998244353;
+ll a[MAX];
+
+int main() {
+    fastio;
+
+
+    cin >> n >> m;
+
+    ll result = (m - 1) * n + (n - 1) * m;
+    result += 2 * (m - 1) * (n - 1);
+ 
+    cout << result;
+    
+
+    return 0;
+}
+
