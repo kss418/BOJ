cba7ecdc336ae6c1edd4c633adca3d740e9e0c21
kss418 <108602943+kss418@users.noreply.github.com>
2024-10-31 15:20:04 +0900

    [Silver II] Title: 점프 숨바꼭질, Time: 0 ms, Memory: 2020 KB, Score: 125 point -BaekjoonHub

---
+++ 백준/Silver/22973. 점프 숨바꼭질/점프 숨바꼭질.cc
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+#include <ext/rope>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace __gnu_cxx;
+#define ordered_set tree<int, null_type, less<int>, rb_tree_tag,tree_order_statistics_node_update>
+#define fastio cin.tie(0), cout.tie(0), ios::sync_with_stdio(0);
+#define all(x) (x).begin(), (x).end()
+#define x first 
+#define y second
+using namespace std; typedef long long ll;
+using ld = long double;
+using ull = unsigned long long;
+using pll = pair<ll, ll>; using tll = tuple<ll, ll, ll>;
+ll n, m, k, t; string s;
+
+constexpr ll INF = 0x3f3f3f3f3f3f3f3f;
+constexpr ll MAX = 201010;
+constexpr ll MOD = 998244353;
+
+
+int main() {
+    fastio;
+
+    cin >> n; n = abs(n);
+    if(!n) { cout << 0; return 0; }
+    if(n % 2 == 0) { cout << -1; return 0; }
+
+    ll result = 0;
+    for(ll i = 0;(1ll << i) <= n;i++) result++;
+
+    cout << result;
+    
+    
+    return 0;
+}
