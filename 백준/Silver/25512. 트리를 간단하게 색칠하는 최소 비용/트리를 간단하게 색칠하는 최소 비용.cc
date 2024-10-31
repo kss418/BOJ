e64220bc427a6708a58cf319f636c92892c18e9e
kss418 <108602943+kss418@users.noreply.github.com>
2024-10-31 15:24:21 +0900

    [Silver I] Title: 트리를 간단하게 색칠하는 최소 비용, Time: 48 ms, Memory: 11792 KB -BaekjoonHub

---
+++ 백준/Silver/25512. 트리를 간단하게 색칠하는 최소 비용/트리를 간단하게 색칠하는 최소 비용.cc
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+#include <ext/rope>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace __gnu_cxx;
+#define ordered_set tree<int, null_type, less<int>, rb_tree_tag,tree_order_statistics_node_update>
+#define fastio cin.tie(0), cout.tie(0), ios::sync_with_stdio(0);
+#define all(x) (x).begin(), (x).end()
+#define x first 
+#define y second
+using namespace std; typedef long long ll;
+using ld = long double;
+using ull = unsigned long long;
+using pll = pair<ll, ll>; using tll = tuple<ll, ll, ll>;
+ll n, m, k, t; string s;
+
+constexpr ll INF = 0x3f3f3f3f3f3f3f3f;
+constexpr ll MAX = 101010;
+constexpr ll MOD = 998244353;
+ll a[MAX][2];
+vector <ll> adj[MAX];
+
+ll solve(ll cur, ll cnt){
+    ll ret = a[cur][cnt];
+    for(auto& nxt : adj[cur]) ret += solve(nxt, cnt ^ 1);
+
+    return ret;
+}
+
+int main() {
+    fastio;
+
+    cin >> n;
+    for(int i = 1;i < n;i++){
+        ll s, e; cin >> s >> e;
+        adj[s].push_back(e);
+    }
+    
+    for(int i = 0;i < n;i++) cin >> a[i][0] >> a[i][1];
+
+    cout << min(solve(0, 0), solve(0, 1));
+    
+    
+    return 0;
+}
