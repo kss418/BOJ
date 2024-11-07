1c84cfca60a350f737eac2ca3a41b01e52258180
kss418 <108602943+kss418@users.noreply.github.com>
2024-11-08 02:39:16 +0900

    [Gold II] Title: 펭귄 네비게이터, Time: 0 ms, Memory: 2344 KB -BaekjoonHub

---
+++ 백준/Gold/21739. 펭귄 네비게이터/펭귄 네비게이터.cc
@@ -0,0 +1,80 @@
+#include <bits/stdc++.h>
+#include <ext/rope>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+#define ordered_set tree<int, null_type, less<int>, rb_tree_tag,tree_order_statistics_node_update>
+#define fastio cin.tie(0), cout.tie(0), ios::sync_with_stdio(0);
+#define all(x) (x).begin(), (x).end()
+#define x first 
+#define y second
+using namespace std; typedef long long ll;
+using ld = long double;
+using ull = unsigned long long;
+using pll = pair<ll, ll>; using tll = tuple<ll, ll, ll>;
+ll n, m, k, t; string s;
+
+constexpr ll INF = 0x3f3f3f3f3f3f3f3f;
+constexpr ll MAX = 20101;
+constexpr ll MOD = 1e9 + 7;
+
+class _comb{
+public:
+    vector <ll> fac, inv; ll n, mod;
+    _comb(ll n, ll m = 998244353){ // m == MOD && m == prime
+        fac.resize(n + 1); inv.resize(n + 1);
+        this->mod = m; this->n = n;
+
+        fac[0] = 1; 
+        for(int i = 1;i <= n;i++) fac[i] = fac[i - 1] * i % mod;
+        inv[n] = pow(fac[n], mod - 2, mod);
+        for(int i = n - 1;i >= 0;i--) inv[i] = inv[i + 1] * (i + 1) % mod;
+    }
+
+    ll pow(ll a, ll p, ll mod) {
+		if (!p) return 1;
+		if (p == 1) return a % mod;
+
+		ll cur = pow(a, p / 2, mod);
+		cur = cur * cur % mod;
+		if (p % 2 == 0) return cur % mod;
+		return (cur * a) % mod;
+	}
+
+    ll ret(ll n, ll m){ // return nCm
+        if(n < 0 || m < 0 || n < m) return 0;
+        return fac[n] * inv[m] % mod * inv[n - m] % mod;
+    }
+};
+
+class _gcd {
+public:
+    _gcd() {}
+
+    tll init(ll a, ll b) {
+        if (!b) return { a, 1, 0 };
+        auto [g, x, y] = init(b, a % b);
+        return { g, y, x - a / b * y };
+    }
+
+    ll ret(ll a, ll b) {
+        return get<0>(init(a, b));
+    }
+
+    ll inv(ll a, ll mod) {
+        auto [g, x, y] = init(a, mod);
+        if (!a || g != 1) return -1;
+        return (x < 0 ? x + mod : x);
+    }
+};
+
+int main() {
+    fastio;
+
+    cin >> n; _comb comb(MAX, MOD); _gcd gcd;
+    cout << (comb.ret(2 * n, n) * gcd.inv(n + 1, MOD)) % MOD;
+
+
+    return 0;
+}
+
