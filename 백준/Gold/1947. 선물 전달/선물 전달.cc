a513016caa39f5d0c83c3caf164c1dd285945b2d
kss418 <108602943+kss418@users.noreply.github.com>
2024-11-08 15:17:36 +0900

    [Gold III] Title: 선물 전달, Time: 8 ms, Memory: 9912 KB -BaekjoonHub

---
+++ 백준/Gold/1947. 선물 전달/선물 전달.cc
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+#include <ext/rope>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace __gnu_cxx;
+#define ordered_set tree<int, null_type, less<int>, rb_tree_tag,tree_order_statistics_node_update>
+#define fastio cin.tie(0), cout.tie(0), ios::sync_with_stdio(0);
+#define all(x) (x).begin(), (x).end()
+#define x first 
+#define y second
+using namespace std; typedef long long ll;
+using ld = long double;
+using ull = unsigned long long;
+using pll = pair<ll, ll>; using tll = tuple<ll, ll, ll>;
+ll n, m, k, t; string s;
+
+constexpr ll INF = 0x3f3f3f3f3f3f3f3f;
+constexpr ll MAX = 1010101;
+constexpr ll MOD = 1e9;
+ll a[MAX];
+
+int main() {
+    fastio;
+
+    cin >> n; a[0] = 1; a[1] = 0;
+    for(int i = 2;i <= n;i++) a[i] = (i - 1) * (a[i - 1] + a[i - 2]) % MOD;
+    
+    cout << a[n] % MOD;
+
+    
+    return 0;
+}
