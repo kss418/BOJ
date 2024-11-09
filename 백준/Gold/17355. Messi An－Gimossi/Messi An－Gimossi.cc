11a524112963e95bbaf4d90472ea69b6f2ebc33e
kss418 <108602943+kss418@users.noreply.github.com>
2024-11-09 12:20:15 +0900

    [Gold II] Title: Messi An-Gimossi, Time: 468 ms, Memory: 172272 KB -BaekjoonHub

---
+++ 백준/Gold/17355. Messi An－Gimossi/Messi An－Gimossi.cc
@@ -0,0 +1,135 @@
+#include <bits/stdc++.h>
+#include <ext/rope>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+#define ordered_set tree<int, null_type, less<int>, rb_tree_tag,tree_order_statistics_node_update>
+#define fastio cin.tie(0), cout.tie(0), ios::sync_with_stdio(0);
+#define all(x) (x).begin(), (x).end()
+#define x first 
+#define y second
+using namespace std; typedef long long ll;
+using ld = long double;
+using ull = unsigned long long;
+using pll = pair<ll, ll>; using tll = tuple<ll, ll, ll>;
+ll n, m, k, t; string s;
+
+constexpr ll INF = 0x3f3f3f3f3f3f3f3f;
+constexpr ll MAX = 10101010; // SET MAX SIZE
+constexpr ll MOD = 1e9 + 7;
+ll cnt[MAX + 1];
+
+class _es { // flag (1 == prime / 2 == phi / 4 == mobius)
+public:
+    ll n; vector <ll> pv, pn, mov, phiv;
+    vector <bool> v;
+    _es(ll n, ll flag = 1) {
+        this->n = n;
+
+        if(flag & 1){
+            pv.resize(n + 1, -1); pv[1] = 1;
+            for (ll i = 2;i <= n;i++) {
+                if(pv[i] != -1) continue; pv[i] = i;
+                pn.push_back(i);
+                for (int j = 2 * i; j <= n; j += i) {
+                    if(pv[j] == -1) pv[j] = i;
+                }
+            }
+        }
+
+        if(flag & 2){
+            phiv.resize(n + 1); iota(all(phiv), 0);
+            v.resize(n + 1);
+            for(ll i = 2;i <= n;i++){
+                if(v[i]) continue;
+                for(ll j = i;j <= n;j += i){
+                    phiv[j] /= i; phiv[j] *= (i - 1); v[j] = 1;
+                }
+            }
+        }
+
+        if(flag & 4){
+            mov.resize(n + 1, -2);
+            mov[0] = 0; mov[1] = 1;
+            for(ll i = 2;i <= n;i++){
+                if(mov[i] != -2) continue; mov[i] = -1;
+                for(ll j = 2 * i;j <= n;j += i) {
+                    if(mov[j] == -2) mov[j] = 1;
+                    mov[j] *= -1;
+                }
+                for(ll j = i * i;j <= n; j += i * i) mov[j] = 0;
+            }
+        }
+    }
+
+    ll mp(ll n) { return pv[n]; } // 최소 소인수 반환
+    vector<ll>& ret() { return pn; } // 소수 벡터 반환
+    bool prime(ll n) { // 소수 판별
+        if (n == 1) return 0;
+        return pv[n] == n;
+    }
+    ll mobius(ll n){ return mov[n]; }
+    ll phi(ll n){ return phiv[n]; }
+};
+
+class _pow {
+public:
+	_pow() {}
+
+	ll ret(ll a, ll b){
+        ll ret = 1;
+        while(b){
+            if(b & 1) ret *= a;
+            a *= a; b >>= 1;
+        }      
+        return ret;
+    }
+
+	ll ret(ll a, ll b, ll p){
+        ll ret = 1;
+        while(b){
+            if(b & 1) ret *= a % p, ret %= p;
+            a *= a; a %= p; b >>= 1;
+        }      
+        return ret;
+    }
+};
+
+int main() {
+    fastio;
+
+    cin >> t; _es es(MAX);
+    while(t--){
+        cin >> n >> m; n = m - n;
+        while(n != 1){
+            cnt[es.mp(n)]++;
+            n /= es.mp(n);
+        }
+
+        while(m != 1){
+            cnt[es.mp(m)]--;
+            m /= es.mp(m);
+        }
+    }
+
+    ll a = 1, b = 1; _pow pow;
+    vector <ll>& p = es.ret();
+
+    for(auto& i : p){
+        if(cnt[i] > 0) {
+            a *= pow.ret(i, cnt[i], MOD);
+            a %= MOD;
+        }
+        
+        if(cnt[i] < 0){
+            b *= pow.ret(i, -cnt[i], MOD);
+            b %= MOD;
+        }
+    }
+
+    cout << a << " " << b;
+
+
+    return 0;
+}
+
