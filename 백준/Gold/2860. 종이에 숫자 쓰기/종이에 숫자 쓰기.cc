b400e8d46da1f0d1f23b46dff4afbdabad25ca52
kss418 <108602943+kss418@users.noreply.github.com>
2024-11-13 17:34:56 +0900

    [Gold IV] Title: 종이에 숫자 쓰기, Time: 0 ms, Memory: 3596 KB -BaekjoonHub

---
+++ 백준/Gold/2860. 종이에 숫자 쓰기/종이에 숫자 쓰기.cc
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+#include <ext/rope>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace __gnu_cxx;
+#define ordered_set tree<int, null_type, less<int>, rb_tree_tag,tree_order_statistics_node_update>
+#define fastio cin.tie(0), cout.tie(0), ios::sync_with_stdio(0);
+#define all(x) (x).begin(), (x).end()
+#define x first 
+#define y second
+using namespace std; typedef long long ll;
+using ld = long double;
+using ull = unsigned long long;
+using pll = pair<ll, ll>; using tll = tuple<ll, ll, ll>;
+ll n, m, k, t; string s;
+
+constexpr ll INF = 0x3f3f3f3f3f3f3f3f;
+constexpr ll MAX = 201010; // PLZ CHK!
+constexpr ll MOD = 998244353;
+ll a[MAX], result[6];
+
+class _gcd {
+public:
+    _gcd() {}
+
+    tll init(ll a, ll b) {
+        if (!b) return { a, 1, 0 };
+        auto [g, x, y] = init(b, a % b);
+        return { g, y, x - a / b * y };
+    }
+
+    ll ret(ll a, ll b) {
+        return get<0>(init(a, b));
+    }
+
+    ll inv(ll a, ll mod) {
+        auto [g, x, y] = init(a, mod);
+        if (!a || g != 1) return -1;
+        return (x < 0 ? x + mod : x);
+    }
+};
+
+int main() {
+    fastio;
+
+    cin >> s;
+    string now;
+    for(auto& i : s){
+        if(i == '.') continue;
+        now.push_back(i);
+    }
+
+    ll sum = stoll(now);
+    ll mul = 1;
+    while(mul <= sum) mul *= 10; mul /= 10;
+
+    _gcd gcd; ll g = gcd.ret(sum, mul);
+    sum /= g; mul /= g;
+    
+    ll mod = sum % mul, div = sum / mul;
+    result[div] = mul - mod;
+    result[div + 1] = mod;
+
+    for(int i = 1;i <= 5;i++) cout << result[i] << " ";
+    
+     
+    return 0;
+}
