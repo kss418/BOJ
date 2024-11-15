10e499870031efb7e33e18c41bb7cef5d351955a
kss418 <108602943+kss418@users.noreply.github.com>
2024-11-16 02:09:34 +0900

    [Silver II] Title: 겉넓이 구하기, Time: 0 ms, Memory: 2100 KB -BaekjoonHub

---
+++ 백준/Silver/16931. 겉넓이 구하기/겉넓이 구하기.cc
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+#include <ext/rope>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+#define ordered_set tree<int, null_type, less<int>, rb_tree_tag,tree_order_statistics_node_update>
+#define fastio cin.tie(0), cout.tie(0), ios::sync_with_stdio(0);
+#define all(x) (x).begin(), (x).end()
+#define x first 
+#define y second
+using namespace std; typedef long long ll;
+using ld = long double;
+using ull = unsigned long long;
+using pll = pair<ll, ll>; using tll = tuple<ll, ll, ll>;
+ll n, m, k, t; string s;
+
+constexpr ll INF = 0x3f3f3f3f3f3f3f3f;
+constexpr ll MAX = 102; // SET MAX SIZE
+constexpr ll MOD = 998244353;
+ll a[MAX][MAX];
+
+
+int main() {
+    fastio;
+
+    cin >> n >> m;
+    for(int i = 1;i <= n;i++){
+        for(int j = 1;j <= m;j++) cin >> a[i][j];
+    }
+
+    ll result = 2 * n * m;
+    for(int i = 1;i <= n;i++){
+        for(int j = 1;j <= m;j++){
+            result += max(a[i][j] - a[i - 1][j], 0ll);
+            result += max(a[i][j] - a[i + 1][j], 0ll);
+            result += max(a[i][j] - a[i][j - 1], 0ll);
+            result += max(a[i][j] - a[i][j + 1], 0ll);
+        }
+    }
+
+    cout << result;
+
+
+    return 0;
+}
+
