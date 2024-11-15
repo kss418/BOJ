80575cba2e27e30c4f945ccd15c5b05cd6944c72
kss418 <108602943+kss418@users.noreply.github.com>
2024-11-15 14:32:20 +0900

    [Platinum III] Title: 접미사 배열 2, Time: 100 ms, Memory: 5416 KB -BaekjoonHub

---
+++ 백준/Platinum/13264. 접미사 배열 2/접미사 배열 2.cc
@@ -0,0 +1,73 @@
+#include <bits/stdc++.h>
+#include <ext/rope>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+#define ordered_set tree<int, null_type, less<int>, rb_tree_tag,tree_order_statistics_node_update>
+#define fastio cin.tie(0), cout.tie(0), ios::sync_with_stdio(0);
+#define all(x) (x).begin(), (x).end()
+#define x first 
+#define y second
+using namespace std; typedef long long ll;
+using ld = long double;
+using ull = unsigned long long;
+using pll = pair<ll, ll>; using tll = tuple<ll, ll, ll>;
+ll n, m, k, t; string s;
+
+constexpr ll INF = 0x3f3f3f3f3f3f3f3f;
+constexpr ll MAX = 201010; // SET MAX SIZE
+constexpr ll MOD = 998244353;
+
+class _sa{
+public:
+    ll n, d, k = 0; string s;
+    vector <ll> sa, pos, tmp, lcp;
+    bool cmp(ll a, ll b){
+        if(pos[a] != pos[b]) return pos[a] < pos[b];
+        a += d; b += d;
+        if(a < n && b < n) return pos[a] < pos[b];
+        return a > b;
+    }  
+
+    void init(string& s){
+        this->s = s;
+        this->n = s.size(); tmp.resize(n);
+        sa.resize(n); pos.resize(n);
+        iota(all(sa), 0); lcp.resize(n);
+        for(int i = 0;i < n;i++) pos[i] = s[i];
+    }
+
+    void get_sa(){
+        for(d = 1; ;d <<= 1){
+            sort(all(sa), [this](ll a, ll b) { return cmp(a, b); }); tmp[0] = 0;
+            for(int i = 0;i < n - 1;i++) {
+                tmp[i + 1] = tmp[i] + cmp(sa[i], sa[i + 1]);
+            }
+            for(int i = 0;i < n;i++) pos[sa[i]] = tmp[i];
+            if(tmp[n - 1] == n - 1) break;
+        }
+    }
+
+    void get_lcp(){
+        for(int cur = 0;cur < n;cur++){
+            k = max(k - 1, 0ll);
+            if(pos[cur] == n - 1) continue;
+            ll nxt = sa[pos[cur] + 1];
+            while(s[cur + k] == s[nxt + k]) k++;
+            lcp[pos[cur]] = k;
+        }
+    }
+
+    _sa(string& s){ init(s); get_sa(); get_lcp(); }
+};
+
+
+int main() {
+    fastio;
+
+    cin >> s; _sa sa(s);
+    for(int i = 0;i < s.size();i++) cout << sa.sa[i] << "\n";
+
+    return 0;
+}
+
