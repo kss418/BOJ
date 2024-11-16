ca1836238daf2720852595611678319f96cbc64c
kss418 <108602943+kss418@users.noreply.github.com>
2024-11-16 13:33:13 +0900

    [Silver I] Title: 직각 삼각형, Time: 16 ms, Memory: 5176 KB -BaekjoonHub

---
+++ 백준/Silver/3000. 직각 삼각형/직각 삼각형.cc
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+#include <ext/rope>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace __gnu_cxx;
+#define ordered_set tree<int, null_type, less<int>, rb_tree_tag,tree_order_statistics_node_update>
+#define fastio cin.tie(0), cout.tie(0), ios::sync_with_stdio(0);
+#define all(x) (x).begin(), (x).end()
+#define x first 
+#define y second
+using namespace std; typedef long long ll;
+using ld = long double;
+using ull = unsigned long long;
+using pll = pair<ll, ll>; using tll = tuple<ll, ll, ll>;
+ll n, m, k, t; string s;
+
+constexpr ll INF = 0x3f3f3f3f3f3f3f3f;
+constexpr ll MAX = 101010; // PLZ CHK!
+constexpr ll MOD = 998244353;
+pll a[MAX];
+ll cx[MAX], cy[MAX];
+
+int main() {
+    fastio;
+
+    cin >> n;
+    for(int i = 1;i <= n;i++) {
+        cin >> a[i].x >> a[i].y;
+        cx[a[i].x]++; cy[a[i].y]++;
+    }
+
+    ll result = 0;
+    for(int i = 1;i <= n;i++) result += (cx[a[i].x] - 1) * (cy[a[i].y] - 1);
+
+    cout << result;
+    
+     
+    return 0;
+}
