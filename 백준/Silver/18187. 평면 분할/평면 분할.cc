727b17fa57ab0b2b36da564c53c32e3c05555065
kss418 <108602943+kss418@users.noreply.github.com>
2024-11-16 13:44:46 +0900

    [Silver II] Title: 평면 분할, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/18187. 평면 분할/평면 분할.cc
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+#include <ext/rope>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace __gnu_cxx;
+#define ordered_set tree<int, null_type, less<int>, rb_tree_tag,tree_order_statistics_node_update>
+#define fastio cin.tie(0), cout.tie(0), ios::sync_with_stdio(0);
+#define all(x) (x).begin(), (x).end()
+#define x first 
+#define y second
+using namespace std; typedef long long ll;
+using ld = long double;
+using ull = unsigned long long;
+using pll = pair<ll, ll>; using tll = tuple<ll, ll, ll>;
+ll n, m, k, t; string s;
+
+constexpr ll INF = 0x3f3f3f3f3f3f3f3f;
+constexpr ll MAX = 101; // PLZ CHK!
+constexpr ll MOD = 998244353;
+
+int main() {
+    fastio;
+
+    cin >> n;
+    ll result = 0;
+    for(int i = 0;i < n;i++){
+        ll left = n - i;
+        ll l = left / 2;
+        ll r = left / 2 + left % 2;
+
+        ll now = (l + 1) * (r + 1);
+        now += i * (l + r + 1);
+
+        result = max(result, now);
+    }
+
+    cout << result;
+    
+     
+    return 0;
+}
