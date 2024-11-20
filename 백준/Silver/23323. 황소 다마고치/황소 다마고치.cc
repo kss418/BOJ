ac207be1517e1e721cf708fb97ee85053d05ed9b
kss418 <108602943+kss418@users.noreply.github.com>
2024-11-20 12:36:38 +0900

    [Silver II] Title: 황소 다마고치, Time: 0 ms, Memory: 3592 KB -BaekjoonHub

---
+++ 백준/Silver/23323. 황소 다마고치/황소 다마고치.cc
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+#include <ext/rope>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+#define ordered_set tree<int, null_type, less<int>, rb_tree_tag,tree_order_statistics_node_update>
+#define fastio cin.tie(0), cout.tie(0), ios::sync_with_stdio(0);
+#define all(x) (x).begin(), (x).end()
+#define x first 
+#define y second
+using namespace std; typedef long long ll;
+using ld = long double;
+using ull = unsigned long long;
+using pll = pair<ll, ll>; using tll = tuple<ll, ll, ll>;
+ll n, m, k, t; string s;
+
+constexpr ll INF = 0x3f3f3f3f3f3f3f3f;
+constexpr ll MAX = 201010; // SET MAX SIZE
+constexpr ll MOD = 998244353;
+ll a[MAX];
+
+
+int main() {
+    fastio;
+
+    cin >> t;
+    while(t--){
+        cin >> n >> m; ll result = 0;
+        while(n) result++, n >>= 1;
+
+        cout << result + m << "\n";
+    }
+
+
+    return 0;
+}
+
