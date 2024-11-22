e37b645288044f209fdee57512dae3269aa96a59
kss418 <108602943+kss418@users.noreply.github.com>
2024-11-22 16:06:35 +0900

    [Gold I] Title: 정사각형, Time: 8416 ms, Memory: 2336 KB -BaekjoonHub

---
+++ 백준/Gold/9015. 정사각형/정사각형.cc
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+#include <ext/rope>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+#define ordered_set tree<int, null_type, less<int>, rb_tree_tag,tree_order_statistics_node_update>
+#define fastio cin.tie(0), cout.tie(0), ios::sync_with_stdio(0);
+#define all(x) (x).begin(), (x).end()
+#define x first 
+#define y second
+using namespace std; typedef long long ll;
+using ld = long double;
+using ull = unsigned long long;
+using pll = pair<ll, ll>; using tll = tuple<ll, ll, ll>;
+ll n, m, k, t; string s;
+
+constexpr ll INF = 0x3f3f3f3f3f3f3f3f;
+constexpr ll MAX = 3010; // SET MAX SIZE
+constexpr ll MOD = 998244353;
+map <pll, bool> num;
+pll a[MAX];
+
+int main() {
+    fastio;
+
+    cin >> t;
+    while(t--){
+        cin >> n; ll result = 0; num.clear();
+        for(int i = 1;i <= n;i++) {
+            cin >> a[i].x >> a[i].y;
+            num[{a[i].x, a[i].y}] = 1;
+        }
+        
+        for(int i = 1;i <= n;i++){
+            for(int j = i + 1;j <= n;j++){
+                ll dx = a[j].x - a[i].x;
+                ll dy = a[j].y - a[i].y;
+
+                pll v1 = {a[i].x - dy, a[i].y + dx};
+                pll v2 = {a[i].x + dx - dy, a[i].y + dy + dx};
+
+                if(!num.count(v1)) continue;
+                if(!num.count(v2)) continue;
+                result = max(result, dx * dx + dy * dy);
+            }
+        }
+
+        cout << result << "\n";
+    }
+
+
+    return 0;
+}
+
