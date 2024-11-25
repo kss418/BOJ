4948e47ea48c39b3bb30d064787cecf695073196
kss418 <108602943+kss418@users.noreply.github.com>
2024-11-25 23:49:17 +0900

    [Platinum II] Title: 고속도로, Time: 568 ms, Memory: 17580 KB -BaekjoonHub

--- 백준/Platinum/10254. 고속도로/고속도로.cc
+++ 백준/Platinum/10254. 고속도로/고속도로.cc
@@ -20,36 +20,26 @@ constexpr ll MAX = 201010; // SET MAX SIZE
 constexpr ll MOD = 998244353;
 pll a[MAX];
 
-template <typename T = ll>
 class _rc{
 public:
     _rc() {}
-    using ptl = pair<T, T>;
-    vector <T> num;
-    vector <ptl> point;
-    ll cnt = 0;
+    vector <ll> num; vector <pll> p; ll cnt = 0;
 
     class node{
     public:
-        T x, y, dx, dy; ll idx;
-        node(ptl a, ll idx) : node(a.x, a.y, 1, 0, idx) {};
-        node(T x, T y, T dx, T dy, ll idx){
+        ll x, y, idx;
+        node(pll a, ll idx) : node(a.x, a.y, idx) {};
+        node(ll x, ll y, ll idx){
             this->x = x; this->y = y;
-            this->dx = dx; this->dy = dy;
             this->idx = idx;
         }
-
-        bool operator <(node& ot){
-            if(dy * ot.dx != dx * ot.dy) return dy * ot.dx < dx * ot.dy;
-            if(y != ot.y) return y < ot.y;
-            return x < ot.x;
-        }
-        operator ptl(){ return { x, y }; }
+        operator pll(){ return { x, y }; }
+        operator pld(){ return { x, y }; }
     };
-    vector <node> arr;
+    vector <node> arr, st;
 
-    ll ccw(ptl v1, ptl v2, ptl v3){
-        T num = (v2.x - v1.x) * (v3.y - v1.y);
+    ll ccw(pll v1, pll v2, pll v3){
+        ll num = (v2.x - v1.x) * (v3.y - v1.y);
         num -= (v3.x - v1.x) * (v2.y - v1.y);
 
         if(num > 0) return 1;
@@ -57,40 +47,31 @@ public:
         return 0;
     }
     
-    void add(T a, T b) { add({a, b}); }
-    void add(ptl a){ arr.push_back({a, cnt++}); }
+    void add(ll a, ll b) { add({a, b}); }
+    void add(pll a){ arr.push_back({a, cnt++}); }
 
-    void init_ch(){
-        sort(all(arr));
-        for(int i = 1;i < arr.size();i++){
-            arr[i].dx = arr[i].x - arr[0].x;
-            arr[i].dy = arr[i].y - arr[0].y;
+    void init_ch(){    
+        swap(arr[0], *min_element(all(arr), [&](pll a, pll b){
+            if(a.x != b.x) return a.x < b.x;
+            return a.y < b.y;
+        }));
+
+        sort(arr.begin() + 1, arr.end(), [&](pll a, pll b){
+            ll dir = ccw(arr[0], {a.x, a.y}, {b.x, b.y});
+            if(dir) return dir > 0;
+            return d(arr[0], {a.x, a.y}) < d(arr[0], {b.x, b.y});
+        });
+
+        for(auto& i : arr){
+            while(st.size() >= 2 && ccw(st[st.size() - 2], st.back(), i) <= 0) st.pop_back();
+            st.push_back(i);
         }
-        sort(arr.begin() + 1, arr.end());
-
-        num.push_back(0);
-        if(arr.size() == 1) {
-            point.push_back(arr[0]); return;
-        }
-        num.push_back(1);
-        ll nxt = 2;
-
-        while(nxt < arr.size()){
-            while(num.size() >= 2){
-                ll fi = num.back(); num.pop_back();
-                ll se = num.back();
-                ll chk = ccw(arr[se], arr[fi], arr[nxt]);
-                if(chk > 0){ num.push_back(fi); break; }
-            }
-            num.push_back(nxt++);
-        }
-
-        for(auto& i : num) point.push_back(arr[i]);
+        for(auto& i : st) p.push_back(i);
     }
 
-    T area(ll a, ll b, ll c){ // div by 2
-        T num = 0;
-        vector <ptl> v = {point[a], point[b], point[c]};
+    ll area(ll a, ll b, ll c){ // div by 2
+        ll num = 0;
+        vector <pll> v = {p[a], p[b], p[c]};
         for(int i = 0;i < v.size();i++){
             auto [x1, y1] = v[i];
             auto [x2, y2] = v[(i + 1) % v.size()];
@@ -99,38 +80,38 @@ public:
         return abs(num);
     }
 
-    T dist(ptl a, ptl b) { // chk overflow
-        T dx = a.x - b.x, dy = a.y - b.y;
+    ld d(pld a, pld b) { // chk overflow
+        ld dx = a.x - b.x, dy = a.y - b.y;
         return dx * dx + dy * dy;
     }
 
-    ll mx = 0; pair <ptl, ptl> result;
+    ll mx = 0; pair <pll, pll> result;
     void get(ll l, ll r){
-        ll sz = point.size();
+        ll sz = p.size();
         for(int i = 0;i <= 1;i++){
             ll nxt = (r + i) % sz;
-            ll now = dist(point[l], point[nxt]);
+            ll now = d(p[l], p[nxt]);
             if(mx > now) continue;
-            mx = now; result = { point[l], point[nxt] };
+            mx = now; result = { p[l], p[nxt] };
         }
     }
 
     void init_rc(){
-        ll sz = point.size();
+        ll sz = p.size();
         if(sz == 2) get(0, 1);
         else{
-            ll l = 0, r = 1;
+            ll l = 0, r = 1; ld now = 0;
             while(area(sz - 1, l, r) < area(sz - 1, l, r + 1)) r++;
-            while(r < sz){
+            while(r < 2 * sz && l < sz){
                 get(l, r);
-                if(area(l, l + 1, r) < area(l, l + 1, (r + 1) % sz)) r++;
+                if(area(l, l + 1, r) < area(l, l + 1, r + 1)) r++;
                 else l++;
             }
         }
     }
 
     void init() { init_ch(); init_rc(); }
-    pair <ptl, ptl> ret(){ return result; }
+    pair <pll, pll> ret(){ return result; }
     ld dist(){ return sqrtl(mx); }
 };
 
