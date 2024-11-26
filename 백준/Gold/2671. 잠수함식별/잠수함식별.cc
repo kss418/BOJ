7f1510c5d7384765818d25825b7aca8e78662ef8
kss418 <108602943+kss418@users.noreply.github.com>
2024-11-26 18:16:40 +0900

    [Gold V] Title: 잠수함식별, Time: 0 ms, Memory: 3748 KB -BaekjoonHub

---
+++ 백준/Gold/2671. 잠수함식별/잠수함식별.cc
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+#include <ext/rope>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace __gnu_cxx;
+#define ordered_set tree<int, null_type, less<int>, rb_tree_tag,tree_order_statistics_node_update>
+#define fastio cin.tie(0), cout.tie(0), ios::sync_with_stdio(0);
+#define all(x) (x).begin(), (x).end()
+#define x first 
+#define y second
+using namespace std; typedef long long ll;
+using ld = long double; using pld = pair<ld, ld>;
+using ull = unsigned long long; 
+using pll = pair<ll, ll>; using tll = tuple<ll, ll, ll>;
+ll n, m, k, t; string s;
+
+constexpr ll INF = 0x3f3f3f3f3f3f3f3f;
+constexpr ll MAX = 201010; // SET MAX SIZE
+constexpr ll MOD = 998244353;
+ll a[MAX];
+
+
+int main() {
+    fastio;
+
+    cin >> s; regex pt("(100+1+|01)+");
+    if(regex_match(s, pt)) cout << "SUBMARINE\n";
+    else cout << "NOISE\n";
+    
+     
+    return 0;
+}
