a109256476b0fe4ca155ea2b1a3f303e5b180a5c
kss418 <108602943+kss418@users.noreply.github.com>
2024-11-27 14:44:46 +0900

    [Silver I] Title: 안정적인 문자열, Time: 0 ms, Memory: 3596 KB -BaekjoonHub

---
+++ 백준/Silver/4889. 안정적인 문자열/안정적인 문자열.cc
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+#include <ext/rope>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace __gnu_cxx;
+#define ordered_set tree<int, null_type, less<int>, rb_tree_tag,tree_order_statistics_node_update>
+#define fastio cin.tie(0), cout.tie(0), ios::sync_with_stdio(0);
+#define all(x) (x).begin(), (x).end()
+#define x first 
+#define y second
+using namespace std; typedef long long ll;
+using ld = long double; using pld = pair<ld, ld>;
+using ull = unsigned long long; 
+using pll = pair<ll, ll>; using tll = tuple<ll, ll, ll>;
+ll n, m, k, t; string s;
+
+constexpr ll INF = 0x3f3f3f3f3f3f3f3f;
+constexpr ll MAX = 201010; // SET MAX SIZE
+constexpr ll MOD = 998244353;
+ll a[MAX];
+
+
+int main() {
+    fastio;
+
+    while(++t){
+        cin >> s; if(s[0] == '-') break;
+        ll result = 0, cnt = 0;
+        for(auto& i : s){
+            if(i == '}') cnt--;
+            else cnt++;
+            
+            if(cnt < 0) cnt = 1, result++;
+        }
+
+        result += (cnt >> 1);
+        cout << t << ". " << result << "\n";
+    }
+    
+     
+    return 0;
+}
