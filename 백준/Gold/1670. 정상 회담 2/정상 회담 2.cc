6359959a7f49fbc431c4ca13134fc937507653ad
kss418 <108602943+kss418@users.noreply.github.com>
2025-01-09 01:49:49 +0900

    [Gold III] Title: 정상 회담 2, Time: 40 ms, Memory: 3592 KB -BaekjoonHub

---
+++ 백준/Gold/1670. 정상 회담 2/정상 회담 2.cc
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+#include <ext/rope>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace __gnu_cxx;
+#define ordered_set tree<int, null_type, less<int>, rb_tree_tag,tree_order_statistics_node_update>
+#define fastio cin.tie(0), cout.tie(0), ios::sync_with_stdio(0);
+#define all(x) (x).begin(), (x).end()
+#define x first 
+#define y second
+using namespace std; typedef long long ll;
+using ld = long double; using pld = pair<ld, ld>;
+using ull = unsigned long long; 
+using pll = pair<ll, ll>; using tll = tuple<ll, ll, ll>;
+ll n, m, k, t = 1; string s;
+
+constexpr ll INF = 0x3f3f3f3f3f3f3f3f;
+constexpr ll MAX = 201010; // SET MAX SIZE
+constexpr ll MOD = 987654321;
+ll c[MAX];
+
+void init(){
+    c[0] = 1;
+    for(int i = 1;i <= n / 2;i++){
+        for(int j = 0;j < i;j++) {
+            c[i] += c[j] * c[i - j - 1];
+            c[i] %= MOD;
+        }
+    }
+}
+
+void run(){
+    cin >> n; init();
+    cout << c[n / 2];
+}
+
+int main() {
+    fastio; // cin >> t;
+    while(t--) run(); 
+
+    return 0;
+}
+
