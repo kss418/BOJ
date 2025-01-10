5eb05907d32a61addcb2e2666274545f273f9701
kss418 <108602943+kss418@users.noreply.github.com>
2025-01-10 21:35:28 +0900

    [Bronze V] Title: 새싹, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.cc
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+#define fastio cin.tie(0), cout.tie(0),ios::sync_with_stdio(0);
+using namespace std; typedef long long ll; 
+using pll = pair<ll, ll>; ll n, m, k, t; string s;
+
+const ll INF = 0x3f3f3f3f3f3f3f3f;
+const ll MAX = 101010;
+
+
+int main() { 
+  fastio;
+
+  cout << "         ,r\'\"7\n";
+  cout << "r`-_   ,\'  ,/\n";
+  cout << " \\. \". L_r\'\n";
+  cout << "   `~\\/\n";
+  cout << "      |\n";
+  cout << "      |\n";
+  
+  return 0;
+}
