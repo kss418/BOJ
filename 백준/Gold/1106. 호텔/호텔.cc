d95074cb2803d32e5b91f9fdcb7a3a452958b007
kss418 <108602943+kss418@users.noreply.github.com>
2025-01-16 16:10:40 +0900

    [Gold IV] Title: 호텔, Time: 0 ms, Memory: 2204 KB -BaekjoonHub

---
+++ 백준/Gold/1106. 호텔/호텔.cc
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+#include <ext/rope>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace __gnu_cxx;
+#define ordered_set tree<int, null_type, less<int>, rb_tree_tag,tree_order_statistics_node_update>
+#define fastio cin.tie(0), cout.tie(0), ios::sync_with_stdio(0);
+#define all(x) (x).begin(), (x).end()
+#define x first 
+#define y second
+using namespace std; typedef long long ll;
+using ld = long double; using pld = pair<ld, ld>;
+using ull = unsigned long long; 
+using pll = pair<ll, ll>; using tll = tuple<ll, ll, ll>;
+ll n, m, k, t = 1; string s;
+
+constexpr ll INF = 0x3f3f3f3f3f3f3f3f;
+constexpr ll MINF = 0xc0c0c0c0c0c0c0c0;
+constexpr ll MAX = 1110; // SET MAX SIZE
+constexpr ll MOD = 998244353;
+ll dp[21][MAX];
+
+void run(){
+    cin >> n >> m; memset(dp, 0x3f, sizeof(dp));
+    dp[0][0] = 0;
+    for(int i = 1;i <= m;i++) {
+        ll a, b; cin >> a >> b;
+        for(int j = 0;j <= n + 100;j++){
+            dp[i][j] = min(dp[i][j], dp[i - 1][j]);
+            if(j >= b) dp[i][j] = min(dp[i][j], dp[i][j - b] + a);
+        }
+    }
+
+    ll result = INF;
+    for(int i = n;i <= n + 100;i++) result = min(result, dp[m][i]);
+    cout << result;
+}
+
+int main() {
+    fastio; // cin >> t;
+    while(t--) run(); 
+
+    return 0;
+}
+
