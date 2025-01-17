ed019aa86766c56782af848b915316c228feb831
kss418 <108602943+kss418@users.noreply.github.com>
2025-01-17 22:40:41 +0900

    [Platinum V] Title: Ignition, Time: 24 ms, Memory: 3952 KB -BaekjoonHub

---
+++ 백준/Platinum/13141. Ignition/Ignition.cc
@@ -0,0 +1,111 @@
+#include <bits/stdc++.h>
+#include <ext/rope>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace __gnu_cxx;
+#define ordered_set tree<int, null_type, less<int>, rb_tree_tag,tree_order_statistics_node_update>
+#define fastio cin.tie(0), cout.tie(0), ios::sync_with_stdio(0);
+#define all(x) (x).begin(), (x).end()
+#define x first 
+#define y second
+using namespace std; typedef long long ll;
+using ld = long double; using pld = pair<ld, ld>;
+using ull = unsigned long long; 
+using pll = pair<ll, ll>; using tll = tuple<ll, ll, ll>;
+ll n, m, k, t = 1; string s;
+
+constexpr ll INF = 0x3f3f3f3f3f3f3f3f;
+constexpr ll MINF = 0xc0c0c0c0c0c0c0c0;
+constexpr ll MAX = 201010; // SET MAX SIZE
+constexpr ll MOD = 998244353;
+class node{
+public:
+    ll s, e, c;
+};
+vector <node> arr;
+
+
+template <typename T = ll> // 1-based index
+class _floyd { // ret(i,j) == INF 처리하기
+public:
+    ll n; vector <vector<T>> d, nxt;
+    vector <ll> tr;
+
+    _floyd(ll n) {
+        this->n = n;
+        d.resize(n + 1, vector<T>(n + 1, INF));
+        nxt.resize(n + 1, vector<T>(n + 1, 0));
+    }
+
+    void add(ll st, ll en, T c = 1) { //양방향
+        d[st][en] = min(d[st][en], c);
+        d[en][st] = min(d[en][st], c);
+        nxt[st][en] = en; nxt[en][st] = st;
+    }
+
+    void addsol(ll st, ll en, T c = 1) { //단방향
+        d[st][en] = min(d[st][en], c);
+        nxt[st][en] = en;
+    }
+
+    void init() {
+        for (int k = 1; k <= n; k++) {
+            for (int i = 1; i <= n; i++) {
+                for (int j = 1; j <= n; j++) {
+                    if (d[i][j] <= d[i][k] + d[k][j]) continue;
+                    d[i][j] = d[i][k] + d[k][j];
+                    nxt[i][j] = nxt[i][k];
+                }
+            }
+        }
+    }
+
+    T ret(ll st, ll en) {
+        return d[st][en];
+    }
+
+    vector <ll> track(ll st, ll en) {
+        ll cur = st; tr.clear();
+
+        while (cur != en) {
+            tr.push_back(cur);
+            cur = nxt[cur][en];
+        }
+        tr.push_back(en);
+
+        return tr;
+    }
+};
+
+void run(){
+    cin >> n >> m; _floyd floyd(n);
+    for(int i = 1;i <= n;i++) floyd.add(i, i, 0);
+    for(int i = 1;i <= m;i++){
+        ll s, e, c; cin >> s >> e >> c; c *= 2;
+        floyd.add(s, e, c); arr.push_back({s, e, c});
+    }
+    floyd.init();
+
+    ll result = INF;
+    for(int i = 1;i <= n;i++){
+        ll now = 0;
+        for(int j = 0;j < m;j++){
+            auto[s, e, c] = arr[j];
+            ll st = floyd.ret(i, s), en = floyd.ret(i, e);
+            now = max(now, (c - abs(en - st)) / 2 + max(st, en)); 
+        }
+        result = min(result, now);
+    }
+
+    cout << result / 2 << ".";
+    cout << (result % 2 ? 5 : 0);
+}
+
+int main() {
+    fastio; // cin >> t;
+    while(t--) run(); 
+
+    return 0;
+}
+
