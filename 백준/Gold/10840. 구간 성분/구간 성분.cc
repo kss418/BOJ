6a248a3eff328b592bb3cf149ac91f5a111d29b3
kss418 <108602943+kss418@users.noreply.github.com>
2025-01-18 16:59:47 +0900

    [Gold I] Title: 구간 성분, Time: 280 ms, Memory: 2160 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/10840. 구간 성분/구간 성분.cc
+++ 백준/Gold/10840. 구간 성분/구간 성분.cc
@@ -19,35 +19,40 @@ constexpr ll INF = 0x3f3f3f3f3f3f3f3f;
 constexpr ll MINF = 0xc0c0c0c0c0c0c0c0;
 constexpr ll MAX = 2010; // SET MAX SIZE
 constexpr ll MOD = 998244353;
-ll p[26];
+ll p[26][2];
 string a, b;
 class _hash{
 public:
-    ll mod, key, v = 0;
+    ll mod, key, v1 = 0, v2 = 0;
     _hash(){}
-    void push(char c){ v += p[c - 'a']; v %= MOD; }
-    void pop(char c){ v -= p[c - 'a']; v = (v + MOD) % MOD; }
+    void push(char c){ 
+        v1 += p[c - 'a'][0]; v1 %= MOD; 
+        v2 += p[c - 'a'][1]; v2 %= 1000000007;
+    }
+    void pop(char c){ 
+        v1 -= p[c - 'a'][0]; v1 = (v1 + MOD) % MOD; 
+        v2 -= p[c - 'a'][1]; v2 = (v2 + 1000000007) % 1000000007; 
+    }
 };
 
 void run(){
-    cin >> a >> b; p[0] = 1;
-    for(int i = 1;i < 26;i++) p[i] = (p[i - 1] * 26) % MOD;
+    cin >> a >> b; p[0][0] = p[0][1] = 1;
+    for(int i = 1;i < 26;i++) p[i][0] = (p[i - 1][0] * 31) % MOD;
+    for(int i = 1;i < 26;i++) p[i][1] = (p[i - 1][1] * 67) % 1000000007;
     for(int i = min(a.size(), b.size());i >= 1;i--){
-        _hash ha;
+        _hash ha, hb; set <ll> num1, num2;
         for(int j = 0;j < i - 1;j++) ha.push(a[j]);
         for(int j = i - 1;j < a.size();j++){
             ha.push(a[j]);
             if(j >= i) ha.pop(a[j - i]);
+            num1.insert(ha.v1); num2.insert(ha.v2);
+        }
 
-            _hash hb;
-            for(int k = 0;k < i - 1;k++) hb.push(b[k]);
-            for(int k = i - 1;k < b.size();k++){
-                hb.push(b[k]);
-                if(k >= i) hb.pop(b[k - i]);
-                if(j >= i - 1 && k >= i - 1 && ha.v == hb.v) {
-                    cout << i; return;
-                }
-            }
+        for(int j = 0;j < i - 1;j++) hb.push(b[j]);
+        for(int j = i - 1;j < b.size();j++){
+            hb.push(b[j]);
+            if(j >= i) hb.pop(b[j - i]);
+            if(num1.count(hb.v1) && num2.count(hb.v2)) { cout << i; return; }
         }
     }
 
