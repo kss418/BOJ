ee1eb21d6afff90be6836de8e30f079e07debd74
kss418 <108602943+kss418@users.noreply.github.com>
2025-01-19 15:45:10 +0900

    [Platinum V] Title: 쿨한 물건 구매, Time: 0 ms, Memory: 3592 KB -BaekjoonHub

---
+++ 백준/Platinum/1214. 쿨한 물건 구매/쿨한 물건 구매.cc
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+#include <ext/rope>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace __gnu_cxx;
+#define ordered_set tree<int, null_type, less<int>, rb_tree_tag,tree_order_statistics_node_update>
+#define fastio cin.tie(0), cout.tie(0), ios::sync_with_stdio(0);
+#define all(x) (x).begin(), (x).end()
+#define x first 
+#define y second
+using namespace std; typedef long long ll;
+using ld = long double; using pld = pair<ld, ld>;
+using ull = unsigned long long; 
+using pll = pair<ll, ll>; using tll = tuple<ll, ll, ll>;
+ll n, m, k, t = 1; string s;
+
+constexpr ll INF = 0x3f3f3f3f3f3f3f3f;
+constexpr ll MINF = 0xc0c0c0c0c0c0c0c0;
+constexpr ll MAX = 201010; // SET MAX SIZE
+constexpr ll MOD = 998244353;
+ll a[MAX];
+
+void run(){
+    cin >> n >> m >> k;
+    if(k < m) swap(m, k);
+
+    ll result = INF;
+    for(ll i = 0;i <= min(n / k + 1, m);i++){
+        ll cnt = (n - k * i) / m + ((n - k * i) % m ? 1 : 0);
+        if(n - k * i <= 0) cnt = 0;
+        ll now = k * i + m * cnt; result = min(result, now); 
+    }
+
+    cout << result;
+}
+
+int main() {
+    fastio; // cin >> t;
+    while(t--) run(); 
+
+    return 0;
+}
+
