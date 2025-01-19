b29e71c04bcd11730496b1125d7b9abf48ddeb59
kss418 <108602943+kss418@users.noreply.github.com>
2025-01-19 16:20:01 +0900

    [Gold III] Title: 최대 최소공배수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/25342. 최대 최소공배수/최대 최소공배수.cc
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+#include <ext/rope>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace __gnu_cxx;
+#define ordered_set tree<int, null_type, less<int>, rb_tree_tag,tree_order_statistics_node_update>
+#define fastio cin.tie(0), cout.tie(0), ios::sync_with_stdio(0);
+#define all(x) (x).begin(), (x).end()
+#define x first 
+#define y second
+using namespace std; typedef long long ll;
+using ld = long double; using pld = pair<ld, ld>;
+using ull = unsigned long long; 
+using pll = pair<ll, ll>; using tll = tuple<ll, ll, ll>;
+ll n, m, k, t = 1; string s;
+
+constexpr ll INF = 0x3f3f3f3f3f3f3f3f;
+constexpr ll MINF = 0xc0c0c0c0c0c0c0c0;
+constexpr ll MAX = 101010; // SET MAX SIZE
+constexpr ll MOD = 998244353;
+
+class _gcd {
+public:
+    _gcd() {}
+
+    tll init(ll a, ll b) {
+        if (!b) return { a, 1, 0 };
+        auto [g, x, y] = init(b, a % b);
+        return { g, y, x - a / b * y };
+    }
+
+    ll ret(ll a, ll b) {
+        return get<0>(init(a, b));
+    }
+
+    ll inv(ll a, ll mod) {
+        auto [g, x, y] = init(a, mod);
+        if (!a || g != 1) return -1;
+        return (x < 0 ? x + mod : x);
+    }
+} g;
+ 
+void run(){
+    cin >> n; ll mi = (n % 2) ^ 1;
+    ll result = (n - mi) * (n - mi - 1) * (n - mi - 2);
+
+    ll mul = n * (n - 1);
+    for(int i = n - 2;i >= max(n - 5, 1ll);i--){
+        result = max(result, mul * i / g.ret(mul, i));
+    }
+
+    cout << result << "\n";
+}
+
+int main() {
+    fastio; cin >> t;
+    while(t--) run(); 
+
+    return 0;
+}
+
