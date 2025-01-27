f39bc4a198d63adf5c836b1540333439837d7927
kss418 <108602943+kss418@users.noreply.github.com>
2025-01-27 13:10:37 +0900

    [Gold IV] Title: 경로 찾기, Time: 16 ms, Memory: 2204 KB -BaekjoonHub

---
+++ 백준/Gold/2479. 경로 찾기/경로 찾기.cc
@@ -0,0 +1,113 @@
+#include <bits/stdc++.h>
+#include <ext/rope>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace __gnu_cxx;
+#define ordered_set tree<int, null_type, less<int>, rb_tree_tag,tree_order_statistics_node_update>
+#define fastio cin.tie(0), cout.tie(0), ios::sync_with_stdio(0);
+#define all(x) (x).begin(), (x).end()
+#define x first 
+#define y second
+using namespace std; typedef long long ll;
+using ld = long double; using pld = pair<ld, ld>;
+using ull = unsigned long long; 
+using pll = pair<ll, ll>; using tll = tuple<ll, ll, ll>;
+ll n, m, k, t = 1; string s;
+
+constexpr ll INF = 0x3f3f3f3f3f3f3f3f;
+constexpr ll MINF = 0xc0c0c0c0c0c0c0c0;
+constexpr ll MAX = 1010; // SET MAX SIZE
+constexpr ll MOD = 998244353;
+string st[MAX];
+vector <ll> result;
+
+class _dij {
+public:
+    class node{
+    public:
+        ll d;
+        node() : node(0){}
+        node(ll d) : d(d){}
+        ll num() const{ return d; }
+        bool operator<(const node& ot) const{ return num() < ot.num(); }
+        bool operator>(const node& ot) const{ return num() > ot.num(); }
+        bool operator==(const node& ot) const{ return num() == ot.num(); }
+        bool operator<=(const node& ot) const{ return num() <= ot.num(); }
+        node operator+(const node& ot) const{
+            return {d + ot.d};
+        }
+        operator ll(){ return d; }
+    };
+    node mx(){ return {INF}; }
+    node mn(){ return {0}; }
+
+    using ptl = pair <node, ll>;
+    ll n; vector <node> d;
+    vector <ll> pre;
+    vector <vector<ptl>> adj;
+    priority_queue <ptl, vector<ptl>, greater<ptl>> pq;
+
+    _dij(){}
+    _dij(ll n) { this->n = n; adj.resize(n + 1); }
+
+    void add(ll st, ll en, node c) { // 양방향
+        adj[st].push_back({ c,en });
+        adj[en].push_back({ c,st });
+    }
+    void addsol(ll st, ll en, node c) { // 단방향
+        adj[st].push_back({ c,en });
+    }
+
+    void init(ll st) {
+        d.clear(); pre.clear();
+        d.resize(n + 1, mx()); pre.resize(n + 1, -1); 
+        pq.push({ mn(), st });
+        d[st] = mn();
+
+        while (!pq.empty()) {
+            auto [cn, cur] = pq.top(); pq.pop();
+            if(cn > d[cur]) continue;
+            
+            for (auto& i : adj[cur]) {
+                auto [nn, nxt] = i;
+                node pl = nn + cn;
+        
+                if (d[nxt] <= pl) continue;
+                d[nxt] = pl; pre[nxt] = cur; 
+                pq.push({ pl, nxt });
+            }
+        }
+    }
+
+    node ret(ll n) { return d[n]; }
+};
+
+void run(){
+    cin >> n >> m; _dij dij(n);
+    for(int i = 1;i <= n;i++) {
+        cin >> st[i];
+        for(int j = 1;j < i;j++){  
+            ll cnt = 0;
+            for(int k = 0;k < m;k++) cnt += (st[i][k] != st[j][k]);
+            if(cnt == 1) dij.add(i, j, 1);
+        }
+    }
+
+    ll s, e; cin >> s >> e; dij.init(s); 
+    if(dij.ret(e) >= INF) { cout << -1; return; }
+    while(e != s){
+        result.push_back(e);
+        e = dij.pre[e];
+    }
+    result.push_back(s); reverse(all(result));
+    for(auto& i : result) cout << i << " ";
+}
+
+int main() {
+    fastio; // cin >> t;
+    while(t--) run(); 
+
+    return 0;
+}
+
