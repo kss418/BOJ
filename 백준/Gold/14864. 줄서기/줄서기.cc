8a667a5b00226522fd0d506fcb8ce4a84fbc902b
kss418 <108602943+kss418@users.noreply.github.com>
2025-01-27 18:27:00 +0900

    [Gold II] Title: 줄서기, Time: 0 ms, Memory: 7548 KB, Score: 28 point -BaekjoonHub

---
+++ 백준/Gold/14864. 줄서기/줄서기.cc
@@ -0,0 +1,100 @@
+#include <bits/stdc++.h>
+#include <ext/rope>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace __gnu_cxx;
+#define ordered_set tree<int, null_type, less<int>, rb_tree_tag,tree_order_statistics_node_update>
+#define fastio cin.tie(0), cout.tie(0), ios::sync_with_stdio(0);
+#define all(x) (x).begin(), (x).end()
+#define x first 
+#define y second
+using namespace std; typedef long long ll;
+using ld = long double; using pld = pair<ld, ld>;
+using ull = unsigned long long; 
+using pll = pair<ll, ll>; using tll = tuple<ll, ll, ll>;
+ll n, m, k, t = 1; string s;
+
+constexpr ll INF = 0x3f3f3f3f3f3f3f3f;
+constexpr ll MINF = 0xc0c0c0c0c0c0c0c0;
+constexpr ll MAX = 101010; // SET MAX SIZE
+constexpr ll MOD = 998244353;
+pll a[MAX];
+vector <ll> adj[MAX];
+ll result[MAX], ind[MAX];
+
+class _sseg {
+public:
+    ll n; vector <ll> seg;
+    _sseg(){}
+    _sseg(ll n) { this->n = n; seg.resize(4 * n + 1); }
+
+    ll query(ll st, ll en) { return query(st, en, 0, n); }
+    ll query(ll st, ll en, ll l, ll r, ll node = 1) {
+        if (en < l || st > r) return 0;
+        if (st <= l && en >= r) return seg[node];
+        ll mid = (l + r) >> 1;
+        return query(st, en, l, mid, node * 2) + query(st, en, mid + 1, r, node * 2 + 1);
+    }
+
+    void add(ll idx, ll val) { add(idx, val, 0, n); }
+    void add(ll idx, ll val, ll l, ll r, ll node = 1) {
+        if (idx < l || idx > r) return;
+        seg[node] += val;
+        if (l == r) return;
+        ll mid = (l + r) >> 1;
+        add(idx, val, l, mid, node * 2);
+        add(idx, val, mid + 1, r, node * 2 + 1);
+    }
+
+    ll update(ll idx, ll val) { return update(idx, val, 0, n); }
+    ll update(ll idx, ll val, ll l, ll r, ll node = 1) {
+        if (idx < l || idx > r) return seg[node];
+        if (l == r) return seg[node] = val;
+        ll mid = (l + r) >> 1;
+
+        seg[node] = update(idx, val, l, mid, node * 2);
+        seg[node] += update(idx, val, mid + 1, r, node * 2 + 1);
+        return seg[node];
+    }
+
+    ll cnt(ll val) { return cnt(val, 0, n); }
+    ll cnt(ll val, ll l, ll r, ll node = 1){
+        if(l == r) return r;
+        ll mid = (l + r) >> 1;
+
+        if(seg[node * 2] < val) return cnt(val - seg[node * 2], mid + 1, r, node * 2 + 1);
+        else return cnt(val, l, mid, node * 2);
+    }
+};
+
+void run(){
+    cin >> n >> m; _sseg seg(n);
+    for(int i = 1;i <= n;i++) seg.update(i, 1);
+    for(int i = 1;i <= m;i++) {
+        cin >> a[i].x >> a[i].y;
+        ind[a[i].x]++;
+    }
+
+    for(int i = 1;i <= n;i++){
+        ll q = seg.cnt(ind[i] + 1);
+        result[i] = q; seg.update(q, 0);
+    }
+
+    ll flag = 1;
+    for(int i = 1;i <= m;i++){
+        if(result[a[i].x] <= result[a[i].y]) flag = 0;
+    }
+
+    if(flag) for(int i = 1;i <= n;i++) cout << result[i] << " ";
+    else cout << -1;
+}
+
+
+int main() {
+    fastio; // cin >> t;
+    while(t--) run(); 
+
+    return 0;
+}
+
