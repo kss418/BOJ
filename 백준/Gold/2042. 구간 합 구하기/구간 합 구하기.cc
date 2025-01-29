3cb986c7a95a8848f7aaa418446013f9e414c0df
kss418 <108602943+kss418@users.noreply.github.com>
2025-01-29 20:20:45 +0900

    [Gold I] Title: 구간 합 구하기, Time: 208 ms, Memory: 41084 KB -BaekjoonHub

--- 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.cc
+++ 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.cc
@@ -1,96 +1,70 @@
-#include <bits/stdc++.h>
-#include <ext/rope>
-#include <ext/pb_ds/assoc_container.hpp>
-#include <ext/pb_ds/tree_policy.hpp>
-using namespace __gnu_pbds;
-using namespace __gnu_cxx;
-#define ordered_set tree<int, null_type, less<int>, rb_tree_tag,tree_order_statistics_node_update>
-#define fastio cin.tie(0), cout.tie(0), ios::sync_with_stdio(0);
-#define all(x) (x).begin(), (x).end()
-#define x first 
-#define y second
-using namespace std; typedef long long ll;
-using ld = long double;
-using ull = unsigned long long;
-using pll = pair<ll, ll>; using tll = tuple<ll, ll, ll>;
-ll n, m, k, t; string s;
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <queue>
+using namespace std;
+long long arr[1000003];
+long long seg[4000010];
+long long n, m, k, num, a, b;
 
-constexpr ll INF = 0x3f3f3f3f3f3f3f3f;
-constexpr ll MAX = 1010101;
-constexpr ll MOD = 998244353;
-ll a[MAX];
+long long mk_seg(int st, int en, int node) {
+	if (st == en) {
+		return seg[node] = arr[st];
+	}
+	int mid = (st + en) / 2;
+	seg[node] = mk_seg(st, mid, node * 2) + mk_seg(mid + 1, en, node * 2 + 1);
+	return seg[node];
+}
 
-template <typename T = ll> // query type
-class _seg { // 구간 예외 처리하기
-public:
-    ll n;
-    class node{
-    public:
-        ll v;
-        node() : node(0){}
-        node(ll v) {
-            this->v = v;
-        }
+long long query(int st, int en, int node, int left, int right) {
+	if (left > en || right < st) {
+		return 0;
+	}
 
-        node operator + (node ot){ // update
-            ll now = v + ot.v;
-            return node(now);
-        }
+	if (left <= st && en <= right) {
+		return seg[node];
+	}
+	int mid = (st + en) / 2;
+	return query(st, mid, node * 2, left, right) + query(mid + 1, en, node * 2 + 1, left, right);
+	
+}
 
-        operator T(){ // update -> query
-            return v;
-        }
+void update(int st, int en, int node, int index, long long value) {
+	if (index < st || index > en) {
+		return;
+	}
 
-        node operator * (node ot){ // query
-            ll ret = v + ot;
-            return ret;
-        }
-    };
-    vector <node> seg, arr;
-
-    _seg(ll n) {
-        this->n = n;
-        arr.resize(n + 1); seg.resize(4 * n + 1);
-    }
-
-    T query(ll st, ll en) { return query(st, en, 0, n); }
-    node query(ll st, ll en, ll l, ll r, ll node = 1) {
-        if (en < l || st > r) return 0;
-        if (st <= l && en >= r) return seg[node];
-        ll mid = (l + r) >> 1;
-        return query(st, en, l, mid, node * 2) * query(st, en, mid + 1, r, node * 2 + 1);
-    }
-
-    node update(ll idx, ll val) { return update(idx, val, 0, n); }
-    node update(ll idx, ll val, ll l, ll r, ll node = 1) {
-        if (idx < l || idx > r) return seg[node];
-        if (l == r) return seg[node] = val;
-        ll mid = (l + r) >> 1;
-
-        seg[node] = update(idx, val, l, mid, node * 2) + update(idx, val, mid + 1, r, node * 2 + 1);
-        return seg[node];
-    }
-};
+	seg[node] += value;
+	if (st == en) {
+		return;
+	}
+	int mid = (st + en) / 2;
+	update(st, mid, node * 2, index, value);
+	update(mid + 1, en, node * 2 + 1, index, value);
+}
 
 int main() {
-    fastio;
+	cin.tie(0);
+	cout.tie(0);
+	ios::sync_with_stdio(false);
 
-    cin >> n >> m >> k; _seg seg(n);
-    for(int i = 1;i <= n;i++) cin >> a[i], seg.update(i, a[i]);
-
-    for(int i = 1;i <= m + k;i++){
-        ll com; cin >> com;
-        if(com == 1){
-            ll idx, val; cin >> idx >> val;
-            seg.update(idx, val);
-        }
-        else{
-            ll l, r; cin >> l >> r;
-            cout << seg.query(l, r) << "\n";
-        }
-    }
-
-    
-    
-    return 0;
+	cin >> n >> m >> k;
+	for (int i = 1; i < n + 1; i++) {
+		cin >> arr[i];
+	}
+	mk_seg(1, n, 1);
+	
+	for (int i = 0; i < m + k; i++) {
+		cin >> num >> a >> b;
+		if (num == 1){
+			long long tmp = arr[a];
+			arr[a] = b;
+			update(1, n, 1, a, b - tmp);
+		}
+		else {
+			cout << query(1, n, 1, a, b) << "\n";
+		}
+	}
+	
+	return 0;
 }
