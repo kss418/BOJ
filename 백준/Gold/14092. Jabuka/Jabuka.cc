d6b44ea6d952ceb30952b6a274df6120a9e6f8ee
kss418 <108602943+kss418@users.noreply.github.com>
2025-02-01 03:21:13 +0900

    [Gold V] Title: Jabuka, Time: 48 ms, Memory: 6116 KB -BaekjoonHub

--- 백준/Gold/14092. Jabuka/Jabuka.cc
+++ 백준/Gold/14092. Jabuka/Jabuka.cc
@@ -1,83 +1,109 @@
-#include <bits/stdc++.h>
-#include <ext/rope>
-#include <ext/pb_ds/assoc_container.hpp>
-#include <ext/pb_ds/tree_policy.hpp>
-using namespace __gnu_pbds;
-using namespace __gnu_cxx;
-#define ordered_set tree<int, null_type, less<int>, rb_tree_tag,tree_order_statistics_node_update>
-#define fastio cin.tie(0), cout.tie(0), ios::sync_with_stdio(0);
-#define all(x) (x).begin(), (x).end()
-#define x first 
-#define y second
-using namespace std; typedef long long ll;
-using ld = long double; using pld = pair<ld, ld>;
-using ull = unsigned long long; 
-using pll = pair<ll, ll>; using tll = tuple<ll, ll, ll>;
-ll n, m, k, t = 1; string s;
+#include <cstdio>
+#include <iostream>
+#include <algorithm>
+#include <sstream>
+#include <vector>
+#include <string>
+#include <tuple>
+#include <cstring>
+#include <string>
+#include <stack>
+#include <queue>
+#include <utility>
+#include <cmath>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <deque>
+#include <iomanip>
+#include <limits>
+#include <numeric>
 
-constexpr ll INF = 0x3f3f3f3f3f3f3f3f;
-constexpr ll MINF = 0xc0c0c0c0c0c0c0c0;
-constexpr ll MAX = 1010; // SET MAX SIZE
-constexpr ll MOD = 998244353;
-ll dx[4] = {0, 1, 0, -1}, dy[4] = {1, 0, -1, 0}, dir;
-string st[MAX], tmp[MAX]; ll cy, cx;
+//#include "atcoder/segtree.hpp"
+//#include <atcoder/modint.hpp>
 
-void gravity(){
-    ll ny = cy + dy[dir], nx = cx + dx[dir];
-    if(ny < 0 || nx < 0 || ny >= n || nx >= m) return;
-    if(st[ny][nx] == '#') return;
-    st[cy][cx] = '.'; cy = ny; cx = nx;
-    st[cy][cx] = 'J';
-}
+#define LMAX 9223372036854775807
+#define IMAX 2147483647
+#define INF 2e9
+#define MOD 1000000007
+#define endl '\n'
+#define END "0"
+#define ll long long
+#define ull unsigned long long
+#define ld long double
+#define FASTIO ios_base :: sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+using namespace std;
+void kill() { cout << END; exit(0); }
+//using mint = atcoder::modint998244353;
 
-void rotate(){
-    for(int i = 0;i < n;i++){
-        for(int j = 0;j < m;j++) {
-            tmp[j][n - i - 1] = st[i][j];
+char arr[1001][1001];
+char temp[1001][1001];
+int n,m;
+int cnt=0;
+int x, y;
+
+void rotate() {
+    for(int i=1;i<=m;i++) {
+        for(int j=1;j<=n;j++) {
+            temp[i][j]=arr[n-j+1][i];
         }
     }
-    swap(n, m);
+    for(int i=1;i<=m;i++) {
+        for(int j=1;j<=n;j++) {
+            arr[i][j]=temp[i][j];
+        }
+    }
+    swap(n,m);
+}
 
-    for(int i = 0;i < n;i++){
-        for(int j = 0;j < m;j++) st[i][j] = tmp[i][j];
+void pri() {
+    for(int i=1;i<=n;i++) {
+        for(int j=1;j<=m;j++) {
+            cout<<arr[i][j];
+        } cout<<endl;
     }
 }
 
-void run(){
-    cin >> n >> m;
-
-    for(int i = 0;i < n;i++) {
-        cin >> st[i];
-        for(int j = 0;j < m;j++){
-            if(st[i][j] != 'J') continue;
-            cy = i; cx = j;
+void sol() {
+    if(cnt<0) cnt+=4;
+    if(cnt%4==0) {
+        if(x+1<=n && arr[x+1][y]=='.') {
+            arr[x][y] = '.'; arr[++x][y] = 'J';
         }
-    }
-    cin >> s;
-
-    for(int i = 0;i < max(n, m);i++) {
-        tmp[i].resize(max(n, m));
-        st[i].resize(max(n, m));
-    }
-
-    for(auto& i : s){
-        if(i == 'R') dir = (dir + 1) % 4;
-        else if(i == 'L') dir = (dir + 3) % 4;
-        gravity();
-    }
-
-    
-    for(int i = 0;i < dir;i++) rotate();
-    for(int i = 0;i < n;i++){
-        for(int j = 0;j < m;j++) cout << st[i][j];
-        cout << "\n";
+    } else if(cnt%4==1) {
+        if(y+1<=m && arr[x][y+1]=='.') {
+            arr[x][y] = '.'; arr[x][++y] = 'J';
+        }
+    } else if(cnt%4==2) {
+        if(x-1>=1 && arr[x-1][y]=='.') {
+            arr[x][y] = '.'; arr[--x][y] = 'J';
+        }
+    } else {
+        if(y-1>=1 && arr[x][y-1]=='.'){
+            arr[x][y] = '.'; arr[x][--y] = 'J';
+        }
     }
 }
 
 int main() {
-    fastio; // cin >> t;
-    while(t--) run(); 
+    FASTIO
+    cin>>n>>m;
+    for(int i=1;i<=n;i++) {
+        for(int j=1;j<=m;j++) {
+            cin>>arr[i][j];
+            if(arr[i][j]=='J') x=i,y=j;
+        }
+    }
+    string str; cin>>str;
+    for(auto f:str) {
+        if(f=='R') cnt++;
+        else if(f=='L') cnt--;
+        sol();
+    }
+    
+    for(int d=1;d<=cnt%4;d++) rotate();
+    pri();
+
 
     return 0;
 }
-
