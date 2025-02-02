8709522dc33cf2c293f9269502343c8c590d0f2d
kss418 <108602943+kss418@users.noreply.github.com>
2025-02-02 19:10:53 +0900

    [Gold IV] Title: Fence Repair, Time: 4 ms, Memory: 4104 KB -BaekjoonHub

---
+++ 백준/Gold/6195. Fence Repair/Fence Repair.cc
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+#include <ext/rope>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace __gnu_cxx;
+#define ordered_set tree<int, null_type, less<int>, rb_tree_tag,tree_order_statistics_node_update>
+#define fastio cin.tie(0), cout.tie(0), ios::sync_with_stdio(0);
+#define all(x) (x).begin(), (x).end()
+#define x first 
+#define y second
+using namespace std; typedef long long ll;
+using ld = long double; using pld = pair<ld, ld>;
+using ull = unsigned long long; 
+using pll = pair<ll, ll>; using tll = tuple<ll, ll, ll>;
+ll n, m, k, t = 1; string s;
+
+constexpr ll INF = 0x3f3f3f3f3f3f3f3f;
+constexpr ll MINF = 0xc0c0c0c0c0c0c0c0;
+constexpr ll MAX = 201010; // SET MAX SIZE
+constexpr ll MOD = 998244353;
+ll a[MAX], result;
+priority_queue <ll, vector<ll>, greater<ll>> pq;
+
+void run(){
+    cin >> n;
+    for(int i = 1;i <= n;i++) cin >> a[i], pq.push(a[i]);
+    while(pq.size() > 1){
+        ll fi = pq.top(); pq.pop();
+        ll se = pq.top(); pq.pop();
+        result += fi + se;
+        pq.push(fi + se);
+    }
+
+    cout << result;
+}
+
+int main() {
+    fastio; // cin >> t;
+    while(t--) run(); 
+
+    return 0;
+}
+
