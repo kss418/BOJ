dd93210b136e7b0322e54c0dedaefad24ac8115f
kss418 <108602943+kss418@users.noreply.github.com>
2025-02-04 17:12:31 +0900

    [Gold I] Title: 깊콘이 넘쳐흘러, Time: 28 ms, Memory: 3600 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Gold/17420. 깊콘이 넘쳐흘러/깊콘이 넘쳐흘러.cc
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+#include <ext/rope>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace __gnu_cxx;
+#define ordered_set tree<int, null_type, less<int>, rb_tree_tag,tree_order_statistics_node_update>
+#define fastio cin.tie(0), cout.tie(0), ios::sync_with_stdio(0);
+#define all(x) (x).begin(), (x).end()
+#define x first 
+#define y second
+using namespace std; typedef long long ll;
+using ld = long double; using pld = pair<ld, ld>;
+using ull = unsigned long long; 
+using pll = pair<ll, ll>; using tll = tuple<ll, ll, ll>;
+ll n, m, k, t = 1; string s;
+
+constexpr ll INF = 0x3f3f3f3f3f3f3f3f;
+constexpr ll MINF = 0xc0c0c0c0c0c0c0c0;
+constexpr ll MAX = 101010; // SET MAX SIZE
+constexpr ll MOD = 998244353;
+pll a[MAX];
+
+void run(){
+    cin >> n;
+    for(int i = 1;i <= n;i++) cin >> a[i].x;
+    for(int i = 1;i <= n;i++) cin >> a[i].y;
+
+    sort(a + 1, a + n + 1, [](pll& a, pll& b){
+        if(a.y != b.y) return a.y < b.y;
+        return a.x < b.x;
+    });
+    
+    ll mx = a[1].x, la = a[1].x, result = 0;
+    for(int i = 1;i <= n;i++){
+        if(a[i].y != a[i - 1].y) mx = max(mx, la);
+
+        if(a[i].x < mx){
+            ll cnt = (mx - a[i].x) / 30;
+            if((mx - a[i].x) % 30) cnt++;
+            result += cnt; a[i].x += 30 * cnt;
+        }
+
+        if(a[i].y > a[i].x){
+            ll cnt = (a[i].y - a[i].x) / 30;
+            if((a[i].y - a[i].x) % 30) cnt++;
+            result += cnt; a[i].x += 30 * cnt;
+        }
+
+        la = max(la, a[i].x);
+    }
+
+    cout << result;
+}
+
+int main() {
+    fastio; // cin >> t;
+    while(t--) run(); 
+
+    return 0;
+}
+
