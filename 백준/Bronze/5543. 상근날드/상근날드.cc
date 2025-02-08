0da3ccd116509b55e2272611aa6f9eb55605b378
kss418 <108602943+kss418@users.noreply.github.com>
2025-02-08 23:00:35 +0900

    [Bronze IV] Title: 상근날드, Time: 0 ms, Memory: 3592 KB -BaekjoonHub

---
+++ 백준/Bronze/5543. 상근날드/상근날드.cc
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+#include <ext/rope>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace __gnu_cxx;
+#define ordered_set tree<int, null_type, less<int>, rb_tree_tag,tree_order_statistics_node_update>
+#define fastio cin.tie(0), cout.tie(0), ios::sync_with_stdio(0);
+#define all(x) (x).begin(), (x).end()
+#define x first 
+#define y second
+using namespace std; typedef long long ll;
+using ld = long double; using pld = pair<ld, ld>;
+using ull = unsigned long long; 
+using pll = pair<ll, ll>; using tll = tuple<ll, ll, ll>;
+ll n, m, k, t = 1; string s;
+
+constexpr ll INF = 0x3f3f3f3f3f3f3f3f;
+constexpr ll MINF = 0xc0c0c0c0c0c0c0c0;
+constexpr ll MAX = 201010; // SET MAX SIZE
+constexpr ll MOD = 998244353;
+ll a[MAX];
+
+void run(){
+    for(int i = 1;i <= 5;i++) cin >> a[i];
+    ll result = INF;
+    for(int i = 1;i <= 3;i++){
+        for(int j = 4;j <= 5;j++) result = min(result, a[i] + a[j]);
+    }
+
+    cout << result - 50;
+}
+
+int main() {
+    fastio; // cin >> t;
+    while(t--) run(); 
+
+    return 0;
+}
+
