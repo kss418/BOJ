5e9d27523787c8645d4e10ee3253a8ff7a12a7de
kss418 <108602943+kss418@users.noreply.github.com>
2025-02-10 21:31:11 +0900

    [Platinum I] Title: 큰 수 곱셈 (2), Time: 220 ms, Memory: 60508 KB -BaekjoonHub

--- 백준/Platinum/15576. 큰 수 곱셈 （2）/큰 수 곱셈 （2）.cc
+++ 백준/Platinum/15576. 큰 수 곱셈 （2）/큰 수 곱셈 （2）.cc
@@ -1,103 +1,97 @@
 #include <bits/stdc++.h>
-#include <ext/rope>
-#include <ext/pb_ds/assoc_container.hpp>
-#include <ext/pb_ds/tree_policy.hpp>
-using namespace __gnu_pbds;
-using namespace __gnu_cxx;
-#define ordered_set tree<int, null_type, less<int>, rb_tree_tag,tree_order_statistics_node_update>
-#define fastio cin.tie(0), cout.tie(0), ios::sync_with_stdio(0);
-#define all(x) (x).begin(), (x).end()
-#define x first 
-#define y second
-using namespace std; typedef long long ll;
-using ld = long double; using pld = pair<ld, ld>;
-using ull = unsigned long long; 
-using pll = pair<ll, ll>; using tll = tuple<ll, ll, ll>;
-ll n, m, k, t = 1; string s;
+#define int long long
+using namespace std;
 
-constexpr ll INF = 0x3f3f3f3f3f3f3f3f;
-constexpr ll MINF = 0xc0c0c0c0c0c0c0c0;
-constexpr ll MAX = 201010; // SET MAX SIZE
-constexpr ll MOD = 998244353;
-vector <ll> va, vb;
+const double PI = acos(-1);
+typedef complex<double> cpx;
 
-class _fft{
-public:
-    const ld PI = acos(-1);
-    using cpd = complex <ld>;
+void FFT(vector<cpx> &v, bool inv) {
+    int S = v.size();
 
-    void fft(vector <cpd>& v, bool inv = 0){
-        ll n = v.size();
-        for(int i = 1,j = 0;i < n;i++){
-            ll bit = n >> 1ll;
-            for(;j >= bit;bit >>= 1ll) j -= bit;
-            j += bit;
-            if(i < j) swap(v[i], v[j]);
+    for(int i=1, j=0; i<S; i++) {
+        int bit = S/2;
+
+        while(j >= bit) {
+            j -= bit;
+            bit /= 2;
         }
+        j += bit;
 
-        for(int k = 1;k < n;k <<= 1ll){
-            ld a = PI / k * (inv ? 1 : -1);
-            cpd w(cos(a), sin(a));
-            for(int i = 0;i < n;i += 2 * k){
-                cpd wp(1, 0);
-                for(int j = 0;j < k;j++){
-                    cpd x = v[i + j], y = v[i + j + k] * wp;
-                    v[i + j] = x + y;
-                    v[i + j + k] = x - y;
-                    wp *= w;
-                }
+        if(i < j) swap(v[i], v[j]);
+    }
+
+    for(int k=1; k<S; k*=2) {
+        double angle = (inv ? PI/k : -PI/k);
+        cpx dir(cos(angle), sin(angle));
+
+        for(int i=0; i<S; i+=k*2) {
+            cpx unit(1, 0);
+
+            for(int j=0; j<k; j++) {
+                cpx a = v[i+j];
+                cpx b = v[i+j+k] * unit;
+
+                v[i+j] = a + b;
+                v[i+j+k] = a - b;
+
+                unit *= dir;
             }
         }
-
-        if(inv) for(int i = 0;i < n;i++) v[i] /= n;
-    }  
-        
-    vector <ll> mul(vector <ll>& a, vector <ll>& b){
-        vector<cpd> av(all(a)), bv(all(b));
-        ll n = 2;
-        while(n < a.size() + b.size()) n <<= 1;
-        av.resize(n); bv.resize(n);
-        fft(av); fft(bv);
-        for(int i = 0;i < n;i++) av[i] *= bv[i];
-        fft(av, 1);
-
-        vector <ll> ret(n);
-        for(int i = 0;i < n;i++) ret[i] = round(av[i].real());
-    
-        return ret;
     }
-        
-    vector <ll> carry(vector <ll>& a, vector <ll>& b){
-        reverse(all(a)); reverse(all(b));
-        vector <ll> ret = mul(a, b);
 
-        for(int i = 0;i < ret.size();i++){
-            if(ret[i] < 10) continue;
-            if(i < ret.size() - 1) ret[i + 1] += ret[i] / 10;
-            else ret.push_back(ret[i] / 10);
-            ret[i] %= 10;
-        }
-        
-        while(!ret.empty() && !ret.back()) ret.pop_back();
-        if(ret.empty()) return vector<ll>(1, 0);
-        reverse(all(ret));
-        return ret;
-    }
-};
-
-void run(){
-    string a, b; cin >> a >> b; _fft fft;
-    for(auto& i : a) va.push_back(i - '0');
-    for(auto& i : b) vb.push_back(i - '0');
-    vector <ll> ret = fft.carry(va, vb);
-
-    for(auto& i : ret) cout << i;
+    if(inv)
+        for(int i=0; i<S; i++) v[i] /= S;
 }
 
-int main() {
-    fastio; // cin >> t;
-    while(t--) run(); 
+vector<cpx> mul(vector<cpx> &v, vector<cpx> &u) {
+    int S = 2;
+    while(S < v.size() + u.size()) S *= 2;
 
-    return 0;
+    v.resize(S); FFT(v, false);
+    u.resize(S); FFT(u, false);
+
+    vector<cpx> w(S);
+    for(int i=0; i<S; i++) w[i] = v[i] * u[i];
+    FFT(w, true);
+
+    return w;
 }
 
+main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    string a, b; cin >> a >> b;
+
+    vector<cpx> v, u;
+    for(int i=0; i<a.length(); i++) v.push_back(cpx(a[i] - '0', 0));
+    for(int i=0; i<b.length(); i++) u.push_back(cpx(b[i] - '0', 0));
+
+    reverse(v.begin(), v.end());
+    reverse(u.begin(), u.end());
+
+    vector<cpx> w = mul(v, u);
+
+    vector<int> ans(w.size());
+    for(int i=0; i<ans.size(); i++) ans[i] = round(w[i].real());
+
+    for(int i=0; i<ans.size(); i++) {
+        if(ans[i] < 10) continue;
+
+        if(i < ans.size()-1) ans[i+1] += ans[i]/10;
+        else ans.push_back(ans[i]/10);
+
+        ans[i] %= 10;
+    }
+
+    reverse(ans.begin(), ans.end());
+
+    int i=0; while(ans[i] == 0) i++;
+    if(i >= ans.size()) cout << 0 << "\n";
+
+    while(i < ans.size()) {
+        cout << ans[i];
+        i++;
+    }
+    cout << "\n";
+}
