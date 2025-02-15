710aba628686ac7d3e9c6131c5b33e7198371d1a
kss418 <108602943+kss418@users.noreply.github.com>
2025-02-15 11:48:16 +0900

    [Platinum II] Title: 순서 섞기, Time: 0 ms, Memory: 3592 KB, Score: 4 point -BaekjoonHub

---
+++ 백준/Platinum/20192. 순서 섞기/순서 섞기.cc
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+#include <ext/rope>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace __gnu_cxx;
+#define ordered_set tree<int, null_type, less<int>, rb_tree_tag,tree_order_statistics_node_update>
+#define fastio cin.tie(0), cout.tie(0), ios::sync_with_stdio(0);
+#define all(x) (x).begin(), (x).end()
+#define x first 
+#define y second
+using namespace std; typedef long long ll;
+using ld = long double; using pld = pair<ld, ld>;
+using ull = unsigned long long; 
+using pll = pair<ll, ll>; using tll = tuple<ll, ll, ll>;
+ll n, m, k, t = 1; string s;
+
+constexpr ll INF = 0x3f3f3f3f3f3f3f3f;
+constexpr ll MINF = 0xc0c0c0c0c0c0c0c0;
+constexpr ll MAX = 201010; // SET MAX SIZE
+constexpr ll MOD = 998244353;
+ll a[MAX], cnt, result;
+
+void run(){
+    cin >> n; ll up = 1; a[0] = MINF;
+    for(int i = 1;i <= n;i++) cin >> a[i];
+
+    for(int i = 1;i <= n;i++){
+        if(a[i] > a[i - 1] && !up) up = 1;
+        else if(a[i] < a[i - 1] && up) up = 0, cnt++;
+    }
+
+    if(cnt && up) cnt++;
+    while(cnt){
+        cnt = (cnt >> 1ll) + (cnt % 2 && cnt != 1);
+        result++;
+    }
+
+    cout << result;
+}
+
+int main() {
+    fastio; // cin >> t;
+    while(t--) run(); 
+
+    return 0;
+}
+
