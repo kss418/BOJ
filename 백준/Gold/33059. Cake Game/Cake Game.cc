a0c885ce0f6f3e83de5b6bb0f27ff46ba1c3d4ad
kss418 <108602943+kss418@users.noreply.github.com>
2025-02-23 14:09:24 +0900

    [Gold II] Title: Cake Game, Time: 124 ms, Memory: 9848 KB -BaekjoonHub

---
+++ 백준/Gold/33059. Cake Game/Cake Game.cc
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+#include <ext/rope>
+#define fastio cin.tie(0), cout.tie(0), ios::sync_with_stdio(0);
+#define all(x) (x).begin(), (x).end()
+#define x first 
+#define y second
+using namespace std; using ll = long long;
+using ld = long double; using pld = pair<ld, ld>;
+using i128 = __int128_t; using f128 = __float128; 
+using pll = pair<ll, ll>; using tll = tuple<ll, ll, ll>;
+ll n, m, k, t = 1; string s;
+
+constexpr ll INF = 0x3f3f3f3f3f3f3f3f;
+constexpr ll MINF = 0xc0c0c0c0c0c0c0c0;
+constexpr ll MAX = 501010; // SET MAX SIZE
+constexpr ll MOD = 998244353;
+ll a[MAX], sum[MAX];
+
+void run(){
+    cin >> n; ll all = 0;
+    for(int i = 1;i <= n;i++) {
+        cin >> a[i], all += a[i];
+        sum[i] = sum[i - 1] + a[i];
+    }   
+    ll diff = n / 2 + 1, result = 0;
+
+    for(int i = 0;i <= n;i++){
+        if(i + diff > n) break;
+        ll now = sum[i] + sum[n] - sum[i + diff];
+        result = max(result, now);
+    }
+
+    cout << all - result << " " << result << "\n"; 
+}
+
+int main() {
+    fastio; cin >> t;
+    while(t--) run(); 
+
+    return 0;
+}
+
