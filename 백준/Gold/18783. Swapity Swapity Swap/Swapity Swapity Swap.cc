644864dba34a7ea82f3934bca1ff15cb097e1a1d
kss418 <108602943+kss418@users.noreply.github.com>
2025-03-20 15:12:18 +0900

    [Gold II] Title: Swapity Swapity Swap, Time: 128 ms, Memory: 28696 KB -BaekjoonHub

---
+++ 백준/Gold/18783. Swapity Swapity Swap/Swapity Swapity Swap.cc
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+#include <ext/rope>
+#define fastio cin.tie(0), cout.tie(0), ios::sync_with_stdio(0);
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define x first 
+#define y second
+using namespace std; using ll = long long;
+using ld = long double; using pld = pair<ld, ld>;
+using i128 = __int128_t; using f128 = __float128; 
+using pll = pair<ll, ll>; using tll = tuple<ll, ll, ll>;
+ll n, m, k, t = 1; string s;
+
+constexpr ll INF = 0x3f3f3f3f3f3f3f3f;
+constexpr ll MINF = 0xc0c0c0c0c0c0c0c0;
+constexpr ll MAX = 201010; // SET MAX SIZE
+constexpr ll MOD = 998244353;
+ll a[MAX];
+
+class _st { // 1-base index
+public:
+    ll n, m; vector <vector<ll>> num;
+    _st(ll n, ll m) { //n 개수 m 깊이
+        this->n = n; this->m = m;
+        num.resize(log2(m + 1) + 1, vector<ll>(n + 1, 0));
+    }
+
+    void add(ll st, ll en) {
+        num[0][st] = en;
+    }
+
+    void init() {
+        for (int i = 1; i <= log2(m + 1); i++) {
+            for (int j = 1; j <= n; j++) num[i][j] = num[i - 1][num[i - 1][j]];
+        }
+    }
+
+    ll ret(ll n, ll d) { //n 노드 d 깊이
+        for (int i = log2(m + 1); i >= 0; i--) {
+            if (d & (1ll << i)) n = num[i][n];
+        }
+
+        return n;
+    }
+};
+
+void run(){
+    cin >> n >> m >> k;
+    vector <ll> arr(n);
+    iota(all(arr), 1);
+
+    while(m--){
+        ll s, e; cin >> s >> e; s--; e--;
+        reverse(arr.begin() + s, arr.begin() + e + 1);
+    }
+
+    _st st(n, k);
+    for(int i = 1;i <= n;i++) st.add(i, arr[i - 1]);
+    st.init();
+
+    for(int i = 1;i <= n;i++) cout << st.ret(i, k) << "\n";
+}
+
+int main() {
+    fastio; // cin >> t;
+    while(t--) run();
+    
+    return 0;
+}
