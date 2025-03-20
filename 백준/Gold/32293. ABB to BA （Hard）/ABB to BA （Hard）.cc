c4287c0505e84e1b3c99baff6e052f01048bb97d
kss418 <108602943+kss418@users.noreply.github.com>
2025-03-20 10:46:10 +0900

    [Gold III] Title: ABB to BA (Hard), Time: 20 ms, Memory: 3688 KB -BaekjoonHub

--- 백준/Gold/32293. ABB to BA （Hard）/ABB to BA （Hard）.cc
+++ 백준/Gold/32293. ABB to BA （Hard）/ABB to BA （Hard）.cc
@@ -1,53 +1,41 @@
-#include <bits/stdc++.h>
-#include <ext/pb_ds/assoc_container.hpp>
-#include <ext/pb_ds/tree_policy.hpp>
-using namespace __gnu_pbds;
-#define ordered_set tree<int, null_type, less<int>, rb_tree_tag,tree_order_statistics_node_update>
+#include <iostream>
+#include <vector>
+using namespace std;
 #define fastio cin.tie(0), cout.tie(0), ios::sync_with_stdio(0);
-#define all(x) (x).begin(), (x).end()
-#define x first 
-#define y second
-using namespace std; typedef long long ll;
-using ld = long double;
-using ull = unsigned long long;
-using pll = pair<ll, ll>; using tll = tuple<ll, ll, ll>;
-ll n, m, k, t; string s;
 
-constexpr ll INF = 0x3f3f3f3f3f3f3f3f;
-constexpr ll MAX = 201010;
-constexpr ll MOD = 998244353;
 vector <char> st;
-
 bool chk(){
-    ll si = st.size();
-    if(si < 3) return 0;
-    if(st[si - 1] == 'A') return 0;
-    if(st[si - 2] == 'A') return 0;
-    if(st[si - 3] == 'B') return 0;
+    int sz = st.size();
+    if(sz < 3) return 0;
+    if(st[sz - 1] != 'B') return 0;
+    if(st[sz - 2] != 'B') return 0;
+    if(st[sz - 3] != 'A') return 0;
 
     return 1;
 }
 
-int main() {
+int main(){
     fastio;
 
-    cin >> t;
+    int t; cin >> t;
     while(t--){
-        cin >> n >> s; st.clear();
-        for(auto& i : s){
-            st.push_back(i); ll cnt = 0;
+        int n; string s;
+        cin >> n >> s;
+        st.clear();
+
+        for(int i = 0;i < n;i++){
+            st.push_back(s[i]);
+            int cnt = 0;
+
             while(chk()){
-                st.pop_back(); st.pop_back(); st.pop_back();
+                for(int i = 0;i < 3;i++) st.pop_back();
                 cnt++; st.push_back('B');
             }
 
             for(int j = 1;j <= cnt;j++) st.push_back('A');
         }
-        
-        for(auto& i : st) cout << i; cout << "\n";
+
+        for(int i = 0;i < st.size();i++) cout << st[i];
+        cout << "\n";
     }
-    
-
-    return 0;
-}
-
+}  
