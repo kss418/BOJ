7dc42e176ac3e5912ebd170af877cffad535fedc
kss418 <108602943+kss418@users.noreply.github.com>
2025-03-20 22:46:49 +0900

    [Gold V] Title: 괄호의 값, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/2504. 괄호의 값/괄호의 값.cc
+++ 백준/Gold/2504. 괄호의 값/괄호의 값.cc
@@ -1,69 +1,47 @@
-#include <bits/stdc++.h>
-#include <ext/pb_ds/assoc_container.hpp>
-#include <ext/pb_ds/tree_policy.hpp>
-using namespace __gnu_pbds;
-#define ordered_set tree<int, null_type, less<int>, rb_tree_tag,tree_order_statistics_node_update>
+#include <iostream>
+#include <stack>
 #define fastio cin.tie(0), cout.tie(0), ios::sync_with_stdio(0);
-#define all(x) (x).begin(), (x).end()
-#define x first 
-#define y second
-using namespace std; typedef long long ll;
-using ull = unsigned long long;
-using pll = pair<ll, ll>; using tll = tuple<ll, ll, ll>;
-ll n, m, k, t; string s;
+using namespace std;
 
-constexpr ll INF = 0x3f3f3f3f3f3f3f3f;
-constexpr ll MAX = 31;
-constexpr ll MOD = 998244353;
-ll result[MAX];
-stack <ll> st;
 
+stack <char> st;
 bool chk(string& s){
-    ll cnt = 0;
-    for(auto& i : s){
-        if(i == '(' || i == '[') cnt++, st.push(i);
-        else {
-            cnt--; if(cnt < 0) return 0;
-            if(i == ')'){
-                if(st.top() == '[') return 0;
-                if(st.top() == '(') st.pop();
-            }
-            else{
-                if(st.top() == '(') return 0;
-                if(st.top() == '[') st.pop();
-            }
+    for(int i = 0;i < s.size();i++){
+        if(s[i] == '(' || s[i] == '[') st.push(s[i]);
+        else{
+            if(st.empty()) return 0;
+            if(s[i] == ')' && st.top() == '[') return 0;
+            else if(s[i] == ']' && st.top() == '(') return 0;
+            st.pop();
         }
     }
 
-    return !cnt;
+    return 1;
 }
 
-int main() {
+int result[31];
+int main(){
     fastio;
-
-    cin >> s;
-    ll cnt = 0, flag = 1;
-
+    string s; cin >> s;
     if(!chk(s)){
         cout << 0; return 0;
     }
 
+    int cnt = 0;
     for(int i = 0;i < s.size();i++){
         if(s[i] == '(' || s[i] == '[') cnt++;
-        else if(s[i] == ')') {
+        else if(s[i] == ')'){
             cnt--;
-            result[cnt] += 2 * max(result[cnt + 1], 1ll);
+            result[cnt] += 2 * max(result[cnt + 1], 1);
             result[cnt + 1] = 0;
         }
         else{
             cnt--;
-            result[cnt] += 3 * max(result[cnt + 1], 1ll);
+            result[cnt] += 3 * max(result[cnt + 1], 1);
             result[cnt + 1] = 0;
         }
     }
 
     cout << result[0];
-    
-
     return 0;
 }
