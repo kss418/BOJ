c3ed3413711b1ad6fe0da840a44228af7a8929b8
kss418 <108602943+kss418@users.noreply.github.com>
2025-03-22 13:07:16 +0900

    [Gold IV] Title: 휴게소 세우기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/1477. 휴게소 세우기/휴게소 세우기.cc
+++ 백준/Gold/1477. 휴게소 세우기/휴게소 세우기.cc
@@ -1,66 +1,44 @@
-#include <bits/stdc++.h>
-#include <ext/rope>
-#include <ext/pb_ds/assoc_container.hpp>
-#include <ext/pb_ds/tree_policy.hpp>
-using namespace __gnu_pbds;
-using namespace __gnu_cxx;
-#define ordered_set tree<int, null_type, less<int>, rb_tree_tag,tree_order_statistics_node_update>
-#define fastio cin.tie(0), cout.tie(0), ios::sync_with_stdio(0);
-#define all(x) (x).begin(), (x).end()
-#define x first 
-#define y second
-using namespace std; typedef long long ll;
-using ld = long double; using pld = pair<ld, ld>;
-using ull = unsigned long long; 
-using pll = pair<ll, ll>; using tll = tuple<ll, ll, ll>;
-ll n, m, k, t = 1; string s;
+#include <iostream>
+#include <algorithm>
+using namespace std;
 
-constexpr ll INF = 0x3f3f3f3f3f3f3f3f;
-constexpr ll MAX = 201010; // SET MAX SIZE
-constexpr ll MOD = 998244353;
-ll a[MAX];
+int a[1010], n, m, l;
 
-template <typename T = ll>
-class _bs { // 0-based index
-public:
-    T l, r;
-    _bs(T l, T r) {
-        this->l = l; this->r = r;
+bool decision(int cur){
+    int cnt = 0;
+    for(int i = 1;i <= n + 1;i++){
+        int diff = a[i] - a[i - 1];
+        cnt += diff / cur;
+        if(diff % cur == 0) cnt--;
     }
 
-    bool f(T cur) {
-        ll ret = 0;
-        for(int i = 1;i <= n + 1;i++){
-            ret += (a[i] - a[i - 1]) / cur - 1;
-            ret += (a[i] - a[i - 1]) % cur ? 1 : 0;
-        }
-
-        return ret > m;
-    }
-
-    T ret() {
-        T lo = l, hi = r, mid = -1;
-        while (hi > lo) {
-            mid = (hi + lo) / (T)2;
-            if (f(mid)) lo = mid + 1;
-            else hi = mid;
-        }
-
-        return lo;
-    }
-};
-
-void run(){
-    cin >> n >> m >> k; _bs bs(1ll, k);
-    for(int i = 1;i <= n;i++) cin >> a[i]; a[n + 1] = k;
-    sort(a + 1, a + n + 1);
-
-    cout << bs.ret();
+    return cnt <= m;
 }
 
-int main() {
-    fastio; // cin >> t;
-    while(t--) run(); 
+int minimazation(){
+    // 정답의 범위는 1 ~ l
+    int lo = 1, hi = l;
+    while(lo < hi){
+        int mid = (lo + hi) / 2; // 중간값
+        // 결정 문제의 답이 1 이면 
+        // 정답은 [lo, mid]에 존재
+        if(decision(mid)) hi = mid;
+        
+        // 결정 문제의 답이 0 이면
+        // 정답은 [mid + 1, hi]에 존재
+        else lo = mid + 1;
+    }
+
+    return lo;
+}
+
+int main(){
+    cin >> n >> m >> l;
+    for(int i = 1;i <= n;i++) cin >> a[i];
+    a[0] = 0; a[n + 1] = l;
+    sort(a, a + n + 2);
+
+    cout << minimazation();
 
     return 0;
 }
