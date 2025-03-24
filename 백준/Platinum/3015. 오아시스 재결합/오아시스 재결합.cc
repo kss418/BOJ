4cdbfbca43eec22acba6bf0b574eb8f9aed01cd3
kss418 <108602943+kss418@users.noreply.github.com>
2025-03-24 18:39:14 +0900

    [Platinum V] Title: 오아시스 재결합, Time: 68 ms, Memory: 8256 KB -BaekjoonHub

--- 백준/Platinum/3015. 오아시스 재결합/오아시스 재결합.cc
+++ 백준/Platinum/3015. 오아시스 재결합/오아시스 재결합.cc
@@ -1,79 +1,33 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
 #include <stack>
+#define fastio cin.tie(0), cout.tie(0), ios::sync_with_stdio(0);
 using namespace std;
-typedef long long ll;
-stack <pair<ll, int>> list;
-ll arr[500001];
-ll result = 0;
 
 
+stack <pair <int, int>> st;
+int a[501010];
 
-int main() {
-	cin.tie(0);
-	cout.tie(0);
-	ios_base::sync_with_stdio(false);
+int main(){
+    fastio;
+    int n; cin >> n;
+    long long result = 0;
+    for(int i = 1;i <= n;i++) cin >> a[i];
 
-	int n;
-	cin >> n;
-	for (int i = 0; i < n; i++) {
-		cin >> arr[i];
-	}
-	
+    for(int i = 1;i <= n;i++){
+        while(!st.empty() && st.top().first < a[i]){
+            result += st.top().second; st.pop();
+        }
 
-	for (int i = 0; i < n; i++) {
-		if (list.empty()) {
-			list.push({ arr[i],1 });
-		}
-		else {
-			if (list.top().first < arr[i]) {
-				while (!list.empty() && list.top().first < arr[i]) {
-					result += list.top().second;
-					list.pop();
-				}
+        if(st.empty() || st.top().first != a[i]) st.push({a[i], 1});
+        else{
+            int num = st.top().second + 1;
+            st.pop(); st.push({a[i], num});
+            result += num - 1;
+        }
+        
+        if(st.size() >= 2) result++;
+    }
 
-				if (!list.empty() && list.top().first > arr[i]) {
-					result++;
-				}
-
-				if (!list.empty() && list.top().first == arr[i]) {
-					int cur = list.top().second;
-					list.pop();
-					if (!list.empty() && list.top().first > arr[i]) {
-						result++;
-					}
-					list.push({ arr[i], cur + 1 });
-					result += cur;
-				}
-				else {
-					list.push({ arr[i],1 });
-				}
-			}
-
-			else if (list.top().first > arr[i]) {
-				list.push({ arr[i],1 });
-				result++;
-			}
-
-			else {
-				int ord = list.top().second;
-				list.pop();
-				if (!list.empty() && list.top().first > arr[i]) {
-					result += ord + 1;
-				}
-				else if (!list.empty() && list.top().first < arr[i]) {
-					result += ord;
-				}
-				else {
-					result += ord;
-				}
-				list.push({ arr[i] , ord + 1 });
-			}
-		}
-	}
-
-	cout << result;
-
-	return 0;
+    cout << result;
+    return 0;
 }
