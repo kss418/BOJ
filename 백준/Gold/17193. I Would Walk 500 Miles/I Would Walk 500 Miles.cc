9bb7105772e0d1061e8384cfe493deee1c8f4194
kss418 <108602943+kss418@users.noreply.github.com>
2025-03-26 18:12:06 +0900

    [Gold II] Title: I Would Walk 500 Miles, Time: 0 ms, Memory: 2080 KB -BaekjoonHub

---
+++ 백준/Gold/17193. I Would Walk 500 Miles/I Would Walk 500 Miles.cc
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+#include <ext/rope>
+#define fastio cin.tie(0), cout.tie(0), ios::sync_with_stdio(0);
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define x first 
+#define y second
+using namespace std; using ll = long long;
+using ld = long double; using pld = pair<ld, ld>;
+using i128 = __int128_t; using f128 = __float128; 
+using pll = pair<ll, ll>; using tll = tuple<ll, ll, ll>;
+ll n, m, k, t = 1; string s;
+
+constexpr ll INF = 0x3f3f3f3f3f3f3f3f;
+constexpr ll MINF = 0xc0c0c0c0c0c0c0c0;
+constexpr ll MAX = 7575; // SET MAX SIZE
+constexpr ll MOD = 998244353;
+ll a[MAX];
+
+void run(){
+    cin >> n >> m;
+    ll result = 2019201997;
+    result -= 84 * (m - 1);
+    result -= 48 * n;
+    cout << result;
+}
+
+int main() {
+    fastio; // cin >> t;
+    while(t--) run();
+    
+    return 0;
+}
