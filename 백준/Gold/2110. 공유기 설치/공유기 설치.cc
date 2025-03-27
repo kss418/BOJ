5e6a1d313e82cdd93623e891836dae69471bd418
kss418 <108602943+kss418@users.noreply.github.com>
2025-03-27 23:16:59 +0900

    [Gold IV] Title: 공유기 설치, Time: 28 ms, Memory: 3592 KB -BaekjoonHub

--- 백준/Gold/2110. 공유기 설치/공유기 설치.cc
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.cc
@@ -1,57 +1,44 @@
 #include <iostream>
 #include <algorithm>
-#include <vector>
-typedef long long ll;
 using namespace std;
-int n, m;
-vector <int> arr;
+using ll = long long;
 
+const ll MAX = 201010;
+ll a[MAX], n, m;
 
-bool dm(int cur) {
-    int cnt = 1;
-    int pre = 0;
-    for (int i = 1; i < arr.size(); i++) {
-        if (arr[i] - arr[pre] >= cur) {
-            cnt++;
-            pre = i;
-        }
-
-        if (cnt == m) {
-            return 1;
-        }
+bool decision(ll cur){
+    ll cnt = 0, last = -1e12;
+    for(int i = 1;i <= n;i++){
+        if(a[i] - last < cur) continue;
+        last = a[i]; cnt++;
     }
 
-    return 0;
+    return cnt >= m;
 }
 
-int main() {
-    cin.tie(0);
-    cout.tie(0);
-    ios::sync_with_stdio(false);
+ll maximization(){
+    ll lo = 1, hi = 2e9;
+    while(lo < hi){
+        ll mid = (lo + hi + 1) / 2;
+        if(decision(mid)) lo = mid;
+        else hi = mid - 1;
+    }
 
-    
+    return lo;
+}
+
+int main(){
+    ios::sync_with_stdio(0); // fastio
+    cin.tie(0), cout.tie(0); // fastio
+
     cin >> n >> m;
-    for (int i = 1; i <= n; i++) {
-        int num;
-        cin >> num;
-        arr.push_back(num);
-    }
+    for(int i = 1;i <= n;i++) cin >> a[i];
+    sort(a + 1, a + n + 1);
 
-    sort(arr.begin(), arr.end());
-    int st = 1, en = arr.back() - arr[0];
-    int mid = (st + en + 1) / 2;
-    while (en > st) {
-        mid = (st + en + 1) / 2;
-        if (dm(mid)) {
-            st = mid;
-        }
-        else {
-            en = mid - 1;
-        }
-    }
-
-    cout << en;
-
+    cout << maximization();
 
     return 0;
 }
+
+
+
