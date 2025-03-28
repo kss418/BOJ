d32f9bedc4f440891cff4562a92c4f5d70f6649c
kss418 <108602943+kss418@users.noreply.github.com>
2025-03-28 13:25:10 +0900

    [Gold IV] Title: 모자이크, Time: 0 ms, Memory: 5160 KB -BaekjoonHub

--- 백준/Gold/2539. 모자이크/모자이크.cc
+++ 백준/Gold/2539. 모자이크/모자이크.cc
@@ -1,66 +1,48 @@
-#include <bits/stdc++.h>
-#include <ext/rope>
-#include <ext/pb_ds/assoc_container.hpp>
-#include <ext/pb_ds/tree_policy.hpp>
-using namespace __gnu_pbds;
-using namespace __gnu_cxx;
-#define ordered_set tree<int, null_type, less<int>, rb_tree_tag,tree_order_statistics_node_update>
-#define fastio cin.tie(0), cout.tie(0), ios::sync_with_stdio(0);
-#define all(x) (x).begin(), (x).end()
-#define x first 
-#define y second
-using namespace std; typedef long long ll;
-using ld = long double; using pld = pair<ld, ld>;
-using ull = unsigned long long; 
-using pll = pair<ll, ll>; using tll = tuple<ll, ll, ll>;
-ll n, m, k, t = 1; string s;
+#include <iostream>
+#include <algorithm>
+using namespace std;
+using ll = long long;
 
-constexpr ll INF = 0x3f3f3f3f3f3f3f3f;
-constexpr ll MINF = 0xc0c0c0c0c0c0c0c0;
-constexpr ll MAX = 201010; // SET MAX SIZE
-constexpr ll MOD = 998244353;
-ll sz; pll a[MAX];
+const ll MAX = 201010;
+ll n, m, l, black;
+pair <ll, ll> a[MAX];
 
-template <typename T = ll>
-class _bs { // 0-based index
-public:
-    T l, r;
-    _bs(T l, T r) {
-        this->l = l; this->r = r;
+bool decision(ll cur){
+    ll cnt = 0, last = -1e12;
+    for(int i = 1;i <= black;i++){
+        if(a[i].second > cur) return 0;
+        if(a[i].first - last + 1 <= cur) continue;
+        last = a[i].first; cnt++;
     }
 
-    bool f(T cur) {
-        ll st = a[1].x, cnt = 1;
-        for(int i = 2;i <= sz;i++){
-            if(a[i].y > cur) return 1;
-            if(a[i].x >= st + cur) st = a[i].x, cnt++;
-        }
-        return cnt > k;
-    }
-
-    T ret() {
-        T lo = l, hi = r, mid = -1;
-        while (hi > lo) {
-            mid = (hi + lo) / (T)2;
-            if (f(mid)) lo = mid + 1;
-            else hi = mid;
-        }
-
-        return lo;
-    }
-};
-
-void run(){
-    cin >> n >> m >> k >> sz; _bs <ll> bs(1, 1e9);
-    for(int i = 1;i <= sz;i++) cin >> a[i].y >> a[i].x;
-    sort(a + 1, a + sz + 1);
-    cout << bs.ret();
+    return cnt <= l;
 }
 
-int main() {
-    fastio; // cin >> t;
-    while(t--) run(); 
+ll minimization(){
+    ll lo = 1, hi = 2e6;
+    while(lo < hi){
+        ll mid = (lo + hi) / 2;
+        if(decision(mid)) hi = mid;
+        else lo = mid + 1;
+    }
+
+    return lo;
+}
+
+int main(){
+    ios::sync_with_stdio(0); // fastio
+    cin.tie(0), cout.tie(0); // fastio
+
+    cin >> n >> m >> l >> black;
+    for(int i = 1;i <= black;i++) cin >> a[i].second >> a[i].first;
+    sort(a + 1, a + black + 1); 
+
+    cout << minimization();
 
     return 0;
 }
+
+
+
+
 
