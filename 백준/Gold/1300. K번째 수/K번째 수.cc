f3affaad9b87e50327eaaee098013b667ce0a633
kss418 <108602943+kss418@users.noreply.github.com>
2025-03-28 14:47:37 +0900

    [Gold I] Title: K번째 수, Time: 32 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/1300. K번째 수/K번째 수.cc
+++ 백준/Gold/1300. K번째 수/K번째 수.cc
@@ -1,53 +1,42 @@
 #include <iostream>
 #include <algorithm>
-#include <vector>
-typedef long long ll;
 using namespace std;
-ll n, k;
+using ll = long long;
 
-int dm(ll cur) {
+const ll MAX = 201010;
+ll n, m;
+
+bool decision(ll cur){
     ll cnt = 0;
-    for (ll i = 1; i <= n; i++) {
-        if (cur % i == 0) {
-            cnt += min(cur / i - 1, n);
-        }
-        else {
-            cnt += min(cur / i, n);
-        }
-        if (cnt >= k) {
-            return 0;
-        }
+    for(int i = 1;i <= n;i++){
+        cnt += min(cur / i, n);
     }
 
-    if (cnt >= k) {
-        return 0;
-    }
-    else {
-        return 1;
-    }
+    return cnt >= m;
 }
 
-int main() {
-    cin.tie(0);
-    cout.tie(0);
-    ios::sync_with_stdio(false);
-
-    cin >> n >> k;
-    ll st = 1, en = 10000000000;
-
-    ll mid = (st + en + 1) / 2;
-    while (en > st) {
-        mid = (st + en + 1) / 2;
-        if (dm(mid)) {
-            st = mid;
-        }
-        else {
-            en = mid - 1;
-        }
+ll minimization(){
+    ll lo = 1, hi = 2e10;
+    while(lo < hi){
+        ll mid = (lo + hi) / 2;
+        if(decision(mid)) hi = mid;
+        else lo = mid + 1;
     }
 
-    cout << st;
-    
+    return lo;
+}
+
+int main(){
+    ios::sync_with_stdio(0); // fastio
+    cin.tie(0), cout.tie(0); // fastio
+
+    cin >> n >> m;
+    cout << minimization();
 
     return 0;
 }
+
+
+
+
+
