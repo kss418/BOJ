b098bb7419d4d2eec7076e320014041893723b46
kss418 <108602943+kss418@users.noreply.github.com>
2025-04-01 14:35:38 +0900

    [Gold III] Title: Teamwork, Time: 24 ms, Memory: 2524 KB -BaekjoonHub

---
+++ 백준/Gold/16765. Teamwork/Teamwork.cc
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+#include <ext/rope>
+#define fastio cin.tie(0), cout.tie(0), ios::sync_with_stdio(0);
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define x first 
+#define y second
+using namespace std; using ll = long long;
+using ld = long double; using pld = pair<ld, ld>;
+using i128 = __int128_t; using f128 = __float128; 
+using pll = pair<ll, ll>; using tll = tuple<ll, ll, ll>;
+ll n, m, k, t = 1; string s;
+
+constexpr ll INF = 0x3f3f3f3f3f3f3f3f;
+constexpr ll MINF = 0xc0c0c0c0c0c0c0c0;
+constexpr ll MAX = 10101; // SET MAX SIZE
+constexpr ll MOD = 998244353;
+ll dp[MAX], a[MAX];
+
+ll solve(ll cur){
+    ll& ret = dp[cur];
+    if(!cur) return ret = 0;
+    if(ret != -1) return ret; ret = 0;
+
+    ll mx = 0;
+    for(int i = 1;i <= m;i++){
+        if(cur - i < 0) break;
+        mx = max(a[cur - i + 1], mx);
+        ret = max(ret, solve(cur - i) + mx * i);
+    }
+
+    return ret;
+}
+
+void run(){
+    cin >> n >> m; memset(dp, -1, sizeof(dp));
+    for(int i = 1;i <= n;i++) cin >> a[i];
+
+    cout << solve(n);
+}
+
+int main() {
+    fastio; // cin >> t;
+    while(t--) run();
+    
+    return 0;
+}
