3ae2a05f919d63b76a9c1584a50fe32692a1fc07
kss418 <108602943+kss418@users.noreply.github.com>
2025-04-03 12:41:30 +0900

    [Gold I] Title: 소가 길을 건너간 이유 4, Time: 8 ms, Memory: 3008 KB -BaekjoonHub

--- 백준/Gold/14464. 소가 길을 건너간 이유 4/소가 길을 건너간 이유 4.cc
+++ 백준/Gold/14464. 소가 길을 건너간 이유 4/소가 길을 건너간 이유 4.cc
@@ -1,49 +1,38 @@
-#include <bits/stdc++.h>
-#include <ext/rope>
-#define fastio cin.tie(0), cout.tie(0), ios::sync_with_stdio(0);
-#define all(x) (x).begin(), (x).end()
-#define x first 
-#define y second
-using namespace std; using ll = long long;
-using ld = long double; using pld = pair<ld, ld>;
-using i128 = __int128_t; using f128 = __float128; 
-using pll = pair<ll, ll>; using tll = tuple<ll, ll, ll>;
-ll n, m, k, t = 1; string s;
+#include <iostream>
+#include <algorithm>
+#include <queue>
+using namespace std;
+using ll = long long;
 
-constexpr ll INF = 0x3f3f3f3f3f3f3f3f;
-constexpr ll MINF = 0xc0c0c0c0c0c0c0c0;
-constexpr ll MAX = 20101; // SET MAX SIZE
-constexpr ll MOD = 998244353;
-ll a[MAX];
-pll arr[MAX];
+const ll MAX = 20101;
+ll n, m, a[MAX];
+pair <ll, ll> b[MAX];
 priority_queue <ll, vector<ll>, greater<ll>> pq;
 
-void run(){
+int main(){
+    ios::sync_with_stdio(0); // fastio
+    cin.tie(0), cout.tie(0); // fastio
+
     cin >> n >> m;
     for(int i = 1;i <= n;i++) cin >> a[i];
+    for(int i = 1;i <= m;i++) cin >> b[i].first >> b[i].second;
     sort(a + 1, a + n + 1);
-
-    for(int i = 1;i <= m;i++){
-        cin >> arr[i].x >> arr[i].y;    
-    }
-    sort(arr + 1, arr + m + 1);
+    sort(b + 1, b + m + 1);
 
     ll idx = 1, result = 0;
     for(int i = 1;i <= n;i++){
-        while(idx <= m && arr[idx].x <= a[i]){
-            pq.push(arr[idx].y); idx++;
+        while(b[idx].first <= a[i] && idx <= m){
+            pq.push(b[idx].second);
+            idx++;
         }
 
         while(!pq.empty() && pq.top() < a[i]) pq.pop();
-        if(!pq.empty()) pq.pop(), result++;
+        if(!pq.empty()){
+            result++; pq.pop();
+        }
     }
 
     cout << result;
-}
-
-int main() {
-    fastio; // cin >> t;
-    while(t--) run(); 
 
     return 0;
 }
