c293bf5fa858a9cf90dd8eed0c6a088a548a05bc
kss418 <108602943+kss418@users.noreply.github.com>
2025-04-04 15:47:31 +0900

    [Gold II] Title: 철로, Time: 36 ms, Memory: 5268 KB -BaekjoonHub

--- 백준/Gold/13334. 철로/철로.cc
+++ 백준/Gold/13334. 철로/철로.cc
@@ -1,47 +1,34 @@
-#include <bits/stdc++.h>
-#include <ext/rope>
-#include <ext/pb_ds/assoc_container.hpp>
-#include <ext/pb_ds/tree_policy.hpp>
-using namespace __gnu_pbds;
-#define ordered_set tree<int, null_type, less<int>, rb_tree_tag,tree_order_statistics_node_update>
-#define fastio cin.tie(0), cout.tie(0), ios::sync_with_stdio(0);
-#define all(x) (x).begin(), (x).end()
-#define x first 
-#define y second
-using namespace std; typedef long long ll;
-using ld = long double;
-using ull = unsigned long long;
-using pll = pair<ll, ll>; using tll = tuple<ll, ll, ll>;
-ll n, m, k, t; string s;
+#include <iostream>
+#include <queue>
+#include <algorithm>
+using namespace std;
+using ll = long long;
 
-constexpr ll INF = 0x3f3f3f3f3f3f3f3f;
-constexpr ll MAX = 201010;
-constexpr ll MOD = 998244353;
-pll a[MAX];
+const ll MAX = 101010;
+ll n, d, result;
+pair <ll, ll> a[MAX];
 priority_queue <ll, vector<ll>, greater<ll>> pq;
 
-int main() {
-    fastio;
+int main(){
+    ios::sync_with_stdio(0); // fastio
+    cin.tie(0), cout.tie(0); // fastio
 
     cin >> n;
     for(int i = 1;i <= n;i++){
-        cin >> a[i].x >> a[i].y;
-        if(a[i].y > a[i].x) swap(a[i].x, a[i].y);
+        cin >> a[i].first >> a[i].second;
+        if(a[i].second > a[i].first) swap(a[i].first, a[i].second);
     }
-    cin >> m;
+
+    cin >> d;
     sort(a + 1, a + n + 1);
 
-    ll result = 0;
     for(int i = 1;i <= n;i++){
-        pq.push(a[i].y);
-        while(!pq.empty() && pq.top() < a[i].x - m) pq.pop();
-
+        pq.push(a[i].second);
+        while(!pq.empty() && pq.top() < a[i].first - d) pq.pop();
         result = max<ll>(result, pq.size());
-    }   
-    
+    }
+
     cout << result;
-
-
     return 0;
 }
 
