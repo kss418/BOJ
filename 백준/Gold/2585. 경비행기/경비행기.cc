1a7ed76ff73071f60c13dae064832ea0efe3cc3a
kss418 <108602943+kss418@users.noreply.github.com>
2025-04-10 20:16:29 +0900

    [Gold II] Title: 경비행기, Time: 32 ms, Memory: 2044 KB -BaekjoonHub

--- 백준/Gold/2585. 경비행기/경비행기.cc
+++ 백준/Gold/2585. 경비행기/경비행기.cc
@@ -1,84 +1,59 @@
-#include <bits/stdc++.h>
-#include <ext/rope>
-#include <ext/pb_ds/assoc_container.hpp>
-#include <ext/pb_ds/tree_policy.hpp>
-using namespace __gnu_pbds;
-using namespace __gnu_cxx;
-#define ordered_set tree<int, null_type, less<int>, rb_tree_tag,tree_order_statistics_node_update>
-#define fastio cin.tie(0), cout.tie(0), ios::sync_with_stdio(0);
-#define all(x) (x).begin(), (x).end()
-#define x first 
-#define y second
-using namespace std; typedef long long ll;
-using ld = long double; using pld = pair<ld, ld>;
-using ull = unsigned long long; 
-using pll = pair<ll, ll>; using tll = tuple<ll, ll, ll>;
-ll n, m, k, t = 1; string s;
+#include <iostream>
+#include <queue>
+using namespace std;
+using ll = long long;
 
-constexpr ll INF = 0x3f3f3f3f3f3f3f3f;
-constexpr ll MINF = 0xc0c0c0c0c0c0c0c0;
-constexpr ll MAX = 1010; // SET MAX SIZE
-constexpr ll MOD = 998244353;
-pll a[MAX];
-ll d[MAX]; deque <pll> q;
+const ll MAX = 1010;
+const ll INF = 1e9;
+ll n, m, d[MAX];
+pair <ll, ll> a[MAX];
 
-ll dist(pll a, pll b){
-    ll dx = a.x - b.x, dy = a.y - b.y;
+ll dist(pair <ll, ll> a, pair <ll, ll> b){
+    ll dx = a.first - b.first;
+    ll dy = a.second - b.second;
     return dx * dx + dy * dy;
 }
 
-void bfs(ll num){
-    memset(d, 0x3f, sizeof(d));
-    q.push_back({ 0, 0 });
+bool decision(ll num){
+    num *= 10;
+    queue <ll> q;
+    for(int i = 1;i <= n + 1;i++) d[i] = INF;
+    d[0] = 0; q.push(0);
+
     while(!q.empty()){
-        auto[cur, cd] = q.front(); q.pop_front();
-        if(d[cur] <= cd) continue; d[cur] = cd;
-
-        for(ll nxt = 1;nxt <= n + 1;nxt++){
-            if(dist(a[cur], a[nxt]) > num * num) continue;
-            if(d[nxt] <= d[cur] + 1) continue;
-            q.push_back({nxt, cd + 1});
+        ll cur = q.front(); q.pop();
+        for(auto nxt = 1;nxt <= n + 1;nxt++){
+            if(dist(a[nxt], a[cur]) > num * num) continue;
+            if(d[nxt] != INF) continue;
+            
+            q.push(nxt);
+            d[nxt] = d[cur] + 1;
         }
     }
+
+    return d[n + 1] - 1 <= m;
 }
 
-template <typename T = ll>
-class _bs { // 0-based index
-public:
-    T l, r;
-    _bs(T l, T r) {
-        this->l = l; this->r = r;
+ll minimization(){
+    ll lo = 0, hi = 1e5;
+    while(lo < hi){
+        ll mid = (lo + hi) / 2;
+        if(decision(mid)) hi = mid;
+        else lo = mid + 1;
     }
 
-    bool f(T cur) {
-        bfs(10 * cur);
-        return d[n + 1] - 1 <= m;
-    }
-
-    T ret() {
-        T lo = l, hi = r, mid = -1;
-        while (hi > lo) {
-            mid = (hi + lo) / (T)2;
-            if (f(mid)) hi = mid;
-            else lo = mid + 1;
-        }
-
-        return lo;
-    }
-};
-
-void run(){
-    cin >> n >> m; a[0] = { 0, 0 }; a[n + 1] = { 10000, 10000 };
-    for(int i = 1;i <= n;i++) cin >> a[i].x >> a[i].y;
-    _bs <ll> bs(0, 1e6);
-
-    cout << bs.ret();
+    return lo;
 }
 
-int main() {
-    fastio; // cin >> t;
-    while(t--) run(); 
+int main(){
+    ios::sync_with_stdio(0); // fastio
+    cin.tie(0), cout.tie(0); // fastio
+
+    cin >> n >> m;
+    for(int i = 1;i <= n;i++) cin >> a[i].first >> a[i].second;
+    a[n + 1] = { 10000, 10000 };
+
+    cout << minimization();
 
     return 0;
 }
-
