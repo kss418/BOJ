6300012599143b0dc03ce334ea96c959c0cfdc6a
kss418 <108602943+kss418@users.noreply.github.com>
2025-04-11 21:00:14 +0900

    [Silver III] Title: 계단 오르기, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.cc
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.cc
@@ -1,32 +1,26 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
 using namespace std;
-int n;
-int arr[10001], dp[10001][2];
+using ll = long long;
 
-int main() {
-	ios_base::sync_with_stdio(0);
-	cin.tie(0);
-	cout.tie(0);
-	
-	cin >> n;
-	for (int i = 1; i < n + 1; i++) {
-		cin >> arr[i];
-	}
+const ll MAX = 303;
+const ll INF = 1e9;
+ll n, a[MAX], dp[MAX][2];
 
-	dp[1][0] = arr[1];
-	dp[1][1] = arr[1];
-	dp[2][0] = arr[2];
-	dp[2][1] = arr[1] + arr[2];
+int main(){
+    ios::sync_with_stdio(0); // fastio
+    cin.tie(0), cout.tie(0); // fastio
 
-	for (int i = 3; i < n + 1; i++) {
-		dp[i][0] = max(dp[i - 2][0], dp[i - 2][1]) + arr[i];
-		dp[i][1] = dp[i - 1][0] + arr[i];
-	}
+    cin >> n;
+    for(int i = 1;i <= n;i++) cin >> a[i];
+    
+    for(int i = 1;i <= n;i++){
+		dp[i][1] = dp[i - 1][0] + a[i];
+        if(i >= 2) dp[i][0] = max(dp[i - 2][0], dp[i - 2][1]) + a[i];
+        else dp[i][0] = a[i];
+    }
 
-	cout << max(dp[n][0], dp[n][1]);
+    cout << max(dp[n][0], dp[n][1]);
 
-
-	return 0;
+    return 0;
 }
+
