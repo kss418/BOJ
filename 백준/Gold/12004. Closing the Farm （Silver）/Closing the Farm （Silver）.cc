507056844dfa5769864799f0c66eb009867b6728
kss418 <108602943+kss418@users.noreply.github.com>
2025-04-17 11:16:11 +0900

    [Gold IV] Title: Closing the Farm (Silver), Time: 4 ms, Memory: 10156 KB -BaekjoonHub

---
+++ 백준/Gold/12004. Closing the Farm （Silver）/Closing the Farm （Silver）.cc
@@ -0,0 +1,76 @@
+#include <bits/stdc++.h>
+#include <ext/rope>
+#define fastio cin.tie(0), cout.tie(0), ios::sync_with_stdio(0);
+#define all(x) (x).begin(), (x).end()
+#define x first 
+#define y second
+using namespace std; using ll = long long;
+using ld = long double; using pld = pair<ld, ld>;
+using i128 = __int128_t; using f128 = __float128; 
+using pll = pair<ll, ll>; using tll = tuple<ll, ll, ll>;
+ll n, m, k, t = 1; string s;
+
+constexpr ll INF = 0x3f3f3f3f3f3f3f3f;
+constexpr ll MINF = 0xc0c0c0c0c0c0c0c0;
+constexpr ll MAX = 201010; // SET MAX SIZE
+constexpr ll MOD = 998244353;
+ll a[MAX], v[MAX];
+vector <ll> adj[MAX];
+stack <string> result;
+
+class _uf { 
+public:
+    ll n; vector <ll> p, si;
+    
+    _uf(){}
+    _uf(ll n) { 
+        this->n = n;
+        p.resize(n + 1, -1); si.resize(n + 1, 1);
+    }
+
+    ll find(ll num) { 
+        if (p[num] == -1) return num;
+        return p[num] = find(p[num]);
+    }
+
+    void merge(ll a, ll b) {
+        a = find(a); b = find(b);
+        if (a == b) return;
+        if (size(a) < size(b)) swap(a, b);
+        p[b] = a, si[a] += si[b];
+    }
+
+    ll size(ll num){ return si[find(num)]; }
+    bool same(ll a, ll b) { return find(a) == find(b); }
+};
+
+void run(){
+    cin >> n >> m; _uf uf(n);
+    while(m--){
+        ll s, e; cin >> s >> e;
+        adj[s].push_back(e);
+        adj[e].push_back(s);
+    }
+
+    for(int i = 1;i <= n;i++) cin >> a[i];
+    reverse(a + 1, a + n + 1);
+    for(int i = 1;i <= n;i++){
+        ll cur = a[i]; v[cur] = 1;
+        for(auto& nxt : adj[cur]){
+            if(v[nxt]) uf.merge(cur, nxt);
+        }
+        result.push((uf.size(cur) == i ? "YES" : "NO"));
+    }
+
+    while(!result.empty()){
+        cout << result.top() << "\n";
+        result.pop();
+    }
+}
+
+int main() {
+    fastio; // cin >> t;
+    while(t--) run(); 
+
+    return 0;
+}
