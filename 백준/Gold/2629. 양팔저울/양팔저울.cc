113128d89f6d8aeb0e0788cfcc12591706437524
kss418 <108602943+kss418@users.noreply.github.com>
2025-04-28 13:21:17 +0900

    [Gold III] Title: 양팔저울, Time: 12 ms, Memory: 28064 KB -BaekjoonHub

--- 백준/Gold/2629. 양팔저울/양팔저울.cc
+++ 백준/Gold/2629. 양팔저울/양팔저울.cc
@@ -1,56 +1,43 @@
 #include <iostream>
-#include <algorithm>
-#include <vector>
-#include <cstring>
-#include <set>
-#include <queue>
-#include <deque>
-#include <cmath>
 using namespace std;
-typedef long long ll;
-ll n, m, k, t; string s;
+using ll = long long;
+
+const ll MAX = 33;
+const ll MID = 50101;
+ll n, m, a[MAX], b[MAX];
+ll dp[MAX][101010];
 
 
-const int MAX = 31;
-int arr[MAX], dp[MAX][100001];
+ll solve(ll cur, ll num){
+    if(num < 0 || num > 101010) return 0;
+    ll& ret = dp[cur][num];
+    if(ret != -1) return ret;
+    if(cur == 0) return ret = (num == MID ? 1 : 0);
 
+    ret = max(ret, solve(cur - 1, num));
+    ret = max(ret, solve(cur - 1, num + a[cur]));
+    ret = max(ret, solve(cur - 1, num - a[cur]));
 
-int main() {
-	cin.tie(0);
-	cout.tie(0);
-	ios::sync_with_stdio(false);
-
-
-	cin >> n;
-	for (int i = 1; i <= n; i++) {
-		cin >> arr[i];
-	}
-
-	dp[0][50000] = 1;
-	for (int i = 1; i <= n; i++) {
-		for (int j = -50000; j < 50000; j++) {
-			if (arr[i] + j <= 50000) {
-				dp[i][arr[i] + j + 50000] = max(dp[i - 1][j + 50000], dp[i][arr[i] + j + 50000]);
-			}
-
-			if (j - arr[i] > -50000) {
-				dp[i][j - arr[i] + 50000] = max(dp[i - 1][j + 50000], dp[i][j - arr[i] + 50000]);
-			}
-			dp[i][j + 50000] = max(dp[i][j + 50000], dp[i - 1][j + 50000]);
-		}
-	}
-
-	cin >> m;
-	while (m--) {
-		cin >> k;
-		if (dp[n][k + 50000]) {
-			cout << "Y" << " ";
-		}
-		else {
-			cout << "N" << " ";
-		}
-	}
-
-
-	return 0;
+    return ret;
 }
+
+int main(){
+    ios::sync_with_stdio(0); // fastio
+    cin.tie(0), cout.tie(0); // fastio
+
+    cin >> n;
+    for(int i = 1;i <= n;i++) cin >> a[i];
+    cin >> m;
+    for(int i = 1;i <= m;i++) cin >> b[i];
+
+    for(int i = 0;i <= n;i++){
+        for(int j = 0;j < 101010;j++) dp[i][j] = -1;
+    }
+
+    for(int i = 1;i <= m;i++){
+        cout << (solve(n, b[i] + MID) ? "Y" : "N") << " ";
+    }
+
+    return 0;
+}
+
