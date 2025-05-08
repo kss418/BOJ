ea5162cdab486c8905221295d0ee25e40f627315
kss418 <108602943+kss418@users.noreply.github.com>
2025-05-08 13:37:42 +0900

    [Gold IV] Title: 해킹, Time: 120 ms, Memory: 9240 KB -BaekjoonHub

--- 백준/Gold/10282. 해킹/해킹.cc
+++ 백준/Gold/10282. 해킹/해킹.cc
@@ -1,103 +1,58 @@
-#include <bits/stdc++.h>
-#include <ext/rope>
-#include <ext/pb_ds/assoc_container.hpp>
-#include <ext/pb_ds/tree_policy.hpp>
-using namespace __gnu_pbds;
-using namespace __gnu_cxx;
-#define ordered_set tree<int, null_type, less<int>, rb_tree_tag,tree_order_statistics_node_update>
-#define fastio cin.tie(0), cout.tie(0), ios::sync_with_stdio(0);
-#define all(x) (x).begin(), (x).end()
-#define x first 
-#define y second
-using namespace std; typedef long long ll;
-using ld = long double; using pld = pair<ld, ld>;
-using ull = unsigned long long; 
-using pll = pair<ll, ll>; using tll = tuple<ll, ll, ll>;
-ll n, m, k, t = 1; string s;
+#include <iostream>
+#include <vector>
+#include <queue> 
+using namespace std;
+using ll = long long;
 
-constexpr ll INF = 0x3f3f3f3f3f3f3f3f;
-constexpr ll MAX = 201010; // SET MAX SIZE
-constexpr ll MOD = 998244353;
-ll a[MAX];
+const ll MAX = 10101;
+const ll INF = 1e12;
+ll n, m, k, d[MAX];
+vector <pair<ll, ll>> adj[MAX];
 
-class _dij {
-public:
-    class node{
-    public:
-        ll d;
-        ll num() const{ return d; }
-        bool operator<(const node& ot) const{ return num() < ot.num(); }
-        bool operator>(const node& ot) const{ return num() > ot.num(); }
-        bool operator==(const node& ot) const{ return num() == ot.num(); }
-        bool operator<=(const node& ot) const{ return num() <= ot.num(); }
-        node operator+(const node& ot) const{
-            return {d + ot.d};
-        }
-    };
-    node mx(){ return {INF}; }
-    node mn(){ return {0}; }
+using pll = pair<ll, ll>;
+priority_queue <pll, vector<pll>, greater<pll>> pq;
 
-    using ptl = pair <node, ll>;
-    ll n; vector <node> d;
-    vector <ll> pre;
-    vector <vector<ptl>> adj;
-    priority_queue <ptl, vector<ptl>, greater<ptl>> pq;
-
-    _dij(ll n) {
-        this->n = n;
-        d.resize(n + 1, mx());
-        pre.resize(n + 1, -1); adj.resize(n + 1);
-    }
-
-    void add(ll st, ll en, node c) { // 양방향
-        adj[st].push_back({ c,en });
-        adj[en].push_back({ c,st });
-    }
-    void addsol(ll st, ll en, node c) { // 단방향
-        adj[st].push_back({ c,en });
-    }
-
-    void init(ll st) {
-        pq.push({ mn(), st });
-
-        while (!pq.empty()) {
-            auto [cn, cur] = pq.top(); pq.pop();
-            if (d[cur] <= cn) continue;
-            d[cur] = cn;
-            
-            for (auto& i : adj[cur]) {
-                auto [nn, nxt] = i;
-                node pl = nn + cn;
-        
-                if (d[nxt] <= pl) continue;
-                pre[nxt] = cur; pq.push({ pl, nxt });
-            }
-        }
-    }
-
-    node ret(ll n) { return d[n]; }
-};
+void init(){
+    while(!pq.empty()) pq.pop();
+    for(int i = 1;i <= n;i++) adj[i].clear();
+}
 
 void run(){
-    cin >> n >> m >> k; _dij dij(n);
+    cin >> n >> m >> k; init();
     while(m--){
         ll s, e, c; cin >> s >> e >> c;
-        dij.addsol(e, s, {c});
+        adj[e].push_back({s, c});
     }
-    dij.init(k);
 
-    ll result = 0, cnt = 0;
+    for(int i = 0;i < MAX;i++) d[i] = INF;
+    pq.push({0, k});
+
+    while(!pq.empty()){
+        auto[cd, cur] = pq.top(); pq.pop();
+        if(d[cur] <= cd) continue;
+        d[cur] = cd;
+
+        for(auto& [nxt, co] : adj[cur]){
+            if(d[nxt] <= cd + co) continue;
+            pq.push({cd + co, nxt});
+        }
+    }
+
+    ll cnt = 0, result = 0;
     for(int i = 1;i <= n;i++){
-        if(dij.ret(i).d >= INF) continue;
-        result = max(result, dij.ret(i).d); cnt++;
+        if(d[i] == INF) continue;
+        cnt++; result = max(result, d[i]);
     }
 
     cout << cnt << " " << result << "\n";
 }
 
-int main() {
-    fastio; cin >> t;
-    while(t--) run(); 
+int main(){
+    ios::sync_with_stdio(0); // fastio
+    cin.tie(0), cout.tie(0); // fastio
+
+    ll t; cin >> t;
+    while(t--) run();
 
     return 0;
 }
