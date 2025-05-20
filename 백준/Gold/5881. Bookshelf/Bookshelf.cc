f17f72d52182bc92ef36215167621eb8e9e3f812
kss418 <108602943+kss418@users.noreply.github.com>
2025-05-20 12:42:28 +0900

    [Gold III] Title: Bookshelf, Time: 4 ms, Memory: 2084 KB -BaekjoonHub

---
+++ 백준/Gold/5881. Bookshelf/Bookshelf.cc
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+#define fastio cin.tie(0), cout.tie(0), ios::sync_with_stdio(0);
+#define all(x) (x).begin(), (x).end()
+#define x first 
+#define y second
+using namespace std; using ll = long long;
+using ld = long double; using pld = pair<ld, ld>;
+using i128 = __int128_t; using f128 = __float128; 
+using pll = pair<ll, ll>;
+ll n, m, k, t = 1; string s;
+
+constexpr ll INF = 0x3f3f3f3f3f3f3f3f;
+constexpr ll MINF = 0xc0c0c0c0c0c0c0c0;
+constexpr ll MAX = 2010; // SET MAX SIZE
+constexpr ll MOD = 998244353;
+ll sum[MAX], dp[MAX];
+pll a[MAX];
+
+ll solve(ll cur){
+    ll& ret = dp[cur];
+    if(ret != -1) return ret; ret = INF;
+    if(!cur) return ret = 0;
+
+    ll mx = 0;
+    for(int i = cur;i >= 1;i--){
+        if(sum[cur] - sum[i - 1] > m) break;
+        mx = max(mx, a[i].x);
+        ret = min(ret, solve(i - 1) + mx);
+    }
+
+    return ret;
+}
+
+void run(){
+    cin >> n >> m; memset(dp, -1, sizeof(dp));
+    for(int i = 1;i <= n;i++){
+        cin >> a[i].x >> a[i].y;
+        sum[i] = sum[i - 1] + a[i].y; 
+    }
+
+    cout << solve(n);
+}
+
+int main() {
+    fastio; // cin >> t;
+    while(t--) run();
+    
+    return 0;
+}
