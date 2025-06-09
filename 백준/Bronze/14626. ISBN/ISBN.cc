79c5932bf80aa3081160e55ca0553adf83560408
kss418 <108602943+kss418@users.noreply.github.com>
2025-06-09 11:20:16 +0900

    [Bronze I] Title: ISBN, Time: 0 ms, Memory: 3596 KB -BaekjoonHub

---
+++ 백준/Bronze/14626. ISBN/ISBN.cc
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+#include <ext/rope>
+#define fastio cin.tie(0), cout.tie(0), ios::sync_with_stdio(0);
+#define all(x) (x).begin(), (x).end()
+#define x first 
+#define y second
+using namespace std; using ll = long long;
+using ld = long double; using pld = pair<ld, ld>;
+using i128 = __int128_t; using f128 = __float128; 
+using pll = pair<ll, ll>; using tll = tuple<ll, ll, ll>;
+ll n, m, k, t = 1; string s;
+
+constexpr ll INF = 0x3f3f3f3f3f3f3f3f;
+constexpr ll MINF = 0xc0c0c0c0c0c0c0c0;
+constexpr ll MAX = 201010; // SET MAX SIZE
+constexpr ll MOD = 998244353;
+ll a[MAX];
+
+void run(){
+    cin >> s; ll sum = 0, idx = -1;
+    for(int i = 0;i < s.size();i++){
+        if(s[i] == '*'){
+            idx = i;
+            continue;
+        }
+        sum += (s[i] - '0') * (i % 2 ? 3 : 1);
+    }
+    
+    for(int i = 0;i < 10;i++){
+        ll now = i * (idx % 2 ? 3 : 1);
+        if((sum + now) % 10 == 0) cout << i;
+    }
+}
+
+int main() {
+    fastio; // cin >> t;
+    while(t--) run(); 
+
+    return 0;
+}
+
