0228cd73c495670e15b5f8759a92f4dd339b9b6a
kss418 <108602943+kss418@users.noreply.github.com>
2025-06-27 16:29:53 +0900

    [Platinum IV] Title: 구간 합 구하기 2, Time: 236 ms, Memory: 159860 KB -BaekjoonHub

--- 백준/Platinum/10999. 구간 합 구하기 2/구간 합 구하기 2.cc
+++ 백준/Platinum/10999. 구간 합 구하기 2/구간 합 구하기 2.cc
@@ -1,45 +1,92 @@
 #include <bits/stdc++.h>
 #define fastio cin.tie(0), cout.tie(0), ios::sync_with_stdio(0);
-using namespace std; typedef long long ll;
-using ull = unsigned long long;
-using pll = pair<ll, ll>; using tll = tuple<ll, ll, ll>;
-ll n, m, k, t; string s;
+#define all(x) (x).begin(), (x).end()
+#define x first 
+#define y second
+using namespace std; using ll = long long;
+using ld = long double; using pld = pair<ld, ld>;
+using i128 = __int128_t; using f128 = __float128; 
+using pll = pair<ll, ll>;
+ll n, m, k, t = 1; string s;
 
 constexpr ll INF = 0x3f3f3f3f3f3f3f3f;
-constexpr ll MAX = 1010;
-constexpr ll MOD = 1e9 + 7;
+constexpr ll MINF = 0xc0c0c0c0c0c0c0c0;
+constexpr ll MAX = 201010; // SET MAX SIZE
+constexpr ll MOD = 998244353;
+ll a[MAX];
 
-class _prop {
+template <typename T = ll> // query type
+class _prop { // 구간 예외 처리하기
 public:
-    vector<ll> seg, lazy, arr; ll n;
+    class node{
+    public:
+        ll v, len;
+        node() : node(0, 0){}
+        node(ll v, ll len) : v(v), len(len){}
+        
+        node& operator += (node ot){ // add
+            return *this;
+        }
 
-    _prop(ll n) {
-        this->n = n; arr.resize(n + 1);
-        seg.resize(4 * n + 1); lazy.resize(4 * n + 1);
+        operator T(){ // node -> query
+            return v;
+        }
+    };
+
+    class lazy_type{
+    public:
+        ll v;
+        lazy_type() : lazy_type(0){}
+        lazy_type(ll v) : v(v){}
+
+        lazy_type& operator += (lazy_type ot){ // add
+            v += ot.v;
+            return *this;
+        }
+    };
+
+    vector<node> seg, tmp; ll n;
+    vector<lazy_type> lazy;
+
+    node merge(node l, node r){ 
+        return { l.v + r.v, l.len + r.len };
     }
 
-    void con(ll idx, ll val) {  arr[idx] = val; }
+    bool empty(lazy_type lazy){ // prop 시 return 여부 결정
+        return !lazy.v;
+    }
 
-    void init() { init(1, n); }
-    ll init(ll l, ll r, ll node = 1) {
-        if (l == r) return seg[node] = arr[l];
-        ll mid = (l + r) >> 1;
-        seg[node] = init(l, mid, node * 2) + init(mid + 1, r, node * 2 + 1);
-        return seg[node];
+    void prop(lazy_type& s, lazy_type p){ // lazy -> lazy prop 연산
+        s += p;
+    }
+
+    void cal(lazy_type lazy, node& seg){ //lazy로 인한 seg 값 계산
+        seg.v += seg.len * lazy.v;
+    }
+
+    void erase(lazy_type& lazy){ // lazy값 초기화
+        lazy = 0;
+    }
+
+    _prop() {}
+    _prop(ll n) {
+        this->n = n; seg.resize(4 * n + 1, node()); 
+        lazy.resize(4 * n + 1, lazy_type());
+        tmp.resize(4 * n + 1, node()); 
     }
 
     void propagate(ll l, ll r, ll node) {
-        if (!lazy[node]) return;
+        if (empty(lazy[node])) return;
         if (l != r) {
-            lazy[node * 2] += lazy[node];
-            lazy[node * 2 + 1] += lazy[node];
+            prop(lazy[node * 2], lazy[node]);
+            prop(lazy[node * 2 + 1], lazy[node]);
         }
-        seg[node] += lazy[node] * (r - l + 1);
-        lazy[node] = 0;
+        cal(lazy[node], seg[node]);
+        erase(lazy[node]);
     }
 
-    void add(ll st, ll en, ll val) { add(st, en, val, 1, n); }
-    void add(ll st, ll en, ll val, ll l, ll r, ll node = 1) {
+    void add(ll st, ll en, lazy_type val) { add(st, en, val, 0, n); }
+    void add(ll st, ll en, lazy_type val, ll l, ll r, ll node = 1) {
         propagate(l, r, node);
 
         if (st > r || en < l) return;
@@ -52,31 +99,44 @@ public:
         add(st, en, val, l, mid, node * 2);
         add(st, en, val, mid + 1, r, node * 2 + 1);
 
-        seg[node] = seg[node * 2] + seg[node * 2 + 1];
+        seg[node] = merge(seg[node * 2], seg[node * 2 + 1]);
     }
 
-    ll query(ll st, ll en) { return query(st, en, 1, n); }
-    ll query(ll st, ll en, ll l, ll r, ll node = 1) {
+    T query(ll st, ll en) { return query(st, en, 0, n); }
+    node query(ll st, ll en, ll l, ll r, ll node = 1) {
         propagate(l, r, node);
 
-        if (st > r || en < l) return 0;
+        if (st > r || en < l) return _prop::node();
         if (l >= st && r <= en) return seg[node];
 
         ll mid = (l + r) >> 1;
-        ll ret = query(st, en, l, mid, node * 2);
-        ret += query(st, en, mid + 1, r, node * 2 + 1);
+        return merge(query(st, en, l, mid, node * 2), query(st, en, mid + 1, r, node * 2 + 1));
+    }
+    
+    void set(ll idx, node val){
+        tmp[idx] = val;
+    }
 
-        return ret;
+    void init(){ init(0, n); }
+    void init(ll l, ll r, ll node = 1){
+        if(l == r){
+            seg[node] = tmp[l];
+            return;
+        }
+
+        ll mid = (l + r) >> 1ll;
+        init(l, mid, node * 2);
+        init(mid + 1, r, node * 2 + 1);
+        seg[node] = merge(seg[node * 2], seg[node * 2 + 1]);
     }
 };
 
-int main() {
-    fastio;
+void run(){
     cin >> n >> m >> k;
     _prop seg(n);
 
     for (int i = 1; i <= n; i++) {
-        cin >> t; seg.con(i, t);
+        ll v; cin >> v; seg.set(i, {v, 1});
     }
     seg.init();
 
@@ -93,7 +153,11 @@ int main() {
         }
     }
     
+}
 
-
+int main() {
+    fastio; // cin >> t;
+    while(t--) run();
+    
     return 0;
 }
