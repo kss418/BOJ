86bc96fe05d74f1b750a516ed49fe29b165d3e3c
kss418 <108602943+kss418@users.noreply.github.com>
2025-07-01 23:06:05 +0900

    [Platinum I] Title: 화물열차, Time: 388 ms, Memory: 100488 KB -BaekjoonHub

---
+++ 백준/Platinum/1665. 화물열차/화물열차.cc
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+#include <ext/rope>
+#define fastio cin.tie(0), cout.tie(0), ios::sync_with_stdio(0);
+#define all(x) (x).begin(), (x).end()
+#define x first 
+#define y second
+using namespace std; using ll = long long;
+using ld = long double; using pld = pair<ld, ld>;
+using i128 = __int128_t; using f128 = __float128; 
+using pll = pair<ll, ll>; using tll = tuple<ll, ll, ll>;
+ll n, m, k, t = 1; string s;
+
+constexpr ll INF = 0x3f3f3f3f3f3f3f3f;
+constexpr ll MINF = 0xc0c0c0c0c0c0c0c0;
+constexpr ll MAX = 1010; // SET MAX SIZE
+constexpr ll MOD = 998244353;
+pll a[MAX], b[MAX];
+vector <pll> num;
+
+void add(ll idx, ll v){
+    num.push_back({idx, v});
+}
+
+void run(){
+    cin >> n;
+    for(int i = 1;i <= n;i++) cin >> a[i].x >> a[i].y;
+    
+    cin >> m;
+    for(int i = 1;i <= m;i++) cin >> b[i].x >> b[i].y;
+
+    for(int i = 1;i <= n;i++){
+        for(int j = 1;j <= m;j++){
+            add(a[i].x + b[j].x - 1, 1);
+            add(a[i].x + b[j].y, -1);
+            add(a[i].y + b[j].x, -1);
+            add(a[i].y + b[j].y + 1, 1);
+        }
+    }
+
+    sort(all(num)); 
+    ll pre = -1, now = 0;
+    ll sum = 0, cur = 0;
+    ll mx = -1, result = -1;
+
+    for(auto& i : num){
+        if(pre == i.x) now += i.y;
+        else{
+            sum += now; 
+            if(pre != -1) cur += (i.x - pre) * sum;
+            
+            if(cur > mx) mx = cur, result = i.x - 1;
+            now = i.y; pre = i.x;
+        }
+    }
+
+    cout << result;
+}
+
+int main() {
+    fastio; // cin >> t;
+    while(t--) run(); 
+
+    return 0;
+}
+
