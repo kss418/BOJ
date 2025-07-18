3cabc220e912087e47f0bc689b7b95d28774601b
kss418 <108602943+kss418@users.noreply.github.com>
2025-07-18 16:23:23 +0900

    [Gold III] Title: 배열 나누기, Time: 20 ms, Memory: 5316 KB -BaekjoonHub

---
+++ 백준/Gold/34049. 배열 나누기/배열 나누기.cc
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+#include <ext/rope>
+#define fastio cin.tie(0), cout.tie(0), ios::sync_with_stdio(0);
+#define all(x) (x).begin(), (x).end()
+#define x first 
+#define y second
+using namespace std; using ll = long long;
+using ld = long double; using pld = pair<ld, ld>;
+using i128 = __int128_t; using f128 = __float128; 
+using pll = pair<ll, ll>; using tll = tuple<ll, ll, ll>;
+ll n, m, k, t = 1; string s;
+
+constexpr ll INF = 0x3f3f3f3f3f3f3f3f;
+constexpr ll MINF = 0xc0c0c0c0c0c0c0c0;
+constexpr ll MAX = 201010; // SET MAX SIZE
+constexpr ll MOD = 998244353;
+ll a[MAX], flag;
+stack <ll> st;
+
+void chk(){
+    while(!st.empty() && st.top() < 0){
+        ll top = st.top(); st.pop();
+        if(st.empty()){ flag = 1; break; }
+        top += st.top(); st.pop();
+        st.push(top);
+    }   
+}
+
+void run(){
+    cin >> n;
+    for(int i = 1;i <= n;i++) cin >> a[i];
+
+    for(int i = 1;i <= n;i++){
+        chk();
+        st.push(a[i]);
+    }
+
+    chk();
+    cout << (flag ? -1 : (ll)st.size());
+}
+
+int main() {
+    fastio; // cin >> t;
+    while(t--) run(); 
+
+    return 0;
+}
+
