459362fc3ea49881c8ac2b019f440ab9676b77f2
kss418 <108602943+kss418@users.noreply.github.com>
2025-07-18 17:25:03 +0900

    [Gold II] Title: 8번 출구, Time: 24 ms, Memory: 7824 KB -BaekjoonHub

---
+++ 백준/Gold/33577. 8번 출구/8번 출구.cc
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+#include <ext/rope>
+#define fastio cin.tie(0), cout.tie(0), ios::sync_with_stdio(0);
+#define all(x) (x).begin(), (x).end()
+#define x first 
+#define y second
+using namespace std; using ll = long long;
+using ld = long double; using pld = pair<ld, ld>;
+using i128 = __int128_t; using f128 = __float128; 
+using pll = pair<ll, ll>; using tll = tuple<ll, ll, ll>;
+ll n, m, k, t = 1; string s;
+
+constexpr ll INF = 0x3f3f3f3f3f3f3f3f;
+constexpr ll MINF = 0xc0c0c0c0c0c0c0c0;
+constexpr ll MAX = 101010; // SET MAX SIZE
+constexpr ll MOD = 998244353;
+ll a[MAX], pre[MAX], now, result;
+priority_queue <ll> pq;
+priority_queue <pll> num;
+
+void run(){
+    cin >> n;
+    for(int i = 1;i <= n;i++) cin >> a[i];
+    for(int i = 1;i <= n;i++) pre[i] = pre[i - 1] + a[i];
+    for(int i = 1;i < n;i++) num.push({-pre[i - 1], a[i] - pre[i - 1]});
+    if(n == 1){ cout << 1; return; }
+
+    while(now < pre[n]){
+        while(!num.empty() && -num.top().x <= now){
+            ll co = num.top().y;
+            num.pop(); pq.push(co);
+        }
+
+        ll nxt = pre[n];
+        if(!num.empty()) nxt = min(nxt, -num.top().x);
+
+        ll div = (nxt - now) / pq.top();
+        if((nxt - now) % pq.top()) div++;
+        now += pq.top() * div; result += div;
+    }
+    
+    cout << result; 
+}
+
+int main() {
+    fastio; // cin >> t;
+    while(t--) run(); 
+
+    return 0;
+}
+
