470dbb3fec680fad5ba8e60861199e2bdb256983
kss418 <108602943+kss418@users.noreply.github.com>
2025-07-30 16:20:45 +0900

    [Gold II] Title: Kbin, Time: 0 ms, Memory: 2108 KB, Score: 70 point -BaekjoonHub

---
+++ 백준/Gold/16005. Kbin/Kbin.cc
@@ -0,0 +1,79 @@
+#include <bits/stdc++.h>
+#include <ext/rope>
+#define fastio cin.tie(0), cout.tie(0), ios::sync_with_stdio(0);
+#define all(x) (x).begin(), (x).end()
+#define x first 
+#define y second
+using namespace std; using ll = long long;
+using ld = long double; using pld = pair<ld, ld>;
+using i128 = __int128_t; using f128 = __float128; 
+using pll = pair<ll, ll>; using tll = tuple<ll, ll, ll>;
+ll n, m, k, t = 1; string s;
+
+constexpr ll INF = 0x3f3f3f3f3f3f3f3f;
+constexpr ll MINF = 0xc0c0c0c0c0c0c0c0;
+constexpr ll MAX = 52; // SET MAX SIZE
+constexpr ll MOD = 1234567;
+ll dp[MAX][MAX][2], c[MAX][MAX][2];
+
+ll num(ll cur, ll cnt, ll f){
+    ll& ret = c[cur][cnt][f];
+    if(ret != -1) return ret; ret = 0;
+    if(cur == 1) return ret = (cnt <= 1 ? 1 : 0);
+
+    if(f){
+        if(n & (1ll << (cur - 1))){
+            if(cnt) ret += num(cur - 1, cnt - 1, 1), ret %= MOD;
+            ret += num(cur - 1, cnt, 0); ret %= MOD;
+        }
+        else ret += num(cur - 1, cnt, 1); ret %= MOD;
+    }   
+    else{
+        if(cnt) ret += num(cur - 1, cnt - 1, 0), ret %= MOD;
+        ret += num(cur - 1, cnt, 0); ret %= MOD;
+    }
+
+    return ret %= MOD;
+}
+
+ll solve(ll cur, ll cnt, ll f){
+    ll& ret = dp[cur][cnt][f];
+    if(ret != -1) return ret; ret = 0;
+    if(cur == 1) return ret = (cnt == 1 ? 1 : 0);
+
+    if(f){
+        if(n & (1ll << (cur - 1))){
+            if(cnt){
+                ll now = ((1ll << (cur - 1)) % MOD * num(cur - 1, cnt - 1, 1)) % MOD;
+                ret += (solve(cur - 1, cnt - 1, 1) + now) % MOD; ret %= MOD;
+            }
+            ret += solve(cur - 1, cnt, 0); ret %= MOD;
+        }
+        else ret += solve(cur - 1, cnt, 1), ret %= MOD;
+    }
+    else{
+        if(cnt){
+            ll now = ((1ll << (cur - 1)) % MOD * num(cur - 1, cnt - 1, 0)) % MOD;
+            ret += (solve(cur - 1, cnt - 1, 0) + now) % MOD; ret %= MOD;
+        }
+        ret += solve(cur - 1, cnt, 0); ret %= MOD;
+    }
+
+    return ret %= MOD;
+}
+
+void run(){
+    cin >> n >> m; 
+    memset(dp, -1, sizeof(dp));
+    memset(c, -1, sizeof(c));
+
+    cout << solve(51, m, 1);
+}
+
+int main() {
+    fastio; // cin >> t;
+    while(t--) run(); 
+
+    return 0;
+}
+
---
+++ 백준/Gold/16005. Kbin/README.md
@@ -0,0 +1,30 @@
+# [Gold II] Kbin - 16005 
+
+[문제 링크](https://www.acmicpc.net/problem/16005) 
+
+### 성능 요약
+
+메모리: 2108 KB, 시간: 0 ms
+
+### 분류
+
+조합론, 수학
+
+### 제출 일자
+
+2025년 7월 30일 16:13:05
+
+### 문제 설명
+
+<p>We call the k-binary number a natural number which has in its binary representation exactly k digits of 1. For example, the 23 is a 4-binary number because the binary representation is 10111 and contains 4 digits of 1.</p>
+
+<p>Given the N and k values, calculate the sum S of all k-binary numbers which are strictly lower than N. Because the sum is very large, you have to calculate modulo 1234567.</p>
+
+### 입력 
+
+ <p>The standard input contains the values N and k separated by a single space.</p>
+
+### 출력 
+
+ <p>The standard output will contain the number S modulo 1234567.</p>
+
