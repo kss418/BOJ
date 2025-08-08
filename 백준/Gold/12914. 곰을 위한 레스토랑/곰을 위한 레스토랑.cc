8db4630aef058443592424147583bc4fd8f808a3
kss418 <108602943+kss418@users.noreply.github.com>
2025-08-08 13:21:52 +0900

    [Gold III] Title: 곰을 위한 레스토랑, Time: 20 ms, Memory: 2164 KB -BaekjoonHub

---
+++ 백준/Gold/12914. 곰을 위한 레스토랑/곰을 위한 레스토랑.cc
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+#include <ext/rope>
+#define fastio cin.tie(0), cout.tie(0), ios::sync_with_stdio(0);
+#define all(x) (x).begin(), (x).end()
+#define x first 
+#define y second
+using namespace std; using ll = long long;
+using ld = long double; using pld = pair<ld, ld>;
+using i128 = __int128_t; using f128 = __float128; 
+using pll = pair<ll, ll>; using tll = tuple<ll, ll, ll>;
+ll n, m, k, t = 1; string s;
+
+constexpr ll INF = 0x3f3f3f3f3f3f3f3f;
+constexpr ll MINF = 0xc0c0c0c0c0c0c0c0;
+constexpr ll MAX = 1010; // SET MAX SIZE
+constexpr ll MOD = 998244353;
+ll a[MAX], fi = 1;
+set <ll> num;
+
+ll mn(ll cur){
+    while(1){
+        auto lb = num.upper_bound(cur - m);
+        if(lb != num.end() && *lb <= cur){
+            cur = *lb + m;
+            continue;
+        }
+
+        auto ub = num.lower_bound(cur + m);
+        if(ub != num.begin() && *--ub >= cur){
+            cur = *ub + m;
+            continue;
+        }
+
+        break;
+    }
+
+    return cur;
+}
+
+void run(){
+    cin >> n >> m;
+    for(int i = 1;i <= n;i++) cin >> a[i];
+
+    for(int i = 1;i <= n;i++){
+        fi = mn(fi);
+        ll now = mn(a[i]);
+
+        cout << now << " ";
+        num.insert(now);
+    }
+}
+
+int main() {
+    fastio; // cin >> t;
+    while(t--) run(); 
+
+    return 0;
+}
