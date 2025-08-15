67a1fc65795b0bab7046a8b416db52512a124c14
kss418 <108602943+kss418@users.noreply.github.com>
2025-08-15 22:17:25 +0900

    [Platinum IV] Title: Redistributing Gifts, Time: 100 ms, Memory: 4260 KB -BaekjoonHub

---
+++ 백준/Platinum/24617. Redistributing Gifts/Redistributing Gifts.cc
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+#include <ext/rope>
+#define fastio cin.tie(0), cout.tie(0), ios::sync_with_stdio(0);
+#define all(x) (x).begin(), (x).end()
+#define x first 
+#define y second
+using namespace std; using ll = long long;
+using ld = long double; using pld = pair<ld, ld>;
+using i128 = __int128_t; using f128 = __float128; 
+using pll = pair<ll, ll>; using tll = tuple<ll, ll, ll>;
+ll n, m, k, t = 1; string s;
+
+constexpr ll INF = 0x3f3f3f3f3f3f3f3f;
+constexpr ll MINF = 0xc0c0c0c0c0c0c0c0;
+constexpr ll MAX = 505; // SET MAX SIZE
+constexpr ll MOD = 998244353;
+ll a[MAX][MAX];
+bool d[MAX][MAX];
+
+void run(){
+    cin >> n;
+    for(int i = 1;i <= n;i++){
+        for(int j = 1;j <= n;j++) cin >> a[i][j];
+    }
+
+    for(int i = 1;i <= n;i++){
+        for(int j = 1;j <= n;j++){
+            d[i][a[i][j]] = 1;
+            if(a[i][j] == i) break;
+        }
+    }
+
+    for(int i = 1;i <= n;i++){
+        for(int j = 1;j <= n;j++){
+            for(int k = 1;k <= n;k++){
+                d[j][k] |= d[j][i] & d[i][k];
+            }
+        }
+    }
+
+    for(int i = 1;i <= n;i++){
+        for(int j = 1;j <= n;j++){
+            if(!d[a[i][j]][i]) continue;
+            cout << a[i][j] << "\n";
+            break;
+        }
+    }
+}
+
+int main() {
+    fastio; // cin >> t;
+    while(t--) run(); 
+
+    return 0;
+}
