22195fa90e03479eafd5dac3e2bfbf1a62487ef7
kss418 <108602943+kss418@users.noreply.github.com>
2025-09-01 13:10:10 +0900

    [Platinum IV] Title: Moo Decomposition, Time: 28 ms, Memory: 18952 KB -BaekjoonHub

---
+++ 백준/Platinum/33763. Moo Decomposition/Moo Decomposition.cc
@@ -0,0 +1,94 @@
+#include <bits/stdc++.h>
+#define fastio cin.tie(0), cout.tie(0), ios::sync_with_stdio(0);
+#define all(x) (x).begin(), (x).end()
+#define x first 
+#define y second
+using namespace std; using ll = long long;
+using ld = long double; using pld = pair<ld, ld>;
+using i128 = __int128_t; using f128 = __float128; 
+using pll = pair<ll, ll>; using tll = tuple<ll, ll, ll>;
+ll n, m, k, t = 1; string s;
+
+constexpr ll INF = 0x3f3f3f3f3f3f3f3f;
+constexpr ll MINF = 0xc0c0c0c0c0c0c0c0;
+constexpr ll MAX = 1010101; // SET MAX SIZE
+constexpr ll MOD = 1e9 + 7;
+ll c[2], result = 1;
+
+class _comb{
+public:
+    vector <ll> fac, inv; ll n, mod;
+    _comb(ll n, ll m = 998244353){ // m == MOD && m == prime
+        fac.resize(n + 1); inv.resize(n + 1);
+        this->mod = m; this->n = n;
+
+        fac[0] = 1; 
+        for(int i = 1;i <= n;i++) fac[i] = fac[i - 1] * i % mod;
+        inv[n] = pow(fac[n], mod - 2, mod);
+        for(int i = n - 1;i >= 0;i--) inv[i] = inv[i + 1] * (i + 1) % mod;
+    }
+
+    ll pow(ll a, ll p, ll mod) {
+        if (!p) return 1;
+        if (p == 1) return a % mod;
+
+        ll cur = pow(a, p / 2, mod);
+        cur = cur * cur % mod;
+        if (p % 2 == 0) return cur % mod;
+        return (cur * a) % mod;
+    }
+
+    ll ret(ll n, ll m){ // return nCm
+        if(n < 0 || m < 0 || n < m) return 0;
+        return fac[n] * inv[m] % mod * inv[n - m] % mod;
+    }
+};
+
+class _pow {
+public:
+    _pow() {}
+
+    ll ret(ll a, ll b){
+        ll ret = 1;
+        while(b){
+            if(b & 1) ret *= a;
+            a *= a; b >>= 1;
+        }      
+        return ret;
+    }
+
+    ll ret(ll a, ll b, ll p){
+        ll ret = 1;
+        while(b){
+            if(b & 1) ret *= a % p, ret %= p;
+            a *= a; a %= p; b >>= 1;
+        }      
+        return ret;
+    }
+};
+
+void run(){
+    cin >> n >> m >> k >> s;
+    reverse(all(s)); _comb comb(MAX, MOD);
+
+    for(auto& i : s){
+        if(i == 'O') c[0]++;
+        else{
+            result = result * comb.ret(c[0] - c[1] * n, n);
+            result %= MOD;
+            c[1]++;
+        }
+    }
+
+    _pow pow;
+    result = pow.ret(result, k, MOD);
+
+    cout << result;
+}
+
+int main() {
+    fastio; // cin >> t;
+    while(t--) run();
+    
+    return 0;
+}
