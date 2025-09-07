bf40aff244c5955199164bc1984e2fdd58944c0b
kss418 <108602943+kss418@users.noreply.github.com>
2025-09-08 01:39:22 +0900

    [Gold II] Title: 도토리 숨기기, Time: 4 ms, Memory: 4300 KB -BaekjoonHub

---
+++ 백준/Gold/15732. 도토리 숨기기/도토리 숨기기.cc
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+#include <ext/rope>
+#define fastio cin.tie(0), cout.tie(0), ios::sync_with_stdio(0);
+#define all(x) (x).begin(), (x).end()
+#define x first 
+#define y second
+using namespace std; using ll = long long;
+using ld = long double; using pld = pair<ld, ld>;
+using i128 = __int128_t; using f128 = __float128; 
+using pll = pair<ll, ll>; using tll = tuple<ll, ll, ll>;
+ll n, m, k, t = 1; string s;
+
+constexpr ll INF = 0x3f3f3f3f3f3f3f3f;
+constexpr ll MINF = 0xc0c0c0c0c0c0c0c0;
+constexpr ll MAX = 201010; // SET MAX SIZE
+constexpr ll MOD = 998244353;
+ll a[MAX];
+vector <tll> arr;
+
+template <typename T = ll>
+class _bs { // 0-based index
+public:
+    T l, r;
+    _bs(T l, T r) {
+        this->l = l; this->r = r;
+    }
+
+    bool f(T cur) {
+        ll ret = 0;
+        for(auto& [s, e, c] : arr){
+            if(s > cur) continue;
+            ll d = min(cur, e) - s;
+            ret += d / c + 1;
+        }
+
+        return ret >= k;
+    }
+
+    T ret() {
+        T lo = l, hi = r, mid = -1;
+        while (hi > lo) {
+            mid = (hi + lo) / (T)2;
+            if (f(mid)) hi = mid;
+            else lo = mid + 1;
+        }
+
+        return lo;
+    }
+};
+
+void run(){
+    cin >> n >> m >> k; _bs <ll> bs(1, 1e6 + 1);
+    for(int i = 1;i <= m;i++){
+        ll s, e, c; cin >> s >> e >> c;
+        arr.push_back({s, e, c});
+    }
+
+    sort(all(arr));
+    cout << bs.ret();
+}
+
+int main() {
+    fastio; // cin >> t;
+    while(t--) run(); 
+
+    return 0;
+}
