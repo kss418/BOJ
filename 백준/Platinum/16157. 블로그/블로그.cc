7a5dd300a955838f2f9b902a8691f5e72647718e
kss418 <108602943+kss418@users.noreply.github.com>
2025-09-10 13:42:52 +0900

    [Platinum IV] Title: 블로그, Time: 0 ms, Memory: 2104 KB -BaekjoonHub

---
+++ 백준/Platinum/16157. 블로그/블로그.cc
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+#define fastio cin.tie(0), cout.tie(0), ios::sync_with_stdio(0);
+#define all(x) (x).begin(), (x).end()
+#define x first 
+#define y second
+using namespace std; using ll = long long;
+using ld = long double; using pld = pair<ld, ld>;
+using i128 = __int128_t; using f128 = __float128; 
+using pll = pair<ll, ll>; using tll = tuple<ll, ll, ll>;
+ll n, m, k, t = 1; string s;
+
+constexpr ll INF = 0x3f3f3f3f3f3f3f3f;
+constexpr ll MINF = 0xc0c0c0c0c0c0c0c0;
+constexpr ll MAX = 101; // SET MAX SIZE
+constexpr ll MOD = 998244353;
+ll dp[MAX][MAX];
+
+ll solve(ll l, ll r){
+    ll& ret = dp[l][r];
+    if(ret != -1) return ret; ret = INF;
+    if(l > r) return ret;
+    if(l == r) return ret = 1;
+
+    ret = min(ret, solve(l + 1, r) + (s[l] != s[r]));
+    ret = min(ret, solve(l, r - 1) + (s[l] != s[r]));
+    for(int i = l;i < r;i++){
+        ret = min(ret, solve(l, i) + solve(i + 1, r));
+    }
+
+    return ret;
+}
+
+void run(){
+    cin >> n >> s; memset(dp, -1, sizeof(dp));
+    cout << solve(0, n - 1);
+}
+
+int main() {
+    fastio; // cin >> t;
+    while(t--) run();
+    
+    return 0;
+}
