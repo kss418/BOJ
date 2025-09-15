133b00b7911baff4c2b513042ac2579baea883ef
kss418 <108602943+kss418@users.noreply.github.com>
2025-09-15 15:02:08 +0900

    [Platinum III] Title: Coronavirus Trend, Time: 928 ms, Memory: 36812 KB -BaekjoonHub

--- 백준/Platinum/20039. Coronavirus Trend/Coronavirus Trend.cc
+++ 백준/Platinum/20039. Coronavirus Trend/Coronavirus Trend.cc
@@ -15,30 +15,33 @@ constexpr ll MAX = 501010; // SET MAX SIZE
 constexpr ll MOD = 998244353;
 ll a[MAX];
 vector <ll> arr;
-map <ll, ll> idx;
 
 template <typename T = ll> // query type
 class _seg { // 구간 예외 처리하기
 public:
-    ll n;
+    ll n, sz;
     class node{
     public:
         int v;
-        node() : node(0xc0c0c0c0){}
+        node() : node(0xc0c0c0c0){} // identity
         node(int v) : v(v) {}
         
         node operator += (node ot){ // add
-            return node();
+            v += ot.v;
+            return *this;
         }
 
-        operator T(){ // update -> query
+        operator T(){ // query
             return v;
         }
     };
     vector <node> seg;
 
     _seg(){}
-    _seg(ll n) { this->n = n; seg.resize(4 * n + 1, node()); }
+    _seg(ll n) : n(n){ 
+        sz = 1; while(sz < n) sz <<= 1ll;
+        seg.assign(2 * sz, _seg::node());
+    }
 
     node merge(node l, node r){
         return{
@@ -46,27 +49,28 @@ public:
         };
     }
 
-    T query(ll st, ll en) { return query(st, en, 1, n); }
-    node query(ll st, ll en, ll l, ll r, ll node = 1) {
+    node query(ll st, ll en) {
         if(st > en) return _seg::node();
-        if (en < l || st > r) return _seg::node();
-        if (st <= l && en >= r) return seg[node];
-        ll mid = (l + r) >> 1;
-        return merge(query(st, en, l, mid, node * 2), query(st, en, mid + 1, r, node * 2 + 1));
-    }
-
-    node update(ll idx, int val) { return update(idx, val, 1, n); }
-    node update(ll idx, int val, ll l, ll r, ll node = 1) {
-        if (idx < l || idx > r) return seg[node];
-        if (l == r) {
-            seg[node] = max(seg[node].v, val);
-            return seg[node];
+        node l = _seg::node(), r = _seg::node();
+        st += sz - 1; en += sz - 1;
+        while(st <= en){
+            if(st & 1) l = merge(l, seg[st++]);
+            if(!(en & 1)) r = merge(seg[en--], r);
+            st >>= 1; en >>= 1; 
         }
-        ll mid = (l + r) >> 1;
-        return seg[node] = merge(update(idx, val, l, mid, node * 2), update(idx, val, mid + 1, r, node * 2 + 1));;
+
+        return merge(l, r);
     }
 
-    void clear(){ seg.clear(); seg.assign(4 * n + 1, node()); }
+    void set(ll idx, node val){
+        ll p = idx + sz - 1;
+        seg[p] = max<T>(seg[p], val);
+        for(p >>= 1ll; p; p >>= 1){
+            seg[p] = merge(seg[p << 1], seg[p << 1 | 1]);
+        }
+    }
+
+    void clear(){ fill(all(seg), node()); }
 };
 
 void run(){
@@ -78,22 +82,22 @@ void run(){
     for(int i = 1;i <= n;i++) a[i] = lower_bound(all(arr), a[i]) - arr.begin() + 1;
 
     for(int i = 1;i <= n;i++){
-        seg[0].update(a[i], 1); seg[3].update(a[i], 1);
+        seg[0].set(a[i], 1); seg[3].set(a[i], 1);
 
-        seg[1].update(a[i], seg[0].query(1, a[i] - 1) + 1);
-        seg[1].update(a[i], seg[5].query(1, a[i] - 1) + 1);
+        seg[1].set(a[i], seg[0].query(1, a[i] - 1) + 1);
+        seg[1].set(a[i], seg[5].query(1, a[i] - 1) + 1);
 
-        seg[2].update(a[i], seg[1].query(1, a[i] - 1) + 1);
-        seg[2].update(a[i], seg[2].query(1, a[i] - 1) + 1);
+        seg[2].set(a[i], seg[1].query(1, a[i] - 1) + 1);
+        seg[2].set(a[i], seg[2].query(1, a[i] - 1) + 1);
 
-        seg[4].update(a[i], seg[2].query(a[i] + 1, n) + 1);
-        seg[4].update(a[i], seg[3].query(a[i] + 1, n) + 1);
+        seg[4].set(a[i], seg[2].query(a[i] + 1, n) + 1);
+        seg[4].set(a[i], seg[3].query(a[i] + 1, n) + 1);
 
-        seg[5].update(a[i], seg[4].query(a[i] + 1, n) + 1);
-        seg[5].update(a[i], seg[5].query(a[i] + 1, n) + 1);
+        seg[5].set(a[i], seg[4].query(a[i] + 1, n) + 1);
+        seg[5].set(a[i], seg[5].query(a[i] + 1, n) + 1);
     }
 
-    ll result = max({seg[2].query(1, n), seg[5].query(1, n), 0});
+    ll result = max<int>({seg[2].query(1, n), seg[5].query(1, n), 0});
     cout << result;
 }
 
