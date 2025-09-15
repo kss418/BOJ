346347081f668d64b34def426360cdffe1403a88
kss418 <108602943+kss418@users.noreply.github.com>
2025-09-15 15:23:18 +0900

    [Gold I] Title: 구간 합 구하기, Time: 208 ms, Memory: 18556 KB -BaekjoonHub

--- 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.cc
+++ 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.cc
@@ -1,70 +1,123 @@
-#include <iostream>
-#include <algorithm>
-#include <vector>
-#include <queue>
-using namespace std;
-long long arr[1000003];
-long long seg[4000010];
-long long n, m, k, num, a, b;
+#include <bits/stdc++.h>
+#include <ext/rope>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace __gnu_cxx;
+#define ordered_set tree<int, null_type, less<int>, rb_tree_tag,tree_order_statistics_node_update>
+#define fastio cin.tie(0), cout.tie(0), ios::sync_with_stdio(0);
+#define all(x) (x).begin(), (x).end()
+#define x first 
+#define y second
+using namespace std; typedef long long ll;
+using ld = long double; using pld = pair<ld, ld>;
+using ull = unsigned long long; 
+using pll = pair<ll, ll>; using tll = tuple<ll, ll, ll>;
+ll n, m, k, t = 1; string s;
 
-long long mk_seg(int st, int en, int node) {
-	if (st == en) {
-		return seg[node] = arr[st];
-	}
-	int mid = (st + en) / 2;
-	seg[node] = mk_seg(st, mid, node * 2) + mk_seg(mid + 1, en, node * 2 + 1);
-	return seg[node];
-}
+constexpr ll INF = 0x3f3f3f3f3f3f3f3f;
+constexpr ll MINF = 0xc0c0c0c0c0c0c0c0;
+constexpr ll MAX = 3010; // SET MAX SIZE
+constexpr ll MOD = 998244353;
+pll a[MAX]; ll co[MAX];
 
-long long query(int st, int en, int node, int left, int right) {
-	if (left > en || right < st) {
-		return 0;
-	}
+template <typename T = ll> // query type
+class _seg { // 구간 예외 처리하기
+public:
+    ll n, sz;
+    class node{
+    public:
+        ll v;
+        node() : node(0){} // identity
+        node(ll v) : v(v) {}
+        
+        node operator += (node ot){ // add
+            v += ot.v;
+            return *this;
+        }
 
-	if (left <= st && en <= right) {
-		return seg[node];
-	}
-	int mid = (st + en) / 2;
-	return query(st, mid, node * 2, left, right) + query(mid + 1, en, node * 2 + 1, left, right);
-	
-}
+        operator T(){ // query
+            return v;
+        }
+    };
+    vector <node> seg;
 
-void update(int st, int en, int node, int index, long long value) {
-	if (index < st || index > en) {
-		return;
-	}
+    _seg(){}
+    _seg(ll n) : n(n){ 
+        sz = 1; while(sz < n) sz <<= 1ll;
+        seg.assign(2 * sz, _seg::node());
+    }
 
-	seg[node] += value;
-	if (st == en) {
-		return;
-	}
-	int mid = (st + en) / 2;
-	update(st, mid, node * 2, index, value);
-	update(mid + 1, en, node * 2 + 1, index, value);
+    node merge(node l, node r){
+        return{
+            l.v + r.v
+        };
+    }
+
+    node query(ll st, ll en) {
+        st = max(1ll, st); en = min(n, en);
+        if(st > en) return _seg::node();
+
+        node l = _seg::node(), r = _seg::node();
+        st += sz - 1; en += sz - 1;
+        while(st <= en){
+            if(st & 1) l = merge(l, seg[st++]);
+            if(!(en & 1)) r = merge(seg[en--], r);
+            st >>= 1; en >>= 1; 
+        }
+
+        return merge(l, r);
+    }
+
+    void set(ll idx, node val){
+        if(idx < 1 || idx > n) return;
+        ll p = idx + sz - 1;
+        seg[p] = val;
+        for(p >>= 1ll; p; p >>= 1){
+            seg[p] = merge(seg[p << 1], seg[p << 1 | 1]);
+        }
+    }
+
+    void add(ll idx, node val){
+        if(idx < 1 || idx > n) return;
+        ll p = idx + sz - 1;
+        seg[p].v += val;
+        for(p >>= 1; p; p >>= 1){
+            seg[p] = merge(seg[p << 1], seg[p << 1 | 1]);
+        }
+    }
+
+    void clear(){ fill(all(seg), node()); }
+};
+
+vector <ll> xv, yv;
+vector <pll> arr[MAX];
+map <ll, ll> xc, yc;
+void comp(vector<ll>& v, map <ll, ll>& c){
+    sort(all(v)); ll cnt = 0;
+    for(auto& i : v){
+        if(!c.count(i)) c[i] = ++cnt;
+    }
+}   
+
+void run(){
+    cin >> n >> m >> k; _seg seg(n);
+    for(int i = 1;i <= n;i++) {
+        ll num; cin >> num;
+        seg.set(i, num);
+    } 
+
+    for(int i = 1;i <= m + k;i++){
+        ll op, idx, v; cin >> op >> idx >> v;
+        if(op == 1) seg.set(idx, v);
+        else cout << seg.query(idx, v) << "\n";
+    }
 }
 
 int main() {
-	cin.tie(0);
-	cout.tie(0);
-	ios::sync_with_stdio(false);
+    fastio; // cin >> t;
+    while(t--) run(); 
 
-	cin >> n >> m >> k;
-	for (int i = 1; i < n + 1; i++) {
-		cin >> arr[i];
-	}
-	mk_seg(1, n, 1);
-	
-	for (int i = 0; i < m + k; i++) {
-		cin >> num >> a >> b;
-		if (num == 1){
-			long long tmp = arr[a];
-			arr[a] = b;
-			update(1, n, 1, a, b - tmp);
-		}
-		else {
-			cout << query(1, n, 1, a, b) << "\n";
-		}
-	}
-	
-	return 0;
+    return 0;
 }
+
