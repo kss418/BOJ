274bd93d11cb0c0c5ddb4a65a6aa545daf2bb970
kss418 <108602943+kss418@users.noreply.github.com>
2025-10-04 01:01:38 +0900

    [Gold II] Title: 수열과 개구리, Time: 196 ms, Memory: 29204 KB -BaekjoonHub

--- 백준/Gold/32294. 수열과 개구리/수열과 개구리.cc
+++ 백준/Gold/32294. 수열과 개구리/수열과 개구리.cc
@@ -1,91 +1,56 @@
-#include <bits/stdc++.h>
-#include <ext/pb_ds/assoc_container.hpp>
-#include <ext/pb_ds/tree_policy.hpp>
-using namespace __gnu_pbds;
-#define ordered_set tree<int, null_type, less<int>, rb_tree_tag,tree_order_statistics_node_update>
-#define fastio cin.tie(0), cout.tie(0), ios::sync_with_stdio(0);
-#define all(x) (x).begin(), (x).end()
-#define x first 
-#define y second
-using namespace std; typedef long long ll;
-using ld = long double;
-using ull = unsigned long long;
-using pll = pair<ll, ll>; using tll = tuple<ll, ll, ll>;
-ll n, m, k, t; string s;
+#include <iostream>
+#include <vector>
+#include <queue> 
+using namespace std;
+using ll = long long;
 
-constexpr ll INF = 0x3f3f3f3f3f3f3f3f;
-constexpr ll MAX = 201010;
-constexpr ll MOD = 998244353;
+const ll MAX = 201010;
+const ll INF = 1e12;
+ll n, m, d[MAX];
+vector <pair<ll, ll>> adj[MAX];
 ll a[MAX], b[MAX];
 
-template <typename T = ll>
-class _dij {
-public:
-    using ptl = pair <T, ll>;
-    ll n; vector <T> d;
-    vector <ll> pre;
-    vector <vector<ptl>> adj;
-    priority_queue <ptl, vector<ptl>, greater<ptl>> pq;
+using pll = pair<ll, ll>;
+priority_queue <pll, vector<pll>, greater<pll>> pq;
 
-    _dij(ll n) {
-        this->n = n;
-        d.resize(n + 1, INF); pre.resize(n + 1, -1);
-        adj.resize(n + 1);
+int main(){
+    ios::sync_with_stdio(0); // fastio
+    cin.tie(0), cout.tie(0); // fastio
+
+    cin >> n;
+    for(int i = 1;i <= n;i++) cin >> a[i];
+    for(int i = 1;i <= n;i++) cin >> b[i];
+
+    for(int i = 1;i <= n;i++){
+        ll nx = i - a[i];
+        // 다음 정점이 0 이하면 0과 연결
+        if(nx <= 0) adj[0].push_back({i, b[i]});
+        // 아니면 다음 정점과 연결
+        else adj[nx].push_back({i, b[i]});
+
+        nx = i + a[i];
+        // 다음 정점이 n 초과면 0과 연결
+        if(nx > n) adj[0].push_back({i, b[i]});
+        // 아니면 다음 정점과 연결
+        else adj[nx].push_back({i, b[i]});
     }
 
-    void add(ll st, ll en, ll c = 1) { // 양방향
-        adj[st].push_back({ c,en });
-        adj[en].push_back({ c,st });
-    }
 
-    void addsol(ll st, ll en, ll c = 1) { // 단방향
-        adj[st].push_back({ c,en });
-    }
+    for(int i = 0;i < MAX;i++) d[i] = INF;
+    pq.push({0, 0});
 
-    void init(ll st, T fi = 0) {
-        d[st] = fi;
-        pq.push({ d[st], st });
+    while(!pq.empty()){
+        auto[cd, cur] = pq.top(); pq.pop();
+        if(d[cur] <= cd) continue;
+        d[cur] = cd;
 
-        while (!pq.empty()) {
-            auto [cd, cur] = pq.top(); pq.pop();
-            if (cd > d[cur]) continue;
-
-            for (auto& nn : adj[cur]) {
-                auto [nd, nxt] = nn;
-                if (d[nxt] <= nd + cd) continue;
-
-                d[nxt] = nd + cd;
-                pre[nxt] = cur;
-                pq.push({ d[nxt], nxt });
-            }
+        for(auto& [nxt, co] : adj[cur]){
+            if(d[nxt] <= cd + co) continue;
+            pq.push({cd + co, nxt});
         }
     }
 
-    T ret(ll n) {
-        return d[n];
-    }
-};
-
-
-int main() {
-    fastio;
-
-
-    cin >> n; _dij dij(n);
-    for(int i = 1;i <= n;i++) cin >> a[i];
-    for(int i = 1;i <= n;i++) {
-        cin >> b[i];
-        if(i - a[i] <= 0) dij.addsol(0, i, b[i]);
-        else dij.addsol(i - a[i], i, b[i]);
-
-        if(i + a[i] > n) dij.addsol(0, i, b[i]);
-        else dij.addsol(i + a[i], i, b[i]);
-    }
-    dij.init(0);
-
-    for(int i = 1;i <= n;i++) cout << dij.ret(i) << " ";
-
+    for(int i = 1;i <= n;i++) cout << d[i] << " ";
 
     return 0;
 }
-
