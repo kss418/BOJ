c6b0b00444303d0d73a6b3d0aecbaa5a0a10925c
kss418 <108602943+kss418@users.noreply.github.com>
2025-10-10 18:16:16 +0900

    [Gold V] Title: Moo 게임, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/5904. Moo 게임/Moo 게임.cc
+++ 백준/Gold/5904. Moo 게임/Moo 게임.cc
@@ -1,45 +1,30 @@
-#include <bits/stdc++.h>
-#include <ext/pb_ds/assoc_container.hpp>
-#include <ext/pb_ds/tree_policy.hpp>
-using namespace __gnu_pbds;
-#define ordered_set tree<int, null_type, less<int>, rb_tree_tag,tree_order_statistics_node_update>
-#define fastio cin.tie(0), cout.tie(0), ios::sync_with_stdio(0);
-#define all(x) (x).begin(), (x).end()
-#define x first 
-#define y second
-using namespace std; typedef long long ll;
-using ull = unsigned long long;
-using pll = pair<ll, ll>; using tll = tuple<ll, ll, ll>;
-ll n, m, k, t; string s;
+#include <iostream>
+using namespace std;
+using ll = long long;
 
-constexpr ll INF = 0x3f3f3f3f3f3f3f3f;
-constexpr ll MAX = 201010;
-constexpr ll MOD = 998244353;
-vector <ll> arr;
-ll len[MAX];
+const ll MAX = 3010;
+const ll INF = 1e12;
+ll n, len[55], mx;
 
-ll solve(ll cur, ll cnt){
-    if(cur <= len[cnt]) return solve(cur, cnt - 1);
-    else if(cur == len[cnt] + 1) return 1;
-    else if(cur <= len[cnt] + cnt + 3) return 0;
-    return solve(cur - len[cnt] - cnt - 3, cnt - 1);
+char dnc(ll cur, ll sz){
+    if(!sz) return (cur == 1 ? 'm' : 'o');
+
+    if(cur <= len[sz - 1]) return dnc(cur, sz - 1);
+    else if(cur > len[sz - 1] + sz + 3) return dnc(cur - len[sz - 1] - sz - 3, sz - 1);
+    return (cur == len[sz - 1] + 1 ? 'm' : 'o');
 }
 
-int main() {
-    fastio;
+int main(){
+    ios::sync_with_stdio(0); // fastio
+    cin.tie(0), cout.tie(0); // fastio
 
-    cin >> n;
-
-    ll cnt = 0, cur = 0; len[0] = 0;
-    while(cur <= n){
-        cnt++;
-        len[cnt] = len[cnt - 1] * 2 + cnt + 2;
-        cur = len[cnt];
+    cin >> n; len[0] = 3;
+    for(int i = 1;i < 55;i++){
+        len[i] = 2 * len[i - 1] + i + 3; mx = i;
+        if(len[i] > n) break;
     }
 
-    ll ret = solve(n, cnt - 1);
-    if(ret) cout << "m";
-    else cout << "o";
-
+    cout << dnc(n, mx);
     return 0;
 }
+
