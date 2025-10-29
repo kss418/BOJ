26badee0407ca5a43c6a009fdb5b3af7f2577ae9
kss418 <108602943+kss418@users.noreply.github.com>
2025-10-29 13:35:08 +0900

    [Platinum I] Title: 그리드 게임, Time: 888 ms, Memory: 104412 KB -BaekjoonHub

---
+++ 백준/Platinum/2452. 그리드 게임/그리드 게임.cc
@@ -0,0 +1,120 @@
+#include <bits/stdc++.h>
+#define fastio cin.tie(0), cout.tie(0), ios::sync_with_stdio(0);
+#define all(x) (x).begin(), (x).end()
+#define x first 
+#define y second
+using namespace std; using ll = long long;
+using ld = long double; using pld = pair<ld, ld>;
+using i128 = __int128_t; using f128 = __float128; 
+using pll = pair<ll, ll>; using tll = tuple<ll, ll, ll>;
+ll n, m, k, t = 1; string s;
+
+constexpr ll INF = 0x3f3f3f3f;
+constexpr ll MINF = 0xc0c0c0c0c0c0c0c0;
+constexpr ll MAX = 101; // SET MAX SIZE
+constexpr ll MOD = 998244353;
+bool a[MAX][MAX], c[MAX * MAX][MAX * MAX], v[MAX * MAX];
+vector <int> adj[MAX * MAX];
+int d[MAX * MAX], result = INF;
+
+class _uf { 
+public:
+    ll n; vector <ll> p, si;
+    
+    _uf(){}
+    _uf(ll n) { 
+        this->n = n;
+        p.resize(n + 1, -1); si.resize(n + 1, 1);
+    }
+
+    ll find(ll num) { 
+        if (p[num] == -1) return num;
+        return p[num] = find(p[num]);
+    }
+
+    void merge(ll a, ll b) {
+        a = find(a); b = find(b);
+        if (a == b) return;
+        if (size(a) < size(b)) swap(a, b);
+        p[b] = a, si[a] += si[b];
+    }
+
+    ll size(ll num){ return si[find(num)]; }
+    bool same(ll a, ll b) { return find(a) == find(b); }
+};
+
+ll num(ll cy, ll cx){
+    return cy * m + cx;
+}
+
+ll dx[4] = {0, 0, 1, -1}, dy[4] = {1, -1, 0, 0};
+bool outrange(ll cy, ll cx){
+    return cy < 0 || cx < 0 || cy >= n || cx >= m;
+}
+
+deque <ll> q;
+int bfs(ll cur){
+    int ret = 0;
+    memset(d, 0x3f, sizeof(d));
+    q.push_back(cur); d[cur] = 0;
+    while(!q.empty()){
+        auto cur = q.front(); q.pop_front();
+        for(auto& nxt : adj[cur]){
+            if(d[nxt] <= d[cur] + 1) continue;
+            d[nxt] = d[cur] + 1;
+            ret = max(ret, d[nxt]);
+            q.push_back(nxt);
+        }
+    }
+
+    return ret;
+}
+
+void run(){
+    cin >> n >> m; _uf uf(n * m);
+    for(int i = 0;i < n;i++){
+        for(int j = 0;j < m;j++) cin >> a[i][j];
+    }
+
+    for(int i = 0;i < n;i++){
+        for(int j = 0;j < m;j++){
+            for(int k = 0;k < 4;k++){
+                ll ny = i + dy[k], nx = j + dx[k];
+                if(outrange(ny, nx)) continue;
+                if(a[ny][nx] != a[i][j]) continue;
+                uf.merge(num(ny, nx), num(i, j));
+            }
+        }
+    }
+
+    for(int i = 0;i < n;i++){
+        for(int j = 0;j < m;j++){
+            for(int k = 0;k < 4;k++){
+                ll ny = i + dy[k], nx = j + dx[k];
+                if(outrange(ny, nx)) continue;
+                if(a[ny][nx] == a[i][j]) continue;
+
+                ll cur = uf.find(num(i, j));
+                ll nxt = uf.find(num(ny, nx));
+
+                if(!c[cur][nxt]) adj[cur].push_back(nxt);
+                if(!c[nxt][cur]) adj[nxt].push_back(cur);
+                c[cur][nxt] = c[nxt][cur] = 1;
+            }
+        }
+    }
+
+    for(int i = 0;i < n * m;i++){
+        if(uf.find(i) != i) continue;
+        result = min(result, bfs(i));
+    }
+
+    cout << result;
+}
+
+int main() {
+    fastio; // cin >> t;
+    while(t--) run();
+    
+    return 0;
+}
