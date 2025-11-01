31f7a6924c4bde8ae3b6995dbef5cc778627d944
kss418 <108602943+kss418@users.noreply.github.com>
2025-11-01 21:27:20 +0900

    [Gold I] Title: 구두 수선공, Time: 0 ms, Memory: 2072 KB -BaekjoonHub

--- 백준/Gold/14908. 구두 수선공/구두 수선공.cc
+++ 백준/Gold/14908. 구두 수선공/구두 수선공.cc
@@ -1,44 +1,22 @@
 #include <iostream>
-#include <vector>
 #include <algorithm>
-#include <tuple>
 using namespace std;
-vector <tuple <double, int, int, int>> arr;
+using ll = long long;
+using ld = long double;
 
-bool cmp(tuple <double, int, int, int> i, tuple <double, int, int, int> j) {
-	double prior1, prior2;
-	int a1, a2, b1, b2, n1, n2;
-	tie(prior1,a1,b1,n1) = i;
-	tie(prior2, a2, b2, n2) = j;
-	if (prior1 != prior2) {
-		return prior1 > prior2;
-	}
-	else {
-		return n1 < n2;
-	}
-}
+const ll MAX = 1010;
+ll n, t[MAX], s[MAX];
+pair <ld, ll> a[MAX];
 
-int main() {
-	cin.tie(0);
-	cout.tie(0);
-	ios_base::sync_with_stdio(false);
+int main(){
+    cin >> n;
+    for(int i = 1;i <= n;i++){
+        cin >> t[i] >> s[i];
+        a[i].first = (ld)t[i] / (ld)s[i];
+        a[i].second = i;
+    }
+    sort(a + 1, a + n + 1);
 
-	int n;
-	cin >> n;
-	for (int i = 1; i <= n;i++) {
-		int a, b;
-		cin >> a >> b;
-		arr.push_back({ (double)b / (double)a,a,b,i });
-	}
-	sort(arr.begin(), arr.end(), cmp);
-
-	for (auto i : arr) {
-		double prior;
-		int a, b, n;
-		tie(prior,a, b, n) = i;
-		cout << n << " ";
-	}
-
-	
-	return 0;
+    for(int i = 1;i <= n;i++) cout << a[i].second << " ";
+    return 0;
 }
