a09e86715ca2ee894d4e0d965096ee686d942886
kss418 <108602943+kss418@users.noreply.github.com>
2025-11-05 15:28:47 +0900

    [Platinum IV] Title: 숫자의 신, Time: 0 ms, Memory: 2040 KB -BaekjoonHub

---
+++ 백준/Platinum/1422. 숫자의 신/숫자의 신.cc
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+#define fastio cin.tie(0), cout.tie(0), ios::sync_with_stdio(0);
+#define all(x) (x).begin(), (x).end()
+#define x first 
+#define y second
+using namespace std; using ll = long long;
+using ld = long double; using pld = pair<ld, ld>;
+using i128 = __int128_t; using f128 = __float128; 
+using pll = pair<ll, ll>; using tll = tuple<ll, ll, ll>;
+ll n, m, k, t = 1; string s;
+
+constexpr ll INF = 0x3f3f3f3f3f3f3f3f;
+constexpr ll MINF = 0xc0c0c0c0c0c0c0c0;
+constexpr ll MAX = 55; // SET MAX SIZE
+constexpr ll MOD = 998244353;
+ll a[MAX], len;
+vector <string> arr, mx;
+
+void run(){
+    cin >> n >> m;
+    for(int i = 1;i <= n;i++){
+        cin >> a[i];
+        string now = to_string(a[i]);
+        arr.push_back(now);
+        len = max<ll>(len, now.size());
+    }
+
+    for(auto& i : arr) if(i.size() == len) mx.push_back(i);
+    sort(all(mx), [](string& a, string& b){
+        return a + b > b + a;
+    });
+
+    for(int i = 1;i <= m - n;i++) arr.push_back(mx[0]);
+    sort(all(arr), [](string& a, string& b){
+        return a + b > b + a;
+    });
+
+    for(auto& i : arr) cout << i;
+}
+
+int main() {
+    fastio; // cin >> t;
+    while(t--) run();
+    
+    return 0;
+}
