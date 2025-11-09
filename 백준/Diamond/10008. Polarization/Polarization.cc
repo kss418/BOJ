d6ab1b580bf397d58e12c05c1aaeb87e98c9d822
kss418 <108602943+kss418@users.noreply.github.com>
2025-11-09 16:39:23 +0900

    [Diamond II] Title: Polarization, Time: 128 ms, Memory: 27488 KB -BaekjoonHub

---
+++ 백준/Diamond/10008. Polarization/Polarization.cc
@@ -0,0 +1,106 @@
+#include <bits/stdc++.h>
+#define fastio cin.tie(0), cout.tie(0), ios::sync_with_stdio(0);
+#define all(x) (x).begin(), (x).end()
+#define x first 
+#define y second
+using namespace std; using ll = long long;
+using ld = long double; using pld = pair<ld, ld>;
+using i128 = __int128_t; using f128 = __float128; 
+using pll = pair<ll, ll>; using tll = tuple<ll, ll, ll>;
+ll n, m, k, t = 1; string s;
+
+constexpr ll INF = 0x3f3f3f3f3f3f3f3f;
+constexpr ll MINF = 0xc0c0c0c0c0c0c0c0;
+constexpr ll MAX = 250101; // SET MAX SIZE
+constexpr ll MOD = 998244353;
+ll sz[MAX], result, p[MAX];
+vector <ll> adj[MAX], c;
+bitset <251010> dp;
+
+void init(ll cur = 1, ll pre = -1){
+    sz[cur] = 1; p[cur] = pre;
+    for(auto& nxt : adj[cur]){
+        if(nxt == pre) continue;
+        init(nxt, cur);
+        sz[cur] += sz[nxt];
+    }
+}   
+
+void cent(ll cur = 1, ll pre = -1){
+    bool flag = 0;
+    for(auto& nxt : adj[cur]){
+        if(nxt == pre) continue;
+        if(2 * sz[nxt] > n){
+            flag = 1;
+            cent(nxt, cur);
+        }
+    }
+
+    if(!flag && 2 * (n - sz[cur]) <= n) c.push_back(cur);
+}
+
+pll cal(ll cur, ll pre = -1){
+    pll ret = {0, 1};
+    for(auto& nxt : adj[cur]){
+        if(nxt == pre) continue;
+        pll now = cal(nxt, cur);
+        ret.x += now.x; ret.y += now.y;
+    }
+
+    ret.x += ret.y;
+    return ret;
+}
+
+void run(){
+    cin >> n;
+    for(int i = 1;i < n;i++){
+        ll s, e; cin >> s >> e;
+        adj[s].push_back(e);
+        adj[e].push_back(s);
+    }
+    init(); cent();
+
+    for(auto& cur : c){
+        vector <ll> arr; vector <pll> comp; dp.reset();
+        for(auto& nxt : adj[cur]){
+            arr.push_back(nxt == p[cur] ? n - sz[cur] : sz[nxt]);
+        }
+        sort(all(arr));
+
+        ll la = -1, cnt = 0;
+        for(auto& i : arr){
+            if(i != la){
+                if(la != -1) comp.push_back({la, cnt});
+                cnt = 1; la = i;
+            }
+            else cnt++;
+        }
+        comp.push_back({la, cnt});
+
+        vector <ll> num;
+        for(auto& [x, y] : comp){
+            ll now = 1;
+            while(y){
+                ll mn = min(now, y);
+                num.push_back(x * mn);
+                y -= mn; now <<= 1ll;
+            }
+        }
+    
+        dp[0] = 1; ll sum = cal(cur).x;
+        for(auto& i : num) dp |= dp << i;
+        for(int i = n / 2 + 1;i >= 0;i--){
+            if(!dp[i]) continue;
+            result = max<ll>(result, i * (n - i - 1) + sum);
+        }
+    }
+
+    cout << n - 1 << " " << max(result - n, 0ll);
+}
+
+int main() {
+    fastio; // cin >> t;
+    while(t--) run();
+    
+    return 0;
+}
---
+++ 백준/Diamond/10008. Polarization/README.md
@@ -0,0 +1,34 @@
+# [Diamond II] Polarization - 10008 
+
+[문제 링크](https://www.acmicpc.net/problem/10008) 
+
+### 성능 요약
+
+메모리: 27488 KB, 시간: 128 ms
+
+### 분류
+
+센트로이드, 배낭 문제, 수학, 트리
+
+### 제출 일자
+
+2025년 11월 9일 16:39:09
+
+### 문제 설명
+
+<p>Everyone knew it would only be a matter of time. So what? Faced for years on, a peril becomes the every-day reality. It loses its meaning...</p>
+
+<p>Today the letter of the Bitotian char Bittard to the Byteotian king Byteasar was released to the public. Bitotia requested annexation of the whole Byteotia on pain of using the Bit Polarizing Magnet (BPM). If used, the BPM would make each and every road in Byteotia unidirectional. The enemy knows only too well that this could be a fatal blow to the minimalist Byteotian infrastructure - there is a unique way between each pair of towns.</p>
+
+<p>How badly can the BPM damage the Byteotian infrastructure? Determine the minimum and the maximum number of such pairs of towns that it will still be possible to travel from one of them to the other while observing the new roads orientation.</p>
+
+### 입력 
+
+ <p>The first line of the standard input gives a single integer n (1 ≤ n ≤ 250,000), the number of towns in Byteotia. The n-1 lines that follow describe these roads. Each such line holds two integers, u and v (1 ≤ u ≤ v ≤ n), which indicate that there is a direct road (still bidirectional at the moment) linking the towns no. u and v.</p>
+
+<p> </p>
+
+### 출력 
+
+ <p>Two integers should be printed to the first and only line of the standard output. The first number should be the minimum and the second - the maximum number of pairs of towns which could remain connected (though in one direction only) after the roads are polarized.</p>
+
